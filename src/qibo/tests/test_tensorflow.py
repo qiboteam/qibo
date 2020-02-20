0e9b9981841dff4d190f0b8c0eff8f57a89a709d
stavros11 <stavrosefthm@gmail.com>
2020-02-20 10:35:15 +0400

    Modify tests to respect the new scheme

--- src/qibo/tests/test_tensorflow.py
+++ src/qibo/tests/test_tensorflow.py
@@ -2,15 +2,31 @@
 Testing tensorflow backend.
 """
 import numpy as np
-from qibo import models
+from qibo import Circuit
 from qibo import gates
+
+
+def test_circuit_sanity():
+    """Check if the number of qbits is preserved."""
+    c = Circuit(2)
+    assert c.nqubits == 2
+    assert c.size == 2
+
+
+def test_circuit_add():
+    """Check if circuit depth increases with the add method."""
+    c = Circuit(2)
+    c.add(gates.H(0))
+    c.add(gates.H(1))
+    c.add(gates.CNOT(0, 1))
+    assert c.depth == 3
 
 
 def test_hadamard():
     """Check Hadamard gate is working properly."""
-    c = models.Circuit(1)
+    c = Circuit(1)
     c.add(gates.H(0))
-    final_state = c.run()
+    final_state = c.execute()
     target_state = np.ones_like(final_state) / np.sqrt(2)
     np.testing.assert_allclose(final_state, target_state)
 
@@ -18,17 +34,17 @@ def test_hadamard():
 def test_flatten():
     """Check flatten gate is working properly."""
     target_state = np.ones(4) / 2.0
-    c = models.Circuit(2)
+    c = Circuit(2)
     c.add(gates.Flatten(target_state))
-    final_state = c.run()
+    final_state = c.execute()
     np.testing.assert_allclose(final_state, target_state)
 
 
 def test_xgate():
     """Check X gate is working properly."""
-    c = models.Circuit(2)
+    c = Circuit(2)
     c.add(gates.X(0))
-    final_state = c.run()
+    final_state = c.execute()
     target_state = np.zeros_like(final_state)
     target_state[2] = 1.0
     np.testing.assert_allclose(final_state, target_state)
@@ -36,9 +52,9 @@ def test_xgate():
 
 def test_rz_no_effect():
     """Check RZ gate is working properly when qubit is on |0>."""
-    c = models.Circuit(2)
+    c = Circuit(2)
     c.add(gates.RZ(0, 0.1234))
-    final_state = c.run()
+    final_state = c.execute()
     target_state = np.zeros_like(final_state)
     target_state[0] = 1.0
     np.testing.assert_allclose(final_state, target_state)
@@ -48,10 +64,10 @@ def test_rz_phase():
     """Check RZ gate is working properly when qubit is on |1>."""
     theta = 0.1234
 
-    c = models.Circuit(2)
+    c = Circuit(2)
     c.add(gates.X(0))
     c.add(gates.RZ(0, theta))
-    final_state = c.run()
+    final_state = c.execute()
 
     target_state = np.zeros_like(final_state)
     target_state[2] = np.exp(1j * np.pi * theta)
@@ -62,10 +78,10 @@ def test_rx():
     """Check RX gate is working properly."""
     theta = 0.1234
 
-    c = models.Circuit(1)
+    c = Circuit(1)
     c.add(gates.H(0))
     c.add(gates.RX(0, theta))
-    final_state = c.run()
+    final_state = c.execute()
 
     phase = np.exp(1j * np.pi * theta / 2.0)
     gate = phase * np.array([[phase.real, -1j * phase.imag],
@@ -78,10 +94,10 @@ def test_ry():
     """Check RY gate is working properly."""
     theta = 0.1234
 
-    c = models.Circuit(1)
+    c = Circuit(1)
     c.add(gates.H(0))
     c.add(gates.RY(0, theta))
-    final_state = c.run()
+    final_state = c.execute()
 
     phase = np.exp(1j * np.pi * theta / 2.0)
     gate = phase * np.array([[phase.real, -phase.imag],
@@ -94,11 +110,11 @@ def test_crz():
     """Check CRZ gate is working properly on |11>."""
     theta = 0.1234
 
-    c = models.Circuit(2)
+    c = Circuit(2)
     c.add(gates.X(0))
     c.add(gates.X(1))
     c.add(gates.CRZ(0, 1, theta))
-    final_state = c.run()
+    final_state = c.execute()
 
     phase = np.exp(1j * np.pi * theta)
     target_state = np.zeros_like(final_state)
@@ -108,9 +124,9 @@ def test_crz():
 
 def test_cnot_no_effect():
     """Check CNOT gate is working properly on |00>."""
-    c = models.Circuit(2)
+    c = Circuit(2)
     c.add(gates.CNOT(0, 1))
-    final_state = c.run()
+    final_state = c.execute()
     target_state = np.zeros_like(final_state)
     target_state[0] = 1.0
     np.testing.assert_allclose(final_state, target_state)
@@ -118,10 +134,10 @@ def test_cnot_no_effect():
 
 def test_cnot():
     """Check CNOT gate is working properly on |10>."""
-    c = models.Circuit(2)
+    c = Circuit(2)
     c.add(gates.X(0))
     c.add(gates.CNOT(0, 1))
-    final_state = c.run()
+    final_state = c.execute()
     target_state = np.zeros_like(final_state)
     target_state[3] = 1.0
     np.testing.assert_allclose(final_state, target_state)
