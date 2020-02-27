2001c6d6e333fb937da199316474f108a695b078
stavros11 <stavrosefthm@gmail.com>
2020-02-27 17:43:41 +0400

    Two qubit gates do not work

--- src/qibo/tensorflow/gates.py
+++ src/qibo/tensorflow/gates.py
@@ -10,14 +10,6 @@ class TensorflowGate:
     """The base Tensorflow gate."""
     from qibo.config import DTYPECPX as dtype
 
-    def slice_generator(self, q: int, is_one: bool = False) -> int:
-        q = self.nqubits - q - 1 # because we use "cirq" like order
-        s = (q + 1) * int(is_one)
-        while s < self.nstates:
-            for i in range(s, s + q + 1):
-                yield i
-            s += 2 * q + 2
-
     def _call_0(self, state0: tf.Tensor, state1: tf.Tensor) -> tf.Tensor:
         raise NotImplementedError
 
@@ -26,8 +18,10 @@ class TensorflowGate:
 
     def __call__(self, state: tf.Tensor) -> tf.Tensor:
         """Implements the `Gate` on a given state."""
-        slice0 = tuple(self.slice_generator(self.qubits[0], False))
-        slice1 = tuple(self.slice_generator(self.qubits[0], True))
+        d = 2**(self.nqubits - self.qubits[0])
+        slice = np.array([x + d * y for x in range(d // 2) for y in range(self.nstates // d)])
+        slice0 = tuple(slice)
+        slice1 = tuple(slice + d // 2)
 
         state0 = tf.gather(state, slice0)
         state1 = tf.gather(state, slice1)
@@ -59,8 +53,21 @@ class TensorflowControlledGate(TensorflowGate):
 
     def __call__(self, state: tf.Tensor) -> tf.Tensor:
         """Implements the `Gate` on a given state."""
-        slice0 = tuple(self.slice_generator(self.qubits[0], self.qubits[1], False))
-        slice1 = tuple(self.slice_generator(self.qubits[0], self.qubits[1], True))
+        #slice0 = tuple(self.slice_generator(self.qubits[0], self.qubits[1], False))
+        #slice1 = tuple(self.slice_generator(self.qubits[0], self.qubits[1], True))
+
+        dc = 2 ** (self.nqubits - self.qubits[0])
+        dt = 2 ** (self.nqubits - self.qubits[1])
+        dmin, dmax = min(dc, dt), min(dc, dt)
+        slice = np.array([x + dmin * y + dmax * z
+                          for x in range(dmin // 2)
+                          for y in range(dmax // dmin)
+                          for z in range(self.nstates // dmax)])
+        slice0 = tuple(slice + dc // 2)
+        slice1 = tuple(slice + (dc + dt) // 2)
+
+        print(slice0)
+        print(slice1)
 
         state0 = tf.gather(state, slice0)
         state1 = tf.gather(state, slice1)
@@ -222,8 +229,32 @@ class SWAP(TensorflowGate, base_gates.SWAP):
     def __init__(self, *args):
         base_gates.SWAP.__init__(self, *args)
 
+    def slice_generator(self, control: int, q: int, is_one: bool = False) -> int:
+        q = self.nqubits - q - 1 # because we use "cirq" like order
+        control = self.nqubits - control - 1
+        s = (q + 1) * int(is_one)
+        while s < self.nstates:
+            for i in range(s, s + q + 1):
+                if control is None:
+                    yield i
+                elif (i // 2**control) % 2 == 1:
+                    yield i
+            s += 2 * q + 2
 
-class CRZ(TensorflowGate, base_gates.CRZ):
+    def __call__(self, state: tf.Tensor) -> tf.Tensor:
+        d1 = 2 ** (self.nqubits - self.qubits[0] - 1)
+        d2 = 2 ** (self.nqubits - self.qubits[1] - 1)
+        ind01, ind10 = [], []
+        for i in range(self.nstates):
+            b1 = (i // d1) % 2
+            b2 = (i // d2) % 2
+            if not b1 and b2:
+                ind01.append(i)
+            elif b1 and not b2:
+                ind10.append(i)
+
+
+class CRZ(TensorflowControlledGate, base_gates.CRZ):
 
     def __init__(self, *args):
         base_gates.CRZ.__init__(self, *args)
