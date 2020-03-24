4057bc9595cb4c969d0cc290bc854db1038e209c
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-03-24 14:06:01 +0400

    Refactor result tests

--- src/qibo/tests/test_measurements.py
+++ src/qibo/tests/test_measurements.py
@@ -1,37 +1,66 @@
+import collections
 import numpy as np
 import pytest
 from qibo import gates, models
+from typing import Optional
+
+
+def assert_results(result,
+                   decimal_samples: Optional[np.ndarray] = None,
+                   binary_samples: Optional[np.ndarray] = None,
+                   decimal_frequencies: Optional[collections.Counter] = None,
+                   binary_frequencies: Optional[collections.Counter] = None):
+  if decimal_samples is not None:
+      np.testing.assert_allclose(result.samples(False).numpy(), decimal_samples)
+  if binary_samples is not None:
+      np.testing.assert_allclose(result.samples(True).numpy(), binary_samples)
+  if decimal_frequencies is not None:
+      assert result.frequencies(False) == collections.Counter(decimal_frequencies)
+  if binary_frequencies is not None:
+      assert result.frequencies(True) == collections.Counter(binary_frequencies)
 
 
 def test_measurement_gate():
-    """Check that measurement gate works when called on a state."""
+    """Check that measurement gate works when called on the state |00>."""
     state = np.zeros(4)
     state[0] = 1
     state = state.reshape((2, 2))
+    result = gates.M(0)(state, nshots=100)
+    assert_results(result,
+                   decimal_samples=np.zeros((100,)),
+                   binary_samples=np.zeros((100, 1)),
+                   decimal_frequencies={0: 100},
+                   binary_frequencies={"0": 100})
 
-    measurements = gates.M(0)(state, nshots=100).samples(False).numpy()
-    target_measurements = np.zeros_like(measurements)
-    assert measurements.shape == (100,)
-    np.testing.assert_allclose(measurements, target_measurements)
 
-    state[0], state[-1] = 0, 1
-    measurements = gates.M(0)(state, nshots=100).samples(False).numpy()
-    target_measurements = np.ones_like(measurements)
-    assert measurements.shape == (100,)
-    np.testing.assert_allclose(measurements, target_measurements)
+def test_measurement_gate2():
+    """Check that measurement gate works when called on the state |11>."""
+    state = np.zeros(4)
+    state[-1] = 1
+    state = state.reshape((2, 2))
+    result = gates.M(1)(state, nshots=100)
+    assert_results(result,
+                   decimal_samples=np.ones((100,)),
+                   binary_samples=np.ones((100, 1)),
+                   decimal_frequencies={1: 100},
+                   binary_frequencies={"1": 100})
 
 
 def test_multiple_qubit_measurement_gate():
-    """Check that multiple qubit measurement gate works when called on state."""
+    """Check that multiple qubit measurement gate works when called on |10>."""
     state = np.zeros(4)
-    state[0] = 0
     state[2] = 1
     state = state.reshape((2, 2))
+    print([state[0, 0], state[0, 1], state[1, 0], state[1, 1]])
+    result = gates.M(0, 1)(state, nshots=100)
 
-    measurements = gates.M(0, 1)(state, nshots=100).samples(False).numpy()
-    target_measurements = 2 * np.ones_like(measurements)
-    assert measurements.shape == (100,)
-    np.testing.assert_allclose(measurements, target_measurements)
+    target_binary_samples = np.zeros((100, 2))
+    target_binary_samples[:, 0] = 1
+    assert_results(result,
+                   decimal_samples=2 * np.ones((100,)),
+                   binary_samples=target_binary_samples,
+                   decimal_frequencies={2: 100},
+                   binary_frequencies={"10": 100})
 
 
 def test_controlled_measurement_error():
