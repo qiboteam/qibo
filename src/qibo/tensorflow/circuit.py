Fix steps calculation

src/qibo/tensorflow/circuit.py
@@ -35,11 +35,11 @@ class TensorflowCircuit(circuit.BaseCircuit):
         # Calculate callbacks for initial state
         callback_results = [[callback(state)] for callback in self.callbacks]
 
-        for gate in self.queue:
+        for ig, gate in enumerate(self.queue):
             state = gate(state)
-            # TODO: Fix this calculation according to callback.steps
-            for i, callback in enumerate(self.callbacks):
-                callback_results[i].append(callback(state))
+            for ic, callback in enumerate(self.callbacks):
+                if (ig + 1) % callback.steps == 0:
+                    callback_results[ic].append(callback(state))
 
         # Stack all results for each callback
         callback_results = [tf.stack(r) for r in callback_results]

src/qibo/tests/test_callbacks.py
@@ -30,6 +30,7 @@ def test_entropy_singlet_state():
 
 
 def test_entropy_in_circuit():
+    """Check that entropy calculation works in circuit."""
     entropy = callbacks.EntanglementEntropy([0])
     c = Circuit(2)
     c.add(gates.H(0))
@@ -37,11 +38,11 @@ def test_entropy_in_circuit():
     state = c(callback=entropy)
 
     result = entropy.results.numpy()
-    print(entropy._results)
     np.testing.assert_allclose(result, [0, 0, np.log(2)], atol=_atol)
 
 
 def test_entropy_in_compiled_circuit():
+    """Check that entropy calculation works when circuit is compiled."""
     entropy = callbacks.EntanglementEntropy([0])
     c = Circuit(2)
     c.add(gates.H(0))
@@ -50,5 +51,19 @@ def test_entropy_in_compiled_circuit():
     state = c()
 
     result = entropy.results.numpy()
-    print(entropy._results)
     np.testing.assert_allclose(result, [0, 0, np.log(2)], atol=_atol)
+
+
+def test_entropy_steps():
+    """Check that using steps skips the appropriate number of gates."""
+    entropy = callbacks.EntanglementEntropy([0], steps=2)
+    c = Circuit(2)
+    c.add(gates.H(0))
+    c.add(gates.CNOT(0, 1))
+    c.add(gates.H(1))
+    c.add(gates.CNOT(1, 0))
+    c.compile(callback=entropy)
+    state = c()
+
+    result = entropy.results.numpy()
+    np.testing.assert_allclose(result, [0, np.log(2), np.log(2)], atol=_atol)