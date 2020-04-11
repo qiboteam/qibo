cbe633f86133ea95aed7f6050aa963da82193532
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-04-11 16:46:08 +0400

    Add variable gradient tests

--- src/qibo/tests/test_tensorflow.py
+++ src/qibo/tests/test_tensorflow.py
@@ -451,3 +451,72 @@ def test_circuit_custom_compilation():
     r2 = compiled_circuit(init_state)
 
     np.testing.assert_allclose(r1, r2)
+
+
+def test_variable_theta():
+    """Check that parametrized gates accept `tf.Variable` parameters."""
+    import tensorflow as tf
+    from qibo.config import DTYPE
+    theta1 = tf.Variable(0.1234, dtype=DTYPE)
+    theta2 = tf.Variable(0.4321, dtype=DTYPE)
+
+    cvar = Circuit(2)
+    cvar.add(gates.RX(0, theta1))
+    cvar.add(gates.RY(1, theta2))
+    final_state = cvar().numpy()
+
+    c = Circuit(2)
+    c.add(gates.RX(0, 0.1234))
+    c.add(gates.RY(1, 0.4321))
+    target_state = c().numpy()
+
+    np.testing.assert_allclose(final_state, target_state)
+
+
+def test_variable_backpropagation():
+    """Check that backpropagation works when using `tf.Variable` parameters."""
+    import tensorflow as tf
+    from qibo.config import DTYPE
+    theta = tf.Variable(0.1234, dtype=DTYPE)
+
+    # TODO: Fix parametrized gates so that `Circuit` can be defined outside
+    # of the gradient tape
+    with tf.GradientTape() as tape:
+        c = Circuit(1)
+        c.add(gates.X(0))
+        c.add(gates.RZ(0, theta).with_backend("MatmulEinsum"))
+        loss = tf.math.real(c()[-1])
+    grad = tape.gradient(loss, theta)
+
+    target_loss = np.cos(np.pi * theta.numpy())
+    np.testing.assert_allclose(loss.numpy(), target_loss)
+
+    target_grad = - np.pi * np.sin(np.pi * theta.numpy())
+    np.testing.assert_allclose(grad.numpy(), target_grad)
+
+
+def test_two_variables_backpropagation():
+    """Check that backpropagation works when using `tf.Variable` parameters."""
+    import tensorflow as tf
+    from qibo.config import DTYPE
+    theta = tf.Variable([0.1234, 0.4321], dtype=DTYPE)
+
+    # TODO: Fix parametrized gates so that `Circuit` can be defined outside
+    # of the gradient tape
+    with tf.GradientTape() as tape:
+        c = Circuit(2)
+        c.add(gates.RX(0, theta[0]).with_backend("MatmulEinsum"))
+        c.add(gates.RY(1, theta[1]).with_backend("MatmulEinsum"))
+        loss = tf.math.real(c()[0])
+    grad = tape.gradient(loss, theta)
+
+    t = np.pi * np.array([0.1234, 0.4321]) / 2.0
+    target_loss = np.cos(t.sum()) * np.cos(t[0]) * np.cos(t[1])
+    np.testing.assert_allclose(loss.numpy(), target_loss)
+
+    target_grad1 = - (np.sin(t.sum()) * np.cos(t[0]) * np.cos(t[1]) +
+                      np.cos(t.sum()) * np.sin(t[0]) * np.cos(t[1]))
+    target_grad2 = - (np.sin(t.sum()) * np.cos(t[0]) * np.cos(t[1]) +
+                      np.cos(t.sum()) * np.cos(t[0]) * np.sin(t[1]))
+    target_grad = np.pi * np.array([target_grad1, target_grad2]) / 2.0
+    np.testing.assert_allclose(grad.numpy(), target_grad)
