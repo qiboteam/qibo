Fix convention in rotation gates

src/qibo/tensorflow/gates.py
@@ -209,11 +209,9 @@ class RX(TensorflowGate, base_gates.RX):
     def __init__(self, q, theta):
         base_gates.RX.__init__(self, q, theta)
 
-        theta = tf.cast(self.theta, dtype=self.dtype)
-        phase = tf.exp(1j * np.pi * theta / 2.0)
-        cos = tf.cast(tf.math.real(phase), dtype=self.dtype)
-        sin = tf.cast(tf.math.imag(phase), dtype=self.dtype)
-        self.matrix = phase * (cos * matrices.I - 1j * sin * matrices.X)
+        cos = tf.cast(tf.cos(self.theta / 2.0), dtype=self.dtype)
+        sin = tf.cast(tf.sin(self.theta / 2.0), dtype=self.dtype)
+        self.matrix = cos * matrices.I - 1j * sin * matrices.X
 
 
 class RY(TensorflowGate, base_gates.RY):
@@ -221,11 +219,9 @@ class RY(TensorflowGate, base_gates.RY):
     def __init__(self, q, theta):
         base_gates.RY.__init__(self, q, theta)
 
-        theta = tf.cast(self.theta, dtype=self.dtype)
-        phase = tf.exp(1j * np.pi * theta / 2.0)
-        cos = tf.cast(tf.math.real(phase), dtype=self.dtype)
-        sin = tf.cast(tf.math.imag(phase), dtype=self.dtype)
-        self.matrix = phase * (cos * matrices.I - 1j * sin * matrices.Y)
+        cos = tf.cast(tf.cos(self.theta / 2.0), dtype=self.dtype)
+        sin = tf.cast(tf.sin(self.theta / 2.0), dtype=self.dtype)
+        self.matrix = cos * matrices.I - 1j * sin * matrices.Y
 
 
 class RZ(TensorflowGate, base_gates.RZ):
@@ -233,13 +229,13 @@ class RZ(TensorflowGate, base_gates.RZ):
     def __init__(self, q, theta):
         base_gates.RZ.__init__(self, q, theta)
 
-        theta = tf.cast(self.theta, dtype=self.dtype)
-        phase = tf.exp(1j * np.pi * theta)
-        rz = tf.eye(2, dtype=self.dtype)
-        self.matrix = tf.tensor_scatter_nd_update(rz, [[1, 1]], [phase])
+        th = tf.cast(self.theta, dtype=self.dtype)
+        phase = tf.exp(1j * th / 2.0)[tf.newaxis]
+        diag = tf.concat([tf.math.conj(phase), phase], axis=0)
+        self.matrix = tf.linalg.diag(diag)
 
     def controlled_by(self, *q):
-        """Fall back to CRZ if control is one."""
+        """Fall back to CRZ for one control qubit."""
         gate = super(RZ, self).controlled_by(*q)
         if len(q) == 1:
             return CRZ(q[0], self.target_qubits[0], self.theta)
@@ -258,10 +254,11 @@ class CRZ(TensorflowGate, base_gates.CRZ):
     def __init__(self, q0, q1, theta):
         base_gates.CRZ.__init__(self, q0, q1, theta)
 
-        theta = tf.cast(self.theta, dtype=self.dtype)
-        phase = tf.exp(1j * np.pi * theta)
-        crz = tf.eye(4, dtype=self.dtype)
-        crz = tf.tensor_scatter_nd_update(crz, [[3, 3]], [phase])
+        th = tf.cast(self.theta, dtype=self.dtype)
+        phase = tf.exp(1j * th / 2.0)[tf.newaxis]
+        diag = tf.concat([tf.ones(2, dtype=self.dtype),
+                          tf.math.conj(phase), phase], axis=0)
+        crz = tf.linalg.diag(diag)
         self.matrix = tf.reshape(crz, 4 * (2,))
 
 