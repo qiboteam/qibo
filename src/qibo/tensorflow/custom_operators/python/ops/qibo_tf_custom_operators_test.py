a602adf640019bcd5caf6b0e2fd51f2174aee31c
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-05-15 23:07:09 +0200

    updating custom operator test to work with pytest

--- src/qibo/tensorflow/custom_operators/python/ops/qibo_tf_custom_operators_test.py
+++ src/qibo/tensorflow/custom_operators/python/ops/qibo_tf_custom_operators_test.py
@@ -1,20 +1,15 @@
-"""Tests for initial_state ops."""
+"""
+Testing Tensorflow custom operators circuit.
+"""
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.platform import test
-from qibo.tensorflow.custom_operators.python.ops.qibo_tf_custom_operators import initial_state
+from qibo.tensorflow.custom_operators import initial_state
 
 
-class InitialStateTest(test.TestCase):
-
-  def testInitialState(self):
-    with self.test_session():
-      a = tf.zeros(10, dtype=tf.complex128)
-      initial_state(a)
-      b = np.zeros(10, dtype=np.complex128)
-      b[0] = 1
-      self.assertAllClose(a, b)
-
-
-if __name__ == '__main__':
-  test.main()
+def test_initial_state():
+  """Check that initial_state updates first element properly."""
+  final_state = tf.zeros(10, dtype=tf.complex128)
+  initial_state(final_state)
+  exact_state = np.zeros(10, dtype=np.complex128)
+  exact_state[0] = 1
+  np.testing.assert_allclose(final_state, exact_state)
