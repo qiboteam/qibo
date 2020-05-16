d5a9853768c9df1fd6e11a4a17afd5324c7063dd
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-05-16 14:30:15 +0200

    extending custom op test to both types

--- src/qibo/tests/test_custom_operators.py
+++ src/qibo/tests/test_custom_operators.py
@@ -1,15 +1,16 @@
 """
 Testing Tensorflow custom operators circuit.
 """
+import pytest
 import numpy as np
 import tensorflow as tf
 from qibo.tensorflow.custom_operators import initial_state
 
 
-def test_initial_state():
+@pytest.mark.parametrize("dtype", [np.complex64, np.complex128])
+def test_initial_state(dtype):
   """Check that initial_state updates first element properly."""
-  final_state = tf.zeros(10, dtype=tf.complex128)
+  final_state = tf.zeros(10, dtype=dtype)
   initial_state(final_state)
-  exact_state = np.zeros(10, dtype=np.complex128)
-  exact_state[0] = 1
+  exact_state = np.array([1] + [0]*9, dtype=dtype)
   np.testing.assert_allclose(final_state, exact_state)
