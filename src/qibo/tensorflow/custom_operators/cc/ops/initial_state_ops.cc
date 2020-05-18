be32f2704b64891f6f3579e12b67051693c2be79
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-05-18 09:51:41 +0200

    updating operator shape attribute

--- src/qibo/tensorflow/custom_operators/cc/ops/initial_state_ops.cc
+++ src/qibo/tensorflow/custom_operators/cc/ops/initial_state_ops.cc
@@ -1,8 +1,13 @@
 #include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
 
 using namespace tensorflow;
 
 REGISTER_OP("InitialState")
-      .Attr("T: {complex64, complex128}")
-      .Input("in: T")
-      .Output("out: T");
+    .Attr("T: {complex64, complex128}")
+    .Input("in: T")
+    .Output("out: T")
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+      c->set_output(0, c->input(0));
+      return Status::OK();
+    });
