11cfe451b648e9eef3313d2a7791136db86dd057
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-05-20 21:50:14 +0200

    registering operator signature

---
+++ src/qibo/tensorflow/custom_operators/cc/ops/apply_gate_ops.cc
@@ -0,0 +1,16 @@
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
+
+using namespace tensorflow;
+
+REGISTER_OP("ApplyGate")
+    .Attr("T: {complex64, complex128}")
+    .Input("state: T")
+    .Input("gate: T")
+    .Input("nqubits: int32")
+    .Input("target: int32")
+    .Output("out: T")
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+      c->set_output(0, c->input(0));
+      return Status::OK();
+    });
