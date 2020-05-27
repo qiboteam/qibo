35bb8ee2c93a984552a5df432fb2b01bc8f8edc7
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-05-27 18:07:40 +0300

    Move backprop tests to different file

--- src/qibo/tests/test_gates.py
+++ src/qibo/tests/test_gates.py
@@ -512,8 +512,8 @@ def test_final_state_property(gates):
     np.testing.assert_allclose(final_state, target_state)
 
 
-@pytest.mark.skip
-def test_variable_theta():
+@pytest.mark.parametrize("gates", _GATES)
+def test_variable_theta(gates):
     """Check that parametrized gates accept `tf.Variable` parameters."""
     import tensorflow as tf
     from qibo.config import DTYPE
@@ -531,55 +531,6 @@ def test_variable_theta():
     target_state = c().numpy()
 
     np.testing.assert_allclose(final_state, target_state)
-
-
-@pytest.mark.skip
-def test_variable_backpropagation():
-    """Check that backpropagation works when using `tf.Variable` parameters."""
-    import tensorflow as tf
-    from qibo.config import DTYPE
-    theta = tf.Variable(0.1234, dtype=DTYPE)
-
-    # TODO: Fix parametrized gates so that `Circuit` can be defined outside
-    # of the gradient tape
-    with tf.GradientTape() as tape:
-        c = Circuit(1)
-        c.add(gates.X(0))
-        c.add(gates.RZ(0, theta).with_backend("MatmulEinsum"))
-        loss = tf.math.real(c()[-1])
-    grad = tape.gradient(loss, theta)
-
-    target_loss = np.cos(theta.numpy() / 2.0)
-    np.testing.assert_allclose(loss.numpy(), target_loss)
-
-    target_grad = - np.sin(theta.numpy() / 2.0) / 2.0
-    np.testing.assert_allclose(grad.numpy(), target_grad)
-
-
-@pytest.mark.skip
-def test_two_variables_backpropagation():
-    """Check that backpropagation works when using `tf.Variable` parameters."""
-    import tensorflow as tf
-    from qibo.config import DTYPE
-    theta = tf.Variable([0.1234, 0.4321], dtype=DTYPE)
-
-    # TODO: Fix parametrized gates so that `Circuit` can be defined outside
-    # of the gradient tape
-    with tf.GradientTape() as tape:
-        c = Circuit(2)
-        c.add(gates.RX(0, theta[0]).with_backend("MatmulEinsum"))
-        c.add(gates.RY(1, theta[1]).with_backend("MatmulEinsum"))
-        loss = tf.math.real(c()[0])
-    grad = tape.gradient(loss, theta)
-
-    t = np.array([0.1234, 0.4321]) / 2.0
-    target_loss = np.cos(t[0]) * np.cos(t[1])
-    np.testing.assert_allclose(loss.numpy(), target_loss)
-
-    target_grad1 = - np.sin(t[0]) * np.cos(t[1])
-    target_grad2 = - np.cos(t[0]) * np.sin(t[1])
-    target_grad = np.array([target_grad1, target_grad2]) / 2.0
-    np.testing.assert_allclose(grad.numpy(), target_grad)
 
 
 @pytest.mark.parametrize("gates", _GATES)
