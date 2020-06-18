5208545f1eb98cfb3b4f24f8ab472e5201684d42
AdrianPerezSalinas <adrian.perezsalinas95@gmail.com>
2020-06-18 17:39:22 +0200

    added folder

--- examples/reuploading_classifier/main.py
+++ examples/reuploading_classifier/main.py
@@ -3,5 +3,7 @@ from qlassifier import single_qubit_classifier
 
 ql = single_qubit_classifier('circle', 1)
 result, parameters = ql.minimize(method='l-bfgs-b', options={'disp':True}, compile=True)
-ql.set_params(parameters)
-print(ql.params)
+ql.set_parameters(parameters)
+labels = ql.eval_test_set_fidelity()
+for x, y in zip(ql.test_set[0], labels):
+    print(x, y)
