8240acead18957259a424b775142591009ebbefd
AdrianPerezSalinas <adrian.perezsalinas95@gmail.com>
2020-06-18 17:35:57 +0200

    added folder

--- examples/reuploading_classifier/main.py
+++ examples/reuploading_classifier/main.py
@@ -3,3 +3,5 @@ from qlassifier import single_qubit_classifier
 
 ql = single_qubit_classifier('circle', 1)
 result, parameters = ql.minimize(method='l-bfgs-b', options={'disp':True}, compile=True)
+ql.set_params(parameters)
+print(ql.params)
