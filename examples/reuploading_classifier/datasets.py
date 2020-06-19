3f86dcd9eb735be9312a25efb01445717c0008c3
Adrián Pérez Salinas <adrian.perez@bsc.es>
2020-06-19 11:57:55 +0200

    Done up to figures

--- examples/reuploading_classifier/datasets.py
+++ examples/reuploading_classifier/datasets.py
@@ -1,5 +1,6 @@
 import numpy as np
 from itertools import product
+import matplotlib.pyplot as plt
 
 
 def create_dataset(name, grid = None, samples = 1000, seed=0):
@@ -42,16 +43,115 @@ def create_target(name):
 
     return targets
 
+def fig_template(name):
+    fig, axs = plt.subplots(ncols=2, figsize=(9.5, 4))
+    if name == 'circle':
+        for ax in axs:
+            circle = plt.Circle((0, 0), np.sqrt(2 / np.pi), color='black', fill=False, zorder=10)
+            ax.add_artist(circle)
+
+    elif name == '3 circles':
+        centers = np.array([[-1, 1], [1, 0], [-.5, -.5]])
+        radii = np.array([1, np.sqrt(6 / np.pi - 1), 1 / 2])
+        for (c, r) in zip(centers, radii):
+            for ax in axs:
+                circle = plt.Circle(c, r, color='black', fill=False, zorder=10)
+                ax.add_artist(circle)
+
+    elif name == 'square':
+        p = .5 * np.sqrt(2)
+        for ax in axs:
+            ax.plot([-p, p, p, -p], [-p, -p, p, p], color='black', zorder=10)
+
+    elif name == '4 squares':
+        for ax in axs:
+            ax.plot([0, 0], [-1, 1], color='black', zorder=10)
+            ax.plot([-1, 1], [0, 0], color='black', zorder=10)
+
+    elif name == 'crown' or name == 'tricrown':
+        centers = [[0, 0], [0, 0]]
+        radii = [np.sqrt(.8), np.sqrt(.8 - 2 / np.pi)]
+        for (c, r) in zip(centers, radii):
+            for ax in axs:
+                circle = plt.Circle(c, r, color='black', fill=False, zorder=10)
+                ax.add_artist(circle)
+
+    elif name == 'wavy lines':
+        freq = 1
+
+        def fun1(s):
+            return s + np.sin(freq * np.pi * s)
+
+        def fun2(s):
+            return -s + np.sin(freq * np.pi * s)
+        x = np.linspace(-1, 1)
+        for ax in axs:
+            ax.plot(x, fun1(x), color='black', zorder=10)
+            ax.plot(x, fun2(x), color='black', zorder=10)
+
+    axs[0].set(xlabel=r'$x_0$', ylabel=r'$x_1$', xlim=[-1,1], ylim=[-1,1])
+    axs[0].axis('equal')
+    axs[1].set(xlabel=r'$x_0$', xlim=[-1, 1], ylim=[-1, 1])
+    axs[1].axis('equal')
+
+    return fig, axs
+
+def world_map_template():
+    fig, ax = plt.subplots(figsize = (20, 10))
+    ax.plot(laea_x(np.pi, np.linspace(-np.pi / 2, np.pi / 2)), laea_y(np.pi, np.linspace(-np.pi / 2, np.pi / 2)), color='k', zorder=10)
+    ax.plot(laea_x(-np.pi, np.linspace(-np.pi / 2, np.pi / 2)), laea_y(-np.pi, np.linspace(-np.pi / 2, np.pi / 2)),
+            color='k', zorder=10)
+    ax.plot(laea_x(np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)), laea_y(np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)),
+           color='k', zorder=10)
+    ax.plot(laea_x(-np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)),
+            laea_y(-np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)),
+            color='k', zorder=10)
+    ax.plot(laea_x(2*np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)),
+            laea_y(2*np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)),
+            color='k', zorder=10)
+    ax.plot(laea_x(-2*np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)),
+            laea_y(-2*np.pi / 3, np.linspace(-np.pi / 2, np.pi / 2)),
+            color='k', zorder=10)
+    ax.plot(laea_x(0, np.linspace(-np.pi / 2, np.pi / 2)),
+            laea_y(0, np.linspace(-np.pi / 2, np.pi / 2)),
+            color='k', zorder=10)
+    ax.plot(laea_x(np.linspace(-np.pi, np.pi), 0),
+            laea_y(np.linspace(-np.pi, np.pi), 0),
+            color='k', zorder=10)
+    ax.plot(laea_x(np.linspace(-np.pi, np.pi), np.pi / 6),
+            laea_y(np.linspace(-np.pi, np.pi), np.pi / 6),
+            color='k', zorder=10)
+    ax.plot(laea_x(np.linspace(-np.pi, np.pi), -np.pi / 6),
+            laea_y(np.linspace(-np.pi, np.pi), -np.pi / 6),
+            color='k', zorder=10)
+    ax.plot(laea_x(np.linspace(-np.pi, np.pi), np.pi / 3),
+            laea_y(np.linspace(-np.pi, np.pi), np.pi / 3),
+            color='k', zorder=10)
+    ax.plot(laea_x(np.linspace(-np.pi, np.pi), -np.pi / 3),
+            laea_y(np.linspace(-np.pi, np.pi), -np.pi / 3),
+            color='k', zorder=10)
+    ax.text(0, 1.47, r'$|0\rangle$', fontsize=20)
+    ax.text(0, -1.53, r'$|1\rangle$', fontsize=20)
+    ax.text(0.05, 0.05, r'$|+\rangle$', fontsize=20)
+    ax.text(2.9, 0, r'$|-\rangle$', fontsize=20)
+    ax.text(-3.2, 0, r'$|-\rangle$', fontsize=20)
+    #ax.plot(laea_x(-np.pi, np.linspace(0, -np.pi)), laea_y(-np.pi, np.linspace(0, -np.pi)), color='k', zorder=10)
+    #ax.plot(laea_x(np.pi/3, np.linspace(0, np.pi)), laea_y(np.pi/3, np.linspace(0, np.pi)), color='k', zorder=10)
+    #ax.plot(laea_x(-np.pi/3, np.linspace(0, -np.pi)), laea_y(-np.pi/3, np.linspace(0, -np.pi)), color='k', zorder=10)
+    #ax.plot(laea_x(2*np.pi / 3, np.linspace(0, np.pi)), laea_y(2*np.pi / 3, np.linspace(0, np.pi)), color='k', zorder=10)
+    #ax.plot(laea_x(-2*np.pi / 3, np.linspace(0, -np.pi)), laea_y(-2*np.pi / 3, np.linspace(0, -np.pi)), color='k',
+            #zorder=10)
+
+    return fig, ax
+
+
+def laea_x(lamb, phi):
+    return 2 * np.sqrt(2) * np.cos(phi) * np.sin(lamb / 2) / np.sqrt(1 + np.cos(phi) * np.cos(lamb / 2))
+
+def laea_y(lamb, phi):
+    return np.sqrt(2) * np.sin(phi) / np.sqrt(1 + np.cos(phi) * np.cos(lamb / 2))
+
 def _circle(points):
-    labels = [0] * len(points)
-    for i, p in enumerate(points):
-        if np.linalg.norm(p) > np.sqrt(2 / np.pi):
-            labels[i] = 1
-
-    return points, labels
-
-
-def _3circles(points):
     labels = [0] * len(points)
     for i, p in enumerate(points):
         if np.linalg.norm(p) > np.sqrt(2 / np.pi):
--- examples/reuploading_classifier/main.py
+++ examples/reuploading_classifier/main.py
@@ -1,9 +1,22 @@
 import datasets as ds
+import numpy as np
 from qlassifier import single_qubit_classifier
 
-ql = single_qubit_classifier('circle', 1)
-result, parameters = ql.minimize(method='l-bfgs-b', options={'disp':True}, compile=True)
-ql.set_parameters(parameters)
+ql = single_qubit_classifier('tricrown', 5)
+'''result, parameters = ql.minimize(method='l-bfgs-b', options={'disp':True}, compile=True, fidelity=True) # minimizador no va muy allá
+print(parameters)'''
+
+params_tricrown = [ 1.71505633,  1.18816839, -0.41820971, -1.17124703, -1.96595341,  1.67279085,
+ -2.67943218,  1.75290379,  0.95497698,  1.16126111, -1.48343009, -0.5020429,
+  1.13796601, -2.61653729,  0.91361391, -1.64368751,  2.52871188, -1.0190579,
+  0.09472308,  1.72974633]
+'''params_circle = [ -1.51413155   1.68075594  -3.53392853   1.49516143  -1.35997827
+   1.41132915  -9.14847509   3.10432847   0.13614464  -3.34835193
+ -12.54425487  -0.05034364   1.36279703  -1.51664904   3.13465347
+   1.50204086   1.49518814  -1.65829912  -0.403254     0.94934493]'''
+#parameters = np.array([-9.22309909e-07,  1.82987902e+00, -1.65534402e+00,  1.53644454e-01])
+#parameters = 20 * np.pi * (.5 - np.random.rand(4))
+ql.set_parameters(params_tricrown)
 labels = ql.eval_test_set_fidelity()
-for x, y in zip(ql.test_set[0], labels):
-    print(x, y)
+# ql.paint_results()
+ql.paint_world_map()
