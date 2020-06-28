5d797f120ec01d592502e95aa09b05aff757445f
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-06-28 23:34:26 +0300

    Add fusion tests

---
+++ src/qibo/tensorflow/fusion.py
@@ -0,0 +1,139 @@
+from qibo.base import gates
+from typing import List, Set
+
+
+class FusionGroup:
+
+    def __init__(self):
+        self.qubit0 = None
+        self.qubit1 = None
+        self.gates0 = [[]]
+        self.gates1 = [[]]
+        self.two_qubit_gates = [] # list of tuples (gate, revert flag)
+
+        self.special_gate = None
+
+    @property
+    def qubits(self) -> Set[int]:
+        if self.qubit0 is None:
+            return {}
+        if self.qubit1 is None:
+            return {self.qubit0}
+        return {self.qubit0, self.qubit1}
+
+    def add(self, gate: gates.Gate):
+        if self.special_gate is not None:
+            raise ValueError("Cannot add gate on special fusion group.")
+        if not gate.qubits:
+            if self.qubits:
+                raise ValueError("Cannot add special gate on fusion group with "
+                                 "qubits already set.")
+            self.special_gate = None
+        elif len(gate.qubits) == 1:
+            self._add_one_qubit_gate(gate)
+        elif len(gate.qubits) == 2:
+            self._add_two_qubit_gate(gate)
+        else:
+            raise ValueError("Cannot add gate acting on {} qubits in fusion "
+                             "group.".format(len(gate.qubits)))
+
+    def _add_one_qubit_gate(self, gate: gates.Gate):
+        qubit = gate.qubits[0]
+        if self.qubit0 is None or self.qubit0 == qubit:
+            self.qubit0 = qubit
+            self.gates0[-1].append(gate)
+        elif self.qubit1 is None or self.qubit1 == qubit:
+            self.qubit1 = qubit
+            self.gates1[-1].append(gate)
+        else: # pragma: no cover
+            raise ValueError("Cannot add gate on qubit {} in fusion group of "
+                             "qubits {} and {}."
+                             "".format(qubit, self.qubit0, self.qubit1))
+
+    def _add_two_qubit_gate(self, gate: gates.Gate):
+        qubit0, qubit1 = gate.qubits
+        if self.qubit0 is None:
+            self.qubit0, self.qubit1 = qubit0, qubit1
+            self.two_qubit_gates.append((gate, False))
+        elif self.qubit1 is None:
+            if self.qubit0 == qubit0:
+                self.qubit1 = qubit1
+                self.two_qubit_gates.append((gate, False))
+            elif self.qubit0 == qubit1:
+                self.qubit1 = qubit0
+                self.two_qubit_gates.append((gate, True))
+            else: # pragma: no cover
+                raise ValueError("Cannot add gate on qubits {} and {} in "
+                                 "fusion group of qubit {}."
+                                 "".format(qubit0, qubit1, self.qubit0))
+        else:
+            if self.qubits != {qubit0, qubit1}: # pragma: no cover
+                raise ValueError("Cannot add gate on qubits {} and {} in "
+                                 "fusion group of qubits {} and {}."
+                                 "".format(qubit0, qubit1, self.qubit0, self.qubit1))
+            self.two_qubit_gates.append((gate, self.qubit1 == qubit0))
+
+        self.gates0.append([])
+        self.gates1.append([])
+
+
+def fuse_queue_recursive(remaining_queue: List[gates.Gate],
+                         group_queue: List[FusionGroup] = []
+                         ) -> List[FusionGroup]:
+    if not remaining_queue:
+        return group_queue
+
+    new_remaining_queue = []
+    gates = iter(remaining_queue)
+    new_group = FusionGroup()
+    new_group.add(next(gates))
+    for gate in gates:
+        commutes = True
+        for blocking_gate in new_remaining_queue:
+            commutes = commutes and gate.commutes(blocking_gate)
+            if not commutes:
+                break
+
+        if commutes:
+            try:
+                new_group.add(gate)
+            except ValueError:
+                new_remaining_queue.append(gate)
+        else:
+            new_remaining_queue.append(gate)
+
+    group_queue.append(new_group)
+    return fuse_queue_recursive(new_remaining_queue, group_queue)
+
+
+def fuse_queue(queue: List[gates.Gate]) -> List[FusionGroup]:
+    group_queue = []
+    remaining_queue = list(queue)
+    while remaining_queue:
+        new_remaining_queue = []
+        gates = iter(remaining_queue)
+        new_group = FusionGroup()
+        new_group.add(next(gates))
+        for gate in gates:
+            commutes = True
+            for blocking_gate in new_remaining_queue:
+                commutes = commutes and gate.commutes(blocking_gate)
+                if not commutes:
+                    break
+
+            if commutes:
+                try:
+                    new_group.add(gate)
+                except ValueError:
+                    new_remaining_queue.append(gate)
+            else:
+                new_remaining_queue.append(gate)
+
+        group_queue.append(new_group)
+        remaining_queue = list(new_remaining_queue)
+
+        #print(group_queue)
+        print(remaining_queue)
+        print()
+
+    return group_queue
---
+++ src/qibo/tests/test_fusion.py
@@ -0,0 +1,53 @@
+"""
+Test functions for gate fusion.
+"""
+import numpy as np
+import pytest
+from qibo.models import Circuit
+from qibo import gates
+from qibo.tensorflow import fusion
+
+
+def test_fuse_queue_single_group():
+    queue = [gates.H(0), gates.X(1), gates.CZ(0, 1)]
+    fused_groups = fusion.fuse_queue(queue)
+    assert len(fused_groups) == 1
+    group = fused_groups[0]
+    assert group.gates0 == [[queue[0]], []]
+    assert group.gates1 == [[queue[1]], []]
+    assert group.two_qubit_gates == [(queue[2], False)]
+
+
+def test_fuse_queue_two_groups():
+    queue = [gates.X(0), gates.H(1), gates.CNOT(1, 2), gates.H(2), gates.Y(1),
+             gates.H(0)]
+    fused_groups = fusion.fuse_queue(queue)
+    assert len(fused_groups) == 2
+    group1, group2 = fused_groups
+    assert group1.gates0 == [[queue[0], queue[5]]]
+    assert group1.gates1 == [[queue[1]]]
+    assert group1.two_qubit_gates == []
+    assert group2.gates0 == [[], [queue[4]]]
+    assert group2.gates1 == [[], [queue[3]]]
+    assert group2.two_qubit_gates == [(queue[2], False)]
+
+
+def test_fuse_queue_variational_layer(nqubits=6):
+    theta = np.pi * np.random.random((2, nqubits))
+    queue0 = [gates.RY(i, theta[0, i]) for i in range(nqubits)]
+    queue1 = [gates.CZ(i, i + 1) for i in range(0, nqubits - 1, 2)]
+    queue2 = [gates.RY(i, theta[1, i]) for i in range(nqubits)]
+    queue3 = [gates.CZ(i, i + 1) for i in range(1, nqubits - 2, 2)]
+    queue3.append(gates.CZ(0, nqubits - 1))
+
+    fused_groups = fusion.fuse_queue(queue0 + queue1 + queue2 + queue3)
+    assert len(fused_groups) == 2 * (nqubits // 2)
+    for i, group in enumerate(fused_groups[:nqubits // 2]):
+        assert group.gates0 == [[queue0[2 * i]], [queue2[2 * i]]]
+        assert group.gates1 == [[queue0[2 * i + 1]], [queue2[2 * i + 1]]]
+        assert group.two_qubit_gates == [(queue1[i], False)]
+    # FIXME: Fix the ``nqubits`` odd case
+    for i, group in enumerate(fused_groups[nqubits // 2:]):
+        assert group.gates0 == [[], []]
+        assert group.gates1 == [[], []]
+        assert group.two_qubit_gates == [(queue3[i], False)]
