ac0c27e2bde9c4f33181d55ed112c3e0fa76826d
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-06-28 18:00:31 +0200

    fixing coverage

--- src/qibo/tests/conftest.py
+++ src/qibo/tests/conftest.py
@@ -6,12 +6,15 @@ Pytest fixtures.
 import sys
 import pytest
 
+
 def pytest_runtest_setup(item):
     ALL = {"darwin", "linux"}
-    supported_platforms = ALL.intersection(mark.name for mark in item.iter_markers())
+    supported_platforms = ALL.intersection(
+        mark.name for mark in item.iter_markers())
     plat = sys.platform
-    if supported_platforms and plat not in supported_platforms:
+    if supported_platforms and plat not in supported_platforms:  # pragma: no cover
         pytest.skip("cannot run on platform {}".format(plat))
+
 
 def pytest_configure(config):
     config.addinivalue_line(
