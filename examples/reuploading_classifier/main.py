cd8e205be8157c87e882de29e87ae38cf8e92eaf
Adrián Pérez Salinas <adrian.perez@bsc.es>
2020-06-30 20:32:15 +0200

    Ready to run again

--- examples/reuploading_classifier/main.py
+++ examples/reuploading_classifier/main.py
@@ -7,7 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--name", default='wavy_lines', help="Name of the example", type=str)
-parser.add_argument("--layers", default=2, help="Number of layers.", type=int)
+parser.add_argument("--layers", default=10, help="Number of layers.", type=int)
 
 def main(name, layers):
     """Perform classification for a given problem and number of layers
@@ -21,12 +21,15 @@ def main(name, layers):
     ql = single_qubit_classifier(name, layers) # Define classifier
     with open('saved_parameters.pkl', 'rb') as f:
         data = pickle.load(f) # Load previous results. Have we ever run these problem?
+        print(data)
     try:
         parameters = data[name][layers]
         print('Problem solved before, obtaining parameters from file...')
     except:
         print('Problem never solved, finding optimal parameters...')
         result, parameters = ql.minimize(method='l-bfgs-b', options={'disp': True})
+        with open('saved_parameters.pkl', 'rb') as f:
+            data = pickle.load(f) # Load previous results. Have we ever run these problem?
         data[name][layers] = parameters
         with open('saved_parameters.pkl', 'wb') as f:
             pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
