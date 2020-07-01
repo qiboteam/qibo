1aadf6739a0f4bc6c3db01dc49b7349f0804d99b
igres26 <igres26@gmail.com>
2020-07-01 12:34:53 +0400

    added if __name__=... to main.py

--- examples/unary/main.py
+++ examples/unary/main.py
@@ -3,20 +3,12 @@ import aux_functions as aux
 import argparse
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--bins", default=16, type=int)
-args = vars(parser.parse_args())
-
-
 def main(data, bins, M, shots):
     S0, sig, r, T, K = data
-
     circuit, (values, pdf) = fun.load_quantum_sim(bins, S0, sig, r, T) # Create circuit to load the probability distribution
     prob_sim = fun.run_quantum_sim(bins, circuit, shots) # Measure the probability distribution
     fun.paint_prob_distribution(bins, prob_sim, S0, sig, r, T) # Paint the probability distribution
     print('Histogram printed for unary simulation with {} qubits.\n'.format(bins))
-
-
     circuit, S = fun.load_payoff_quantum_sim(bins, S0, sig, r, T, K) # Create circuit to compute the expected payoff
     qu_payoff_sim = fun.run_payoff_quantum_sim(bins, circuit, shots, S, K) # Run the circuit for expected payoff
     cl_payoff = aux.classical_payoff(S0, sig, r, T, K, samples=1000000) # Computing exact classical payoff
@@ -25,22 +17,23 @@ def main(data, bins, M, shots):
     print('Expected payoff from quantum simulation: {}\n'.format(qu_payoff_sim))
     print('Percentage error: {} %\n'.format(error))
     print('-'*60+'\n')
-
     a_s, error_s = fun.amplitude_estimation(bins, M, data) # Applying amplitude estimation
     print('Amplitude estimation with a total of {} runs.\n'.format(M))
     fun.paint_AE(a_s, error_s, bins, M, data) # Results of amplitude estimation
     print('Amplitude estimation results printed.')
 
 
-S0 = 2
-K = 1.9
-sig = 0.4
-r = 0.05
-T = 0.1
-data = (S0, sig, r, T, K)
-shots = 1000000
-bins = args.get('bins')
-max_m = 10
-
-
-main(data, bins, max_m, shots)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bins", default=16, type=int)
+    args = vars(parser.parse_args())
+    S0 = 2
+    K = 1.9
+    sig = 0.4
+    r = 0.05
+    T = 0.1
+    data = (S0, sig, r, T, K)
+    shots = 1000000
+    bins = args.get('bins')
+    max_m = 10
+    main(data, bins, max_m, shots)
