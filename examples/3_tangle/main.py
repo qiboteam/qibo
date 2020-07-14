0c7d897758eee84f2a7ea3b219d0d153ad7e3764
AdrianPerezSalinas <adrian.perezsalinas95@gmail.com>
2020-07-14 13:58:25 +0200

    Comments added

--- examples/3_tangle/main.py
+++ examples/3_tangle/main.py
@@ -15,14 +15,26 @@ def main(N, p, shots, post_selection):
     tangles = np.empty(N)
     opt_tangles = np.empty(N)
     if p != 0:
+        # backend is adapted to the error implementation
         from qibo import set_backend
         set_backend("matmuleinsum")
     for i in range(N):
+        """
+        For every seed from 0 to N, the steps to follow are
+        1) A random state with three qubits is created from the seed
+        2) The tangle of the recently created random state is computed exactly
+        3) Transformation to the up-to-phases canonical form is performed by applying local operations 
+                that drive some coefficients to zero
+        4) The tangle of the up-to-phases canonical form of the created state is measured from the outcomes of the state.
+                Post-selection can be applied at this stage
+        
+        Results are stored into variables to paint the results
+        """
         print('Initialized state with seed %s'%i)
-        state = create_random_state(i) # Create a random state from the seed
-        tangles[i] = compute_random_tangle(i) # Compute its tangle
-        fun, params = canonize(state, p=p, shots=shots) # Perform transformation
-        opt_tangles[i] = canonical_tangle(state, params, post_selection=post_selection) # Measuring the tangle with or without post selection
+        state = create_random_state(i)
+        tangles[i] = compute_random_tangle(i)
+        fun, params = canonize(state, p=p, shots=shots)
+        opt_tangles[i] = canonical_tangle(state, params, post_selection=post_selection)
 
     print('Painting results')
     fig, ax = plt.subplots() # Plotting
@@ -35,7 +47,6 @@ def main(N, p, shots, post_selection):
     ax.set(xlabel='Exact tangle', ylabel='Measured tangle', xlim=[0,1], ylim=[0,1])
     plt.grid('on')
     plt.show()
-
 
 
 if __name__ == "__main__":
