80f1742a785454327edd1d11b91ecdf00a396330
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-07-18 17:39:31 +0200

    fixing tests on gpu

--- src/qibo/tests/test_measurements.py
+++ src/qibo/tests/test_measurements.py
@@ -411,7 +411,7 @@ def test_probabilistic_measurement(accelerators):
     result = c(nshots=1000)
 
     # update reference values based on device
-    if tf.config.list_physical_devices("GPU") and not accelerators: # pragma: no cover
+    if tf.config.list_physical_devices("GPU"): # pragma: no cover
         decimal_freqs = {0: 273, 1: 233, 2: 242, 3: 252}
         binary_freqs = {"00": 273, "01": 233, "10": 242, "11": 252}
     else:
