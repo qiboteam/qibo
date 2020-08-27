8aaa40fe5c855a8d212582762cb20c7699e9b9e0
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-08-27 21:31:34 +0300

    Code simplifications

--- examples/adiabatic-3SAT/main.py
+++ examples/adiabatic-3SAT/main.py
@@ -25,7 +25,7 @@ def main(nqubits, instance, T, dt, solver, plot, trotter):
     control, solution, clauses = functions.read_file(nqubits, instance)
     nqubits = int(control[0])
     # Define "easy" and "problem" Hamiltonians
-    if trotter == True:
+    if trotter:
         print('Using Trotter decomposition for the Hamiltonian\n')
         parts0, parts1 = functions.trotter_dict(clauses)
         H0 = hamiltonians.TrotterHamiltonian(*parts0)
@@ -36,34 +36,39 @@ def main(nqubits, instance, T, dt, solver, plot, trotter):
         H0 = hamiltonians.Hamiltonian(nqubits, functions.h0(nqubits, t))
         H1 = hamiltonians.Hamiltonian(nqubits, functions.h_p(nqubits, clauses))
 
-    # Define evolution model and (optionally) callbacks
     print('-'*20+'\n')
-    initial_state = np.ones(2 ** nqubits) / np.sqrt(2 ** nqubits)
+    if plot and nqubits >= 14:
+        print('Currently not possible to calculate gap energy for {} qubits.'
+              '\n Proceeding to adiabatic evolution without plotting data.\n'
+              ''.format(nqubits))
+        plot = False
+
+    # Define evolution model and (optionally) callbacks
     s = lambda t: t
-    if plot == True:
-        if nqubits >= 14:
-            print('Currently not possible to calculate gap energy for {} qubits.\n Proceeding to adiabatic evolution without plotting data.\n'.format(nqubits))
-            evolve = models.AdiabaticEvolution(H0, H1, s, dt, solver=solver)
-        else:
-            ground = callbacks.Gap(0)
-            excited = callbacks.Gap(1)
-            gap = callbacks.Gap()
-            evolve = models.AdiabaticEvolution(H0, H1, s, dt, solver=solver, callbacks=[gap, ground, excited])
+    if plot:
+        ground = callbacks.Gap(0)
+        excited = callbacks.Gap(1)
+        gap = callbacks.Gap()
+        evolve = models.AdiabaticEvolution(H0, H1, s, dt, solver=solver,
+                                           callbacks=[gap, ground, excited])
     else:
         evolve = models.AdiabaticEvolution(H0, H1, s, dt, solver=solver)
 
     # Perform evolution
+    initial_state = np.ones(2 ** nqubits) / np.sqrt(2 ** nqubits)
     final_state = evolve(final_time=T, initial_state=initial_state)
-    max_output = "{0:0{bits}b}".format((np.abs(final_state.numpy())**2).argmax(), bits = nqubits)
+    output_dec = (np.abs(final_state.numpy())**2).argmax()
+    max_output = "{0:0{bits}b}".format(output_dec, bits = nqubits)
     max_prob = (np.abs(final_state.numpy())**2).max()
     print("Exact cover instance with {} qubits.\n".format(nqubits))
     if solution:
         print('Known solution: {}\n'.format(''.join(solution)))
     print('-'*20+'\n')
-    print('Adiabatic evolution with total time {T}, evolution step {dt} and solver {solver}.\n'.format(T=T, dt=dt, solver=solver))
-    print('Most common solution after adiabatic evolution: {}.\n'.format(max_output))
-    print('Found with probability: {}.\n'.format(max_prob))
-    if plot == True and  nqubits <= 14:
+    print(f'Adiabatic evolution with total time {T}, evolution step {dt} and '
+           'solver {solver}.\n')
+    print(f'Most common solution after adiabatic evolution: {max_output}.\n')
+    print(f'Found with probability: {maxprob}.\n')
+    if plot:
         print('-'*20+'\n')
         functions.plot(nqubits, ground[:], excited[:], gap[:], dt, T)
         print('Plots finished.\n')
