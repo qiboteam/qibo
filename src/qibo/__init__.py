f7921c15647776bf13b8dd84295ab2bc81d2409c
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-09-05 11:25:00 +0200

    updating version

--- src/qibo/__init__.py
+++ src/qibo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1b5"
+__version__ = "0.1.0"
 from qibo.config import set_precision, set_backend, set_device, get_backend, get_precision, get_device, matrices, K
 from qibo import callbacks
 from qibo import evolution
