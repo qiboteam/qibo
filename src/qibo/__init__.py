59295359b0a1e5aa4aafb5c6b9bf3dddb99209f6
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-09-21 00:05:01 +0400

    Change version to 0.1.1

--- src/qibo/__init__.py
+++ src/qibo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0-dev"
+__version__ = "0.1.1-dev"
 from qibo.config import set_precision, set_backend, set_device, get_backend, get_precision, get_device, matrices, K
 from qibo import callbacks
 from qibo import evolution
