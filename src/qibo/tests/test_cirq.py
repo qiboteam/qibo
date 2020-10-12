Fix depth calculation

src/qibo/tests/test_cirq.py
@@ -38,7 +38,8 @@ def execute_cirq(cirq_gates, nqubits, initial_state=None) -> np.ndarray:
     for gate, targets in cirq_gates:
         c.append(gate(*[q[i] for i in targets]))
     result = cirq.Simulator().simulate(c, initial_state=initial_state)
-    return result.final_state
+    depth = len(cirq.Circuit(c.all_operations()))
+    return result.final_state, depth - 1
 
 
 def assert_gates_equivalent(qibo_gate, cirq_gates, nqubits,
@@ -52,7 +53,8 @@ def assert_gates_equivalent(qibo_gate, cirq_gates, nqubits,
         atol: Absolute tolerance in state vector comparsion.
     """
     initial_state = utils.random_numpy_state(nqubits)
-    target_state = execute_cirq(cirq_gates, nqubits, np.copy(initial_state))
+    target_state, target_depth = execute_cirq(cirq_gates, nqubits,
+                                              np.copy(initial_state))
     accelerators = None if ndevices is None else {"/GPU:0": ndevices}
 
     if isinstance(qibo_gate, native_gates.TensorflowGate) and accelerators:
@@ -63,7 +65,8 @@ def assert_gates_equivalent(qibo_gate, cirq_gates, nqubits,
         c = models.Circuit(nqubits, accelerators)
         c.add(qibo_gate)
         final_state = c(np.copy(initial_state)).numpy()
-        np.testing.assert_allclose(target_state, final_state, atol=atol)
+        assert c.depth == target_depth
+        np.testing.assert_allclose(final_state, target_state, atol=atol)
 
 
 @pytest.mark.parametrize("backend", _BACKENDS)
@@ -243,6 +246,6 @@ def test_qft(backend, nqubits, accelerators):
     c = models.QFT(nqubits, accelerators=accelerators)
     final_state = c(np.copy(initial_state)).numpy()
     cirq_gates = [(cirq.QFT, list(range(nqubits)))]
-    target_state = execute_cirq(cirq_gates, nqubits, np.copy(initial_state))
+    target_state, _ = execute_cirq(cirq_gates, nqubits, np.copy(initial_state))
     np.testing.assert_allclose(target_state, final_state, atol=1e-6)
     qibo.set_backend(original_backend)