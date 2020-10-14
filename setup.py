178cd7f80080a47fd73e5ce5dfe521cb27fb950b
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-10-14 17:51:03 +0200

    adding extra package requirement

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     install_requires=requirements,
     extras_require={
         "docs": ["sphinx", "sphinx_rtd_theme", "recommonmark", "sphinxcontrib-bibtex", "sphinx_markdown_tables"],
-        "tests": ["cirq", "ply"],
+        "tests": ["cirq", "ply", "sklearn"],
     },
     python_requires=">=3.6.0",
     long_description=long_description,
