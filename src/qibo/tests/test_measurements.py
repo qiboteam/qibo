7f956e549fef7d47ee76fe3785d0951607219c3b
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-11-08 19:45:30 +0400

    Add asymmetric bitflip test

--- src/qibo/tests/test_measurements.py
+++ src/qibo/tests/test_measurements.py
@@ -620,6 +620,30 @@ def test_post_measurement_bitflips(probs):
     np.testing.assert_allclose(noisy_result.samples(), target_samples)
 
 
+def test_post_measurement_asymmetric_bitflips():
+    """Check applying asymmetric bitflips to measurement samples."""
+    import tensorflow as tf
+    from qibo.config import DTYPES
+    from qibo.tensorflow import measurements
+    qubits = tuple(range(4))
+    samples = np.random.randint(0, 2, (20, 4))
+    result = measurements.GateResult(qubits, binary_samples=samples)
+    p1_map = {0: 0.2, 1: 0.0, 2: 0.0, 3: 0.1}
+    tf.random.set_seed(123)
+    noisy_result = result.apply_bitflips(p0=0.2, p1=p1_map)
+
+    p0 = 0.2 * np.ones(4)
+    p1 = np.array([0.2, 0.0, 0.0, 0.1])
+    tf.random.set_seed(123)
+    sprobs = tf.random.uniform(samples.shape, dtype=DTYPES.get('DTYPE')).numpy()
+    target_samples = np.copy(samples).ravel()
+    ids = (np.where(target_samples == 0)[0], np.where(target_samples == 1)[0])
+    target_samples[ids[0]] = samples.ravel()[ids[0]] + (sprobs < p0).ravel()[ids[0]]
+    target_samples[ids[1]] = samples.ravel()[ids[1]] - (sprobs < p1).ravel()[ids[1]]
+    target_samples = target_samples.reshape(samples.shape)
+    np.testing.assert_allclose(noisy_result.samples(), target_samples)
+
+
 @pytest.mark.parametrize("accelerators", _ACCELERATORS)
 @pytest.mark.parametrize("probs,target",
                          [([0.0, 0.0, 0.0], {5:30}),
@@ -674,6 +698,9 @@ def test_post_measurement_bitflip_errors():
     # Passing wrong bitflip error map type
     with pytest.raises(TypeError):
         noisy_result = result.apply_bitflips("test")
+    # Passing negative bitflip probability
+    with pytest.raises(ValueError):
+        noisy_result = result.apply_bitflips(-0.4)
     # Check bitflip error map errors when creating measurement gate
     gate = gates.M(0, 1, p0=2 * [0.1])
     with pytest.raises(ValueError):
