d20448c1704b6337f0cf2b246229643318d42b3e
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2020-12-03 15:45:43 +0400

    Create tomography class

---
+++ tomography/tmain.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import argparse
+import json
+import utils
+import numpy as np
+import tomography
+
+filename = ["tomo_181120-00.json",
+            "tomo_181120-01.json",
+            "tomo_181120-10.json",
+            "tomo_181120-11.json",
+            "tomo_181120-bell.json",
+            "tomo_181120-hadamard-tunable.json",
+            "tomo_181120-hadamard-fixed.json",
+            "tomo_181120-bell_beta.json"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--index", default=0, type=int)
+parser.add_argument("--plot", action="store_true")
+
+
+def extract_data(filename):
+    with open(filename,"r") as r:
+        r = r.read()
+        raw = json.loads(r)
+    return {k: np.array(v) for k, v in raw.items()}
+
+
+def main(index, plot):
+    # Extract state data and define ``gate``
+    states = extract_data("data/states_181120.json")
+    states = {k: np.sqrt(v[0] ** 2 + v[1] ** 2) for k, v in states.items()}
+
+    # Extract tomography amplitudes
+    data = extract_data("data/" + filename[index])
+    amp = np.sqrt([v[0] ** 2 + v[1] ** 2 for v in data.values()])
+
+    # Create tomography object
+    tom = tomography.Tomography(states, amp)
+    tom.minimize()
+
+    rho_theory = utils.matrices.rho_theory(index)
+    fidelity = tom.fidelity(rho_theory)
+    print("Convergence:", tom.success)
+    print("Fidelity:", fidelity)
+
+    if plot:
+        from plot import plot
+        plot(rho_theory, tom.linear, tom.fit, fidelity, tom.success)
+
+
+if __name__ == "__main__":
+    args = vars(parser.parse_args())
+    main(**args)
---
+++ tomography/tomography.py
@@ -0,0 +1,192 @@
+import numpy as np
+from scipy.linalg import solve, sqrtm
+from scipy.optimize import minimize
+
+
+class Cholesky:
+
+    def __init__(self, matrix=None, vector=None):
+        self._matrix = matrix
+        self._vector = vector
+
+    @classmethod
+    def from_matrix(cls, x):
+        return cls(matrix=x)
+
+    @classmethod
+    def from_vector(cls, x):
+        return cls(vector=x)
+
+    @classmethod
+    def decompose(cls, inmatrix):
+        T_linear = np.zeros((4, 4), dtype=complex)
+        det_linear = np.linalg.det(inmatrix)
+        M11 = np.copy(inmatrix)
+        M11 = np.delete(M11, 0, 0)
+        M11 = np.delete(M11, 0, 1)
+        M11 = np.linalg.det(M11)
+        M12 = np.copy(inmatrix)
+        M12 = np.delete(M12, 0, 0)
+        M12 = np.delete(M12, 1, 1)
+        M12 = np.linalg.det(M12)
+        M1122 = np.copy(inmatrix)
+        M1122 = np.delete(M1122, [0,1] , 0)
+        M1122 = np.delete(M1122, [0,1] , 1)
+        M1122 = np.linalg.det(M1122)
+        M1223 = np.copy(inmatrix)
+        M1223 = np.delete(M1223, [0,1] , 0)
+        M1223 = np.delete(M1223, [1,2] , 1)
+        M1223 = np.linalg.det(M1223)
+        M1123 = np.copy(inmatrix)
+        M1123 = np.delete(M1123, [0,1] , 0)
+        M1123 = np.delete(M1123, [0,2] , 1)
+        M1123 = np.linalg.det(M1123)
+        T_linear[0,0] = np.sqrt(det_linear/M11)
+        T_linear[1,0] = M12/np.sqrt(M11*M1122)
+        T_linear[1,1] = np.sqrt(M11/M1122)
+        T_linear[2,0] = M1223/np.sqrt(inmatrix[3,3])/np.sqrt(M1122)
+        T_linear[2,1] = M1123/np.sqrt(inmatrix[3,3])/np.sqrt(M1122)
+        T_linear[2,2] = np.sqrt(M1122)/np.sqrt(inmatrix[3,3])
+        T_linear[3,0] = inmatrix[3,0]/np.sqrt(inmatrix[3,3])
+        T_linear[3,1] = inmatrix[3,1]/np.sqrt(inmatrix[3,3])
+        T_linear[3,2] = inmatrix[3,2]/np.sqrt(inmatrix[3,3])
+        T_linear[3,3] = np.sqrt(inmatrix[3,3])
+        return cls(matrix=T_linear)
+
+    @property
+    def matrix(self):
+        if self._matrix is None:
+            n = int(np.sqrt(len(self.vector)))
+            k = (len(self.vector) - n) // 2
+            idx = np.tril_indices(n, k=-1)
+            m_re = np.diag(self.vector[:n])
+            m_re[idx] = self.vector[n:-k]
+            m_im = np.zeros_like(m_re)
+            m_im[idx] = self.vector[-k:]
+            return np.matrix(m_re + 1j * m_im)
+        return self._matrix
+
+    @property
+    def vector(self):
+        if self._vector is None:
+            diag = np.diag(self.matrix).real
+            idx = np.tril_indices(len(self.matrix), k=-1)
+            tril = self.matrix[idx]
+            self._vector = np.concatenate([diag, tril.real, tril.imag])
+        return self._vector
+
+    def reconstruct(self):
+        m = self.matrix.H * self.matrix
+        return np.array(m) / np.trace(m)
+
+
+class Tomography:
+
+    def __init__(self, states, amplitudes, gates=None, gatesets=None):
+        self.states = states
+        self.amplitudes = amplitudes
+        self._gates = None
+        self._gatesets = None
+
+        self._linear = None
+        self._fitres = None
+
+    @property
+    def gates(self):
+        if self._gates is None:
+            beta = self.find_beta(self.states)
+            self._gates = self._default_gates(beta)
+        return self._gates
+
+    @property
+    def gatesets(self):
+        if self._gatesets is None:
+            self._gatesets = self._default_gatesets()
+        return self._gatesets
+
+    @staticmethod
+    def find_beta(states):
+        """Finds beta from state data."""
+        refer_A = np.matrix([[1, 1, 1, 1],
+                             [1, 1, -1, -1],
+                             [1, -1, 1, -1],
+                             [1, -1, -1, 1]])
+        refer_B = np.array([states["00"], states["01"],
+                            states["10"], states["11"]])
+        beta = solve(refer_A, refer_B)
+        return np.array(beta).flatten()
+
+    @staticmethod
+    def _default_gates(beta):
+        I = np.matrix([[1, 0], [0, 1]], dtype = complex)
+        X = np.matrix([[0, 1], [1, 0]], dtype = complex)
+        Y = np.matrix([[0, -1j], [1j, 0]], dtype = complex)
+        Z = np.matrix([[1, 0], [0, -1]], dtype = complex)
+        return np.array([
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Z,I) + beta[2]*np.kron(I,Z) + beta[3]*np.kron(Z,Z),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(Z,I) + beta[2]*np.kron(I,Z) - beta[3]*np.kron(Z,Z),
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Z,I) - beta[2]*np.kron(I,Z) - beta[3]*np.kron(Z,Z),
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Y,I) + beta[2]*np.kron(I,Z) + beta[3]*np.kron(Y,Z),
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Y,I) + beta[2]*np.kron(I,Y) + beta[3]*np.kron(Y,Y),
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Y,I) - beta[2]*np.kron(I,X) - beta[3]*np.kron(Y,X),
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Y,I) - beta[2]*np.kron(I,Z) - beta[3]*np.kron(Y,Z),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(X,I) + beta[2]*np.kron(I,Z) - beta[3]*np.kron(X,Z),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(X,I) + beta[2]*np.kron(I,Y) - beta[3]*np.kron(X,Y),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(X,I) - beta[2]*np.kron(I,X) + beta[3]*np.kron(X,X),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(X,I) - beta[2]*np.kron(I,Z) + beta[3]*np.kron(X,Z),
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Z,I) + beta[2]*np.kron(I,Y) + beta[3]*np.kron(Z,Y),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(Z,I) + beta[2]*np.kron(I,Y) - beta[3]*np.kron(Z,Y),
+            beta[0]*np.kron(I,I) + beta[1]*np.kron(Z,I) - beta[2]*np.kron(I,X) - beta[3]*np.kron(Z,X),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(Z,I) - beta[2]*np.kron(I,X) + beta[3]*np.kron(Z,X),
+            beta[0]*np.kron(I,I) - beta[1]*np.kron(Z,I) - beta[2]*np.kron(I,Z) + beta[3]*np.kron(Z,Z)
+        ])
+
+    def _default_gatesets(self):
+        return [([0, 1, 2, 15], (0, 1, 2, 3), (0, 1, 2, 3)),
+                ([11, 12, 13, 14], (1, 0, 3, 2), (0, 1, 2, 3)),
+                ([3, 6, 7, 10], (2, 3, 0, 1), (0, 1, 2, 3)),
+                ([4, 5, 8, 9], (3, 2, 1, 0), (0, 1, 2, 3))]
+
+    @property
+    def linear(self):
+        """Linear estimation of the density matrix."""
+        if self._linear is None:
+            dtype = self.gates.dtype
+            self._linear = np.zeros_like(self.gates[0])
+            for i, (seti, seta, setb) in enumerate(self.gatesets):
+                setA = self.gates[seti][:, seta, setb]
+                minus = sum(np.dot(self.gates[seti][:, sa, sb], self._linear[sb, sa])
+                            for _, sa, sb in self.gatesets[:i])
+                setB = self.amplitudes[seti].astype(dtype) - minus
+                self._linear[setb, seta] = solve(setA, setB)
+        return self._linear
+
+    @property
+    def fit(self):
+        if self._fitres is None:
+            raise ValueError
+        return Cholesky.from_vector(self._fitres.x).reconstruct()
+
+    @property
+    def success(self):
+        if self._fitres is None:
+            raise ValueError
+        return self._fitres.success
+
+    def mle(self, x):
+        rho = Cholesky.from_vector(x).reconstruct()
+        loss = 0
+        for gate, amp in zip(self.gates, self.amplitudes):
+            tr = np.trace(rho * gate)
+            loss += 0.5 * (amp - tr) ** 2 / tr
+        return abs(loss)
+
+    def minimize(self, tol=1e-9):
+        """Fits density matrix to minimize MLE."""
+        t_guess = Cholesky.decompose(self.linear).vector
+        self._fitres = minimize(self.mle, t_guess, tol=tol)
+        return self._fitres
+
+    def fidelity(self, theory):
+        sqrt_th = sqrtm(theory)
+        return abs(np.trace(sqrtm(sqrt_th @ self.fit @ sqrt_th))) * 100
