66cd14f2fc7453f96646756deaa7f193d7b4df66
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-02-05 13:50:05 +0400

    Move tomography in hardware

--- src/qibo/__init__.py
+++ src/qibo/__init__.py
@@ -4,6 +4,5 @@ from qibo.backends import set_precision, set_backend, set_device
 from qibo.backends import get_backend, get_precision, get_device
 from qibo.backends import numpy_matrices as matrices
 from qibo.backends import K
-from qibo.numpy import tomography
 from qibo import callbacks, evolution, gates, hamiltonians, models
 from qibo import parallel, optimizers, solvers
