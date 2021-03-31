27f7232ed17ca1e40d76ddbd1d4ca9f62f886cbc
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-03-31 17:29:56 +0400

    Create grover.py

--- src/qibo/models/__init__.py
+++ src/qibo/models/__init__.py
@@ -1,4 +1,5 @@
 from qibo.models.circuit import Circuit, QFT
 from qibo.models.evolution import StateEvolution, AdiabaticEvolution
 from qibo.models.variational import VQE, QAOA
+from qibo.models.grover import Grover
 from qibo.models import hep
