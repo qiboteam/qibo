abc4fff47b646856ce0e1c9cb6d27e0d2581b090
igres26 <igres26@gmail.com>
2021-04-07 18:12:25 +0400

    added backend switcher

--- examples/bell-variational/main.py
+++ examples/bell-variational/main.py
@@ -1,16 +1,19 @@
 import numpy as np
 from qibo.optimizers import optimize
 from functions import set_parametrized_circuits, cost_function, compute_chsh
+from qibo import set_backend
 import argparse
 
 
-def main(nshots):
+def main(nshots, backend):
     '''Variationally find a maximally entangled state and the correct measurement angle
        for violation of Bell inequalities.
     Args:
         nshots: number of shots to use for the minimization.
+        backend: choice of backend to run the example in.
         
     '''
+    set_backend(backend)
     initial_parameters = np.random.uniform(0, 2*np.pi, 2)
     circuits = set_parametrized_circuits()
     best, params, _ = optimize(cost_function, initial_parameters, args=(circuits, nshots))
@@ -30,5 +33,6 @@ def main(nshots):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--nshots", default=10000, type=int, help='Number of shots for each circuit base')
+    parser.add_argument("--backend", default='defaulteinsum', type=str, help='Backend to use for the example')
     args = vars(parser.parse_args())
     main(**args)
