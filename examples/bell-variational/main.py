889c810d26605fa034c3e25309def40abf81f1df
Sergi Ramos <36544268+igres26@users.noreply.github.com>
2021-04-11 15:53:16 +0400

    backend to custom as default
    Co-authored-by: Stefano Carrazza <scarrazza@users.noreply.github.com>

--- examples/bell-variational/main.py
+++ examples/bell-variational/main.py
@@ -33,6 +33,6 @@ def main(nshots, backend):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--nshots", default=10000, type=int, help='Number of shots for each circuit base')
-    parser.add_argument("--backend", default='defaulteinsum', type=str, help='Backend to use for the example')
+    parser.add_argument("--backend", default='custom', type=str, help='Backend to use for the example')
     args = vars(parser.parse_args())
     main(**args)
