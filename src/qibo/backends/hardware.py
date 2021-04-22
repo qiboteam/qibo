ae9bbae9759c337a35202c71e42cbd93614f0165
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-04-22 15:34:56 +0400

    Fix hw backend inheritance

--- src/qibo/backends/hardware.py
+++ src/qibo/backends/hardware.py
@@ -4,6 +4,14 @@ from qibo.config import raise_error
 
 class HardwareBackend(NumpyBackend):
 
+    def __init__(self):
+        super().__init__()
+        from qibo.hardware.scheduler import TaskScheduler
+        self.name = "hardwarebase"
+        self.custom_gates = True
+        self.scheduler = TaskScheduler()
+        self.experiment = None
+
     def create_einsum_cache(self, qubits, nqubits, ncontrol=None): # pragma: no cover
         raise_error(NotImplementedError, "`create_einsum_cache` method is "
                                          "not required for hardware backends.")
@@ -20,8 +28,5 @@ class IcarusQBackend(HardwareBackend):
     def __init__(self):
         super().__init__()
         from qibo.hardware.experiments import IcarusQ
-        from qibo.hardware.scheduler import TaskScheduler
         self.name = "icarusq"
-        self.custom_gates = True
         self.experiment = IcarusQ()
-        self.scheduler = TaskScheduler()
