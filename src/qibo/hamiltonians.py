70c559717e9843d3916863a04d328f0f3f08bd11
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-06-30 19:04:36 +0400

    Rename trotter flag to dense

--- src/qibo/hamiltonians.py
+++ src/qibo/hamiltonians.py
@@ -28,7 +28,7 @@ def _build_spin_model(nqubits, matrix, condition):
     return h
 
 
-def XXZ(nqubits, delta=0.5, numpy=False, trotter=False):
+def XXZ(nqubits, delta=0.5, numpy=False, dense=True):
     """Heisenberg XXZ model with periodic boundary conditions.
 
     .. math::
@@ -40,9 +40,9 @@ def XXZ(nqubits, delta=0.5, numpy=False, trotter=False):
         numpy (bool): If ``True`` the Hamiltonian is created using numpy as the
             calculation backend, otherwise TensorFlow is used.
             Default option is ``numpy = False``.
-        trotter (bool): If ``True`` it creates the Hamiltonian as a
-            :class:`qibo.abstractions.hamiltonians.TrotterHamiltonian` object, otherwise
-            it creates a :class:`qibo.abstractions.hamiltonians.Hamiltonian` object.
+        dense (bool): If ``True`` it creates the Hamiltonian as a
+            :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
+            a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
 
     Example:
         ::
@@ -50,37 +50,36 @@ def XXZ(nqubits, delta=0.5, numpy=False, trotter=False):
             from qibo.hamiltonians import XXZ
             h = XXZ(3) # initialized XXZ model with 3 qubits
     """
-    if trotter:
-        hx = K.np.kron(matrices.X, matrices.X)
-        hy = K.np.kron(matrices.Y, matrices.Y)
-        hz = K.np.kron(matrices.Z, matrices.Z)
+    if dense:
+        condition = lambda i, j: i in {j % nqubits, (j+1) % nqubits}
+        hx = _build_spin_model(nqubits, matrices.X, condition)
+        hy = _build_spin_model(nqubits, matrices.Y, condition)
+        hz = _build_spin_model(nqubits, matrices.Z, condition)
         matrix = hx + hy + delta * hz
-        terms = [HamiltonianTerm(matrix, i, i + 1) for i in range(nqubits - 1)]
-        terms.append(HamiltonianTerm(matrix, nqubits - 1, 0))
-        return SymbolicHamiltonian.from_terms(terms)
+        return Hamiltonian(nqubits, matrix, numpy=numpy)
 
-    condition = lambda i, j: i in {j % nqubits, (j+1) % nqubits}
-    hx = _build_spin_model(nqubits, matrices.X, condition)
-    hy = _build_spin_model(nqubits, matrices.Y, condition)
-    hz = _build_spin_model(nqubits, matrices.Z, condition)
+    hx = K.np.kron(matrices.X, matrices.X)
+    hy = K.np.kron(matrices.Y, matrices.Y)
+    hz = K.np.kron(matrices.Z, matrices.Z)
     matrix = hx + hy + delta * hz
-    return Hamiltonian(nqubits, matrix, numpy=numpy)
+    terms = [HamiltonianTerm(matrix, i, i + 1) for i in range(nqubits - 1)]
+    terms.append(HamiltonianTerm(matrix, nqubits - 1, 0))
+    return SymbolicHamiltonian.from_terms(terms)
 
 
-def _OneBodyPauli(nqubits, matrix, numpy=False, trotter=False,
-                  ground_state=None):
+def _OneBodyPauli(nqubits, matrix, numpy=False, dense=True, ground_state=None):
     """Helper method for constracting non-interacting X, Y, Z Hamiltonians."""
-    if not trotter:
-        condition = lambda i, j: i == j % nqubits
-        ham = -_build_spin_model(nqubits, matrix, condition)
-        return Hamiltonian(nqubits, ham, numpy=numpy)
+    if dense:
+        matrix = - matrix
+        terms = [HamiltonianTerm(matrix, i) for i in range(nqubits)]
+        return SymbolicHamiltonian.from_terms(terms, ground_state)
 
-    matrix = - matrix
-    terms = [HamiltonianTerm(matrix, i) for i in range(nqubits)]
-    return SymbolicHamiltonian.from_terms(terms, ground_state)
+    condition = lambda i, j: i == j % nqubits
+    ham = -_build_spin_model(nqubits, matrix, condition)
+    return Hamiltonian(nqubits, ham, numpy=numpy)
 
 
-def X(nqubits, numpy=False, trotter=False):
+def X(nqubits, numpy=False, dense=True):
     """Non-interacting Pauli-X Hamiltonian.
 
     .. math::
@@ -91,19 +90,19 @@ def X(nqubits, numpy=False, trotter=False):
         numpy (bool): If ``True`` the Hamiltonian is created using numpy as the
             calculation backend, otherwise TensorFlow is used.
             Default option is ``numpy = False``.
-        trotter (bool): If ``True`` it creates the Hamiltonian as a
-            :class:`qibo.abstractions.hamiltonians.TrotterHamiltonian` object, otherwise
-            it creates a :class:`qibo.abstractions.hamiltonians.Hamiltonian` object.
+        dense (bool): If ``True`` it creates the Hamiltonian as a
+            :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
+            a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
     """
     from qibo import K
     def ground_state():
         n = K.cast(2 ** nqubits, dtype='DTYPEINT')
         state = K.ones(n, dtype='DTYPECPX')
         return state / K.sqrt(K.cast(n, dtype=state.dtype))
-    return _OneBodyPauli(nqubits, matrices.X, numpy, trotter, ground_state)
+    return _OneBodyPauli(nqubits, matrices.X, numpy, dense, ground_state)
 
 
-def Y(nqubits, numpy=False, trotter=False):
+def Y(nqubits, numpy=False, dense=True):
     """Non-interacting Pauli-Y Hamiltonian.
 
     .. math::
@@ -114,14 +113,14 @@ def Y(nqubits, numpy=False, trotter=False):
         numpy (bool): If ``True`` the Hamiltonian is created using numpy as the
             calculation backend, otherwise TensorFlow is used.
             Default option is ``numpy = False``.
-        trotter (bool): If ``True`` it creates the Hamiltonian as a
-            :class:`qibo.abstractions.hamiltonians.TrotterHamiltonian` object, otherwise
-            it creates a :class:`qibo.abstractions.hamiltonians.Hamiltonian` object.
+        dense (bool): If ``True`` it creates the Hamiltonian as a
+            :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
+            a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
     """
-    return _OneBodyPauli(nqubits, matrices.Y, numpy, trotter)
+    return _OneBodyPauli(nqubits, matrices.Y, numpy, dense)
 
 
-def Z(nqubits, numpy=False, trotter=False):
+def Z(nqubits, numpy=False, dense=True):
     """Non-interacting Pauli-Z Hamiltonian.
 
     .. math::
@@ -132,14 +131,14 @@ def Z(nqubits, numpy=False, trotter=False):
         numpy (bool): If ``True`` the Hamiltonian is created using numpy as the
             calculation backend, otherwise TensorFlow is used.
             Default option is ``numpy = False``.
-        trotter (bool): If ``True`` it creates the Hamiltonian as a
-            :class:`qibo.abstractions.hamiltonians.TrotterHamiltonian` object, otherwise
-            it creates a :class:`qibo.abstractions.hamiltonians.Hamiltonian` object.
+        dense (bool): If ``True`` it creates the Hamiltonian as a
+            :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
+            a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
     """
-    return _OneBodyPauli(nqubits, matrices.Z, numpy, trotter)
+    return _OneBodyPauli(nqubits, matrices.Z, numpy, dense)
 
 
-def TFIM(nqubits, h=0.0, numpy=False, trotter=False):
+def TFIM(nqubits, h=0.0, numpy=False, dense=True):
     """Transverse field Ising model with periodic boundary conditions.
 
     .. math::
@@ -151,26 +150,25 @@ def TFIM(nqubits, h=0.0, numpy=False, trotter=False):
         numpy (bool): If ``True`` the Hamiltonian is created using numpy as the
             calculation backend, otherwise TensorFlow is used.
             Default option is ``numpy = False``.
-        trotter (bool): If ``True`` it creates the Hamiltonian as a
-            :class:`qibo.abstractions.hamiltonians.TrotterHamiltonian` object, otherwise
-            it creates a :class:`qibo.abstractions.hamiltonians.Hamiltonian` object.
+        dense (bool): If ``True`` it creates the Hamiltonian as a
+            :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
+            a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
     """
-    if trotter:
-        matrix = -(K.np.kron(matrices.Z, matrices.Z) +
-                   h * K.np.kron(matrices.X, matrices.I))
-        terms = [HamiltonianTerm(matrix, i, i + 1) for i in range(nqubits - 1)]
-        terms.append(HamiltonianTerm(matrix, nqubits - 1, 0))
-        return SymbolicHamiltonian.from_terms(terms)
+    if dense:
+        condition = lambda i, j: i in {j % nqubits, (j+1) % nqubits}
+        ham = -_build_spin_model(nqubits, matrices.Z, condition)
+        if h != 0:
+            condition = lambda i, j: i == j % nqubits
+            ham -= h * _build_spin_model(nqubits, matrices.X, condition)
+        return Hamiltonian(nqubits, ham, numpy=numpy)
 
-    condition = lambda i, j: i in {j % nqubits, (j+1) % nqubits}
-    ham = -_build_spin_model(nqubits, matrices.Z, condition)
-    if h != 0:
-        condition = lambda i, j: i == j % nqubits
-        ham -= h * _build_spin_model(nqubits, matrices.X, condition)
-    return Hamiltonian(nqubits, ham, numpy=numpy)
+    matrix = -(K.np.kron(matrices.Z, matrices.Z) + h * K.np.kron(matrices.X, matrices.I))
+    terms = [HamiltonianTerm(matrix, i, i + 1) for i in range(nqubits - 1)]
+    terms.append(HamiltonianTerm(matrix, nqubits - 1, 0))
+    return SymbolicHamiltonian.from_terms(terms)
 
 
-def MaxCut(nqubits, numpy=False, trotter=False):
+def MaxCut(nqubits, numpy=False, dense=True):
     """Max Cut Hamiltonian.
 
     .. math::
@@ -181,9 +179,9 @@ def MaxCut(nqubits, numpy=False, trotter=False):
         numpy (bool): If ``True`` the Hamiltonian is created using numpy as the
             calculation backend, otherwise TensorFlow is used.
             Default option is ``numpy = False``.
-        trotter (bool): If ``True`` it creates the Hamiltonian as a
-            :class:`qibo.abstractions.hamiltonians.TrotterHamiltonian` object, otherwise
-            it creates a :class:`qibo.abstractions.hamiltonians.Hamiltonian` object.
+        dense (bool): If ``True`` it creates the Hamiltonian as a
+            :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
+            a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
     """
     import sympy as sp
 
@@ -197,6 +195,6 @@ def MaxCut(nqubits, numpy=False, trotter=False):
     smap.update({s: (i, v[i]) for i, s in enumerate(V)})
 
     ham = SymbolicHamiltonian(sham, smap)
-    if trotter:
-        return ham
-    return ham.dense
+    if dense:
+        return ham.dense
+    return ham
