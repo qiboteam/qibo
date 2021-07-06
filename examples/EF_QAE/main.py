4277525791bd3b2aa58c093044a3547ed3a9cb42
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-07-06 20:38:21 +0400

    Fix efqae

--- examples/EF_QAE/main.py
+++ examples/EF_QAE/main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from qibo.models import Circuit
-from qibo import hamiltonians, gates, models, matrices
+from qibo import hamiltonians, gates, models, K
 from qibo.hamiltonians import Hamiltonian
 from scipy.optimize import minimize
 from sklearn.datasets import load_digits
@@ -10,7 +10,7 @@ import argparse
 
 
 def main(layers, autoencoder, example):
-    
+
     def encoder_hamiltonian_simple(nqubits, ncompress):
         """Creates the encoding Hamiltonian.
         Args:
@@ -24,7 +24,7 @@ def main(layers, autoencoder, example):
         m1 = np.eye(2 ** (nqubits - ncompress), dtype=m0.dtype)
         ham = hamiltonians.Hamiltonian(nqubits, np.kron(m1, m0))
         return 0.5 * (ham + ncompress)
-    
+
     def rotate(theta, x):
         new_theta = []
         index = 0
@@ -45,14 +45,14 @@ def main(layers, autoencoder, example):
     compress = 2
     encoder = encoder_hamiltonian_simple(nqubits, compress)
     count = [0]
-    
+
     if example == 0:
         ising_groundstates = []
         lambdas = np.linspace(0.5, 1.0, 20)
         for lamb in lambdas:
             ising_ham = -1 * hamiltonians.TFIM(nqubits, h=lamb)
             ising_groundstates.append(ising_ham.eigenvectors()[0])
-            
+
         if autoencoder == 1:
             circuit = models.Circuit(nqubits)
             for l in range(layers):
@@ -72,36 +72,36 @@ def main(layers, autoencoder, example):
                 circuit.add(gates.CZ(4, 1))
             for q in range(nqubits-compress, nqubits, 1):
                 circuit.add(gates.RY(q, theta=0))
-                
+
             def cost_function_QAE_Ising(params, count):
                 """Evaluates the cost function to be minimized for the QAE and Ising model.
-        
+
                 Args:
                     params (array or list): values of the parameters.
-        
+
                 Returns:
                     Value of the cost function.
-                """                    
+                """
                 cost = 0
                 circuit.set_parameters(params) # this will change all thetas to the appropriate values
                 for i in range(len(ising_groundstates)):
                     final_state = circuit.execute(np.copy(ising_groundstates[i]))
-                    cost += encoder.expectation(final_state).numpy().real
-                    
+                    cost += K.to_numpy(encoder.expectation(final_state)).real
+
                 cost_function_steps.append(cost/len(ising_groundstates)) # save cost function value after each step
-        
+
                 if count[0] % 50 == 0:
                     print(count[0], cost/len(ising_groundstates))
                 count[0] += 1
-        
+
                 return cost/len(ising_groundstates)
-        
+
             nparams = 2 * nqubits * layers + compress
             initial_params = np.random.uniform(0, 2*np.pi, nparams)
-        
+
             result = minimize(cost_function_QAE_Ising, initial_params,
                               args=(count), method='BFGS', options={'maxiter': 5.0e4})
-            
+
         elif autoencoder == 0:
             circuit = models.Circuit(nqubits)
             for l in range(layers):
@@ -121,38 +121,38 @@ def main(layers, autoencoder, example):
                 circuit.add(gates.CZ(4, 1))
             for q in range(nqubits-compress, nqubits, 1):
                 circuit.add(gates.RY(q, theta=0))
-                
+
             def cost_function_EF_QAE_Ising(params, count):
                 """Evaluates the cost function to be minimized for the EF-QAE and Ising model.
-        
+
                 Args:
                     params (array or list): values of the parameters.
-        
+
                 Returns:
                     Value of the cost function.
-                """                                                               
+                """
                 cost = 0
                 for i in range(len(ising_groundstates)):
                     newparams = rotate(params, lambdas[i])
                     circuit.set_parameters(newparams)
                     final_state = circuit.execute(np.copy(ising_groundstates[i]))
-                    cost += encoder.expectation(final_state).numpy().real
-                    
+                    cost += K.to_numpy(encoder.expectation(final_state)).real
+
                 cost_function_steps.append(cost/len(ising_groundstates)) # save cost function value after each step
-        
+
                 if count[0] % 50 == 0:
                     print(count[0], cost/len(ising_groundstates))
                 count[0] += 1
-        
+
                 return cost/len(ising_groundstates)
-        
-            
+
+
             nparams = 4 * nqubits * layers + 2 * compress
             initial_params = np.random.uniform(0, 2*np.pi, nparams)
-        
+
             result = minimize(cost_function_EF_QAE_Ising, initial_params,
                               args=(count), method='BFGS', options={'maxiter': 5.0e4})
-            
+
         else:
             raise ValueError("You have to introduce a value of 0 or 1 in the autoencoder argument.")
 
@@ -161,10 +161,10 @@ def main(layers, autoencoder, example):
         vector_0 = []
         vector_1 = []
         for value in [0, 10, 20, 30, 36, 48, 49, 55, 72, 78]:
-            vector_0.append(np.array(digits.data[value])/np.linalg.norm(np.array(digits.data[value])))    
+            vector_0.append(np.array(digits.data[value])/np.linalg.norm(np.array(digits.data[value])))
         for value in [1, 11, 21, 42, 47, 56, 70, 85, 90, 93]:
             vector_1.append(np.array(digits.data[value])/np.linalg.norm(np.array(digits.data[value])))
-        
+
         if autoencoder == 1:
             circuit = models.Circuit(nqubits)
             for l in range(layers):
@@ -184,36 +184,36 @@ def main(layers, autoencoder, example):
                 circuit.add(gates.CZ(4, 1))
             for q in range(nqubits-compress, nqubits, 1):
                 circuit.add(gates.RY(q, theta=0))
-                
+
             def cost_function_QAE_Digits(params, count):
                 """Evaluates the cost function to be minimized for the QAE and Handwritten digits.
-        
+
                 Args:
                     params (array or list): values of the parameters.
-        
+
                 Returns:
                     Value of the cost function.
-                """                                            
+                """
                 cost = 0
                 circuit.set_parameters(params) # this will change all thetas to the appropriate values
                 for i in range(len(vector_0)):
                     final_state = circuit.execute(np.copy(vector_0[i]))
-                    cost += encoder.expectation(final_state).numpy().real
+                    cost += K.to_numpy(encoder.expectation(final_state)).real
                 for i in range(len(vector_1)):
                     final_state = circuit.execute(np.copy(vector_1[i]))
-                    cost += encoder.expectation(final_state).numpy().real
-                    
+                    cost += K.to_numpy(encoder.expectation(final_state)).real
+
                 cost_function_steps.append(cost/(len(vector_0)+len(vector_1))) # save cost function value after each step
-        
+
                 if count[0] % 50 == 0:
                     print(count[0], cost/(len(vector_0)+len(vector_1)))
                 count[0] += 1
-        
+
                 return cost/(len(vector_0)+len(vector_1))
-        
+
             nparams = 2 * nqubits * layers + compress
             initial_params = np.random.uniform(0, 2*np.pi, nparams)
-        
+
             result = minimize(cost_function_QAE_Digits, initial_params,
                               args=(count), method='BFGS', options={'maxiter': 5.0e4})
 
@@ -236,49 +236,49 @@ def main(layers, autoencoder, example):
                 circuit.add(gates.CZ(4, 1))
             for q in range(nqubits-compress, nqubits, 1):
                 circuit.add(gates.RY(q, theta=0))
-                
+
             def cost_function_EF_QAE_Digits(params, count):
                 """Evaluates the cost function to be minimized for the EF-QAE and Handwritten digits.
-        
+
                 Args:
                     params (array or list): values of the parameters.
-        
+
                 Returns:
                     Value of the cost function.
-                """                                    
+                """
                 cost = 0
                 newparams = rotate(params, 1)
                 circuit.set_parameters(newparams)
                 for i in range(len(vector_0)):
                     final_state = circuit.execute(np.copy(vector_0[i]))
-                    cost += encoder.expectation(final_state).numpy().real
+                    cost += K.to_numpy(encoder.expectation(final_state)).real
                 newparams = rotate(params, 2)
                 circuit.set_parameters(newparams)
                 for i in range(len(vector_1)):
                     final_state = circuit.execute(np.copy(vector_1[i]))
-                    cost += encoder.expectation(final_state).numpy().real
-                    
+                    cost += K.to_numpy(encoder.expectation(final_state)).real
+
                 cost_function_steps.append(cost/(len(vector_0)+len(vector_1))) # save cost function value after each step
-        
+
                 if count[0] % 50 == 0:
                     print(count[0], cost/(len(vector_0)+len(vector_1)))
                 count[0] += 1
-        
+
                 return cost/(len(vector_0)+len(vector_1))
-        
-            
+
+
             nparams = 4 * nqubits * layers + 2 * compress
             initial_params = np.random.uniform(0, 2*np.pi, nparams)
-        
+
             result = minimize(cost_function_EF_QAE_Digits, initial_params,
-                              args=(count), method='BFGS', options={'maxiter': 5.0e4})           
-            
+                              args=(count), method='BFGS', options={'maxiter': 5.0e4})
+
         else:
             raise ValueError("You have to introduce a value of 0 or 1 in the autoencoder argument.")
-            
+
     else:
         raise ValueError("You have to introduce a value of 0 or 1 in the example argument.")
-        
+
     print('Final parameters: ', result.x)
     print('Final cost function: ', result.fun)
 
