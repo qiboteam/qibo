1f6849c23c761fc7f21d9256dc2bc8d5b9359125
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-09-04 14:51:10 +0200

    adding new loader mechanism

--- src/qibo/__init__.py
+++ src/qibo/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.6.dev2"
 from qibo.config import set_batch_size, get_batch_size
 from qibo.config import set_metropolis_threshold, get_metropolis_threshold
-from qibo.backends import set_backend, get_backend
+from qibo.backends import set_backend, get_backend, get_available_backends
 from qibo.backends import set_precision, get_precision
 from qibo.backends import set_device, get_device
 from qibo.backends import set_threads, get_threads
