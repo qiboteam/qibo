4cd8c494e7e1a5c05ca5feb0a0ee865b53026c24
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-09-06 10:09:03 +0200

    excluding hardware backends

--- src/qibo/tests/conftest.py
+++ src/qibo/tests/conftest.py
@@ -7,13 +7,10 @@ import sys
 import pytest
 import qibo
 
-_available_backends = set(qibo.K.available_backends.keys())
+_available_backends = set(qibo.K.available_backends.keys()) - set(qibo.K.hardware_backends.keys())
 _ACCELERATORS = None
 if ("qibotf" in _available_backends or "qibojit" in _available_backends):
     _ACCELERATORS = "2/GPU:0,1/GPU:0+1/GPU:1,2/GPU:0+1/GPU:1+1/GPU:2"
-if "qiboicarusq" in _available_backends: # pragma: no cover
-    # skip hardware backend for tests
-    _available_backends.remove("qiboicarusq")
 _BACKENDS = ",".join(_available_backends)
 
 
