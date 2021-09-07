068751d9bdc3716251843f2c80cc4cd6adf75e1d
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-09-07 19:06:57 +0200

    another go

--- src/qibo/tests/conftest.py
+++ src/qibo/tests/conftest.py
@@ -9,8 +9,8 @@ import qibo
 
 _available_backends = set(qibo.K.available_backends.keys()) - set(qibo.K.hardware_backends.keys())
 _ACCELERATORS = None
-for backend in qibo.K.available_backends:
-    if backend.supports_multigpu:
+for bkd in qibo.K.available_backends:
+    if bkd.supports_multigpu:
         _ACCELERATORS = "2/GPU:0,1/GPU:0+1/GPU:1,2/GPU:0+1/GPU:1+1/GPU:2"
 _BACKENDS = ",".join(_available_backends)
 
