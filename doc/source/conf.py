1f7134f5adfdc409aaca5b0c84aba1e5ac556ae8
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-10-02 20:15:34 +0200

    reverting to rtd theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,12 +72,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_book_theme"
-html_theme_options = {
-    "repository_url": "https://github.com/qiboteam/qibo",
-    "use_repository_button": True,
-}
-html_title = f"{release}"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
