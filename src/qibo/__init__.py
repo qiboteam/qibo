201b2b1c452a230e1f2a78d89d99e1029173ee8a
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-11-07 23:07:08 +0100

    increasing dev version

--- src/qibo/__init__.py
+++ src/qibo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.7rc0"
+__version__ = "0.1.7.dev1"
 from qibo.config import set_batch_size, get_batch_size
 from qibo.config import set_metropolis_threshold, get_metropolis_threshold
 from qibo.backends import set_backend, get_backend
