3b6ffa75e08cf63ff09646bf8044e9ad1bc21dfa
Marco Lazzarin <48728634+mlazzarin@users.noreply.github.com>
2021-12-23 12:27:16 +0400

    Fix duplicaed np.ndarrays when casting

--- src/qibo/backends/numpy.py
+++ src/qibo/backends/numpy.py
@@ -64,7 +64,7 @@ class NumpyBackend(abstract.AbstractBackend):
             dtype = self.dtypes(dtype)
         if isinstance(x, self.backend.ndarray):
             return x.astype(dtype, copy=False)
-        return self.backend.array(x, dtype=dtype)
+        return self.backend.array(x, dtype=dtype, copy=False)
 
     def diag(self, x, dtype='DTYPECPX'):
         if isinstance(dtype, str):
