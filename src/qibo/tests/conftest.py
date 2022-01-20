e8def0ac0da929d95eeb06f3e3263c11e3fa0000
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2022-01-20 18:16:09 +0400

    Test all available platforms of each backend automatically

--- src/qibo/tests/conftest.py
+++ src/qibo/tests/conftest.py
@@ -6,14 +6,15 @@ Pytest fixtures.
 import sys
 import pytest
 import qibo
+from qibo import K
 
-_available_backends = set(b.get('name') for b in qibo.K.profile.get('backends')
+_available_backends = set(b.get('name') for b in K.profile.get('backends')
                           if (not b.get('is_hardware', False) and
-                          qibo.K.check_availability(b.get('name'))))
+                          K.check_availability(b.get('name'))))
 _available_backends.add("numpy")
 _ACCELERATORS = None
 for bkd in _available_backends:
-    if qibo.K.construct_backend(bkd).supports_multigpu:
+    if K.construct_backend(bkd).supports_multigpu:
         _ACCELERATORS = "2/GPU:0,1/GPU:0+1/GPU:1,2/GPU:0+1/GPU:1+1/GPU:2"
 _BACKENDS = ",".join(_available_backends)
 
@@ -46,11 +47,17 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture
-def backend(backend_name):
+def backend(backend_platform):
+    if "-" in backend_platform:
+        backend_name, platform_name = backend_platform.split("-")
+    else:
+        backend_name, platform_name = backend_platform, None
+
     original_backend = qibo.get_backend()
-    qibo.set_backend(backend_name)
+    original_platform = K.get_platform()
+    qibo.set_backend(backend_name, platform=platform_name)
     yield
-    qibo.set_backend(original_backend)
+    qibo.set_backend(original_backend, platform=original_platform)
 
 
 def pytest_generate_tests(metafunc):
@@ -70,11 +77,23 @@ def pytest_generate_tests(metafunc):
     the user when calling `pytest`.
     """
     backends = metafunc.config.option.backends.split(",")
-    accelerators = metafunc.config.option.accelerators
+    # construct backend instances to find what platforms each backend supports
+    # and if it supports multi-GPU
+    backend_platforms = []
+    distributed_backends = [] # list
+    for name in backends:
+        instance = K.construct_backend(name)
+        platforms = [f"{name}-{p}" if p is not None else name
+                     for p in instance.available_platforms]
+        backend_platforms.extend(platforms)
+        if instance.supports_multigpu:
+            distributed_backends.extend(platforms)
+
     # parse accelerator stings to dicts
+    accelerators = metafunc.config.option.accelerators.split(",")
     if accelerators is not None:
-        accelerators = [{dev[1:]: int(dev[0]) for dev in x.split("+")}
-                        for x in accelerators.split(",")]
+        accelerators = [{dev[1:]: int(dev[0]) for dev in x.split("+")} for x in accelerators]
+
     distributed_tests = {
         "qibo.tests.test_core_states_distributed",
         "qibo.tests.test_core_distutils",
@@ -83,8 +102,7 @@ def pytest_generate_tests(metafunc):
     }
     module_name = metafunc.module.__name__
     # skip distributed tests if qibojit or qibotf are not installed
-    if ((module_name in distributed_tests) and ("qibotf" not in backends) and
-        ("qibojit" not in backends)): # pragma: no cover
+    if module_name in distributed_tests and not distributed_backends: # pragma: no cover
         pytest.skip("Skipping distributed tests because are not supported by "
                     "the available backends.")
     # skip distributed tests on mac
@@ -96,29 +114,27 @@ def pytest_generate_tests(metafunc):
     # for `test_backends_agreement.py`
     if "tested_backend" in metafunc.fixturenames:
         target = metafunc.config.option.target_backend
-        test_backends = [x for x in backends if x != target and x not in qibo.K.hardware_backends]
-        metafunc.parametrize("tested_backend", test_backends)
+        metafunc.parametrize("tested_backend", [x for x in backends if x != target])
         metafunc.parametrize("target_backend", [target])
 
     if "backend_name" in metafunc.fixturenames:
+        metafunc.parametrize("backend_name", backends)
+
+    if "backend_platform" in metafunc.fixturenames:
         if metafunc.module.__name__ in distributed_tests:
-            distributed_backends = list(set(backends) & {"qibotf", "qibojit"})
-            metafunc.parametrize("backend_name", distributed_backends)
+            metafunc.parametrize("backend_platform", distributed_backends)
             if "accelerators" in metafunc.fixturenames:
                 metafunc.parametrize("accelerators", accelerators)
 
         elif "accelerators" in metafunc.fixturenames:
             if accelerators is None: # pragma: no cover
                 # `accelerators` is never `None` in CI test execution
-                metafunc.parametrize("backend_name", backends)
+                metafunc.parametrize("backend_platform", backend_platforms)
                 metafunc.parametrize("accelerators", [None])
             else:
-                config = [(b, None) for b in backends]
-                if "qibotf" in backends:
-                    config.extend(("qibotf", d) for d in accelerators)
-                if "qibojit" in backends:
-                    config.extend(("qibojit", d) for d in accelerators)
-                metafunc.parametrize("backend_name,accelerators", config)
+                config = [(b, None) for b in backend_platforms]
+                config.extend((b, a) for b in distributed_backends for a in accelerators)
+                metafunc.parametrize("backend_platform,accelerators", config)
 
         else:
-            metafunc.parametrize("backend_name", backends)
+            metafunc.parametrize("backend_platform", backend_platforms)
