821fac5f1daf2de2e9728b56f040bcd6d900563f
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2022-02-17 16:10:09 +0400

    Try coo_matrix instead of coo_array

--- src/qibo/backends/tensorflow.py
+++ src/qibo/backends/tensorflow.py
@@ -102,11 +102,11 @@ class TensorflowBackend(NumpyBackend):
         if isinstance(x, self.numeric_types):
             return x
         elif self.issparse(x):
-            from scipy.sparse import coo_array  # pylint: disable=E0611
+            from scipy.sparse import coo_matrix  # pylint: disable=E0611
             idx = self.np.array(x.indices)
             values = self.np.array(x.values)
             shape = tuple(x.shape)
-            return coo_array((values, (idx[:, 0], idx[:, 1])), shape=shape)
+            return coo_matrix((values, (idx[:, 0], idx[:, 1])), shape=shape)
         return self.np.array(x, copy=False)
 
     def to_complex(self, re, img):
