bdd1cbbd577884aab44f12c2eb7bcf7b7679bdb7
shangtai <christo173@hotmail.com>
2022-04-02 15:06:11 +0800

    added depolarizing channel

--- src/qibo/models/error_mitigation.py
+++ src/qibo/models/error_mitigation.py
@@ -3,6 +3,7 @@ this file is to collect various functions used in error mitigation
 '''
 
 from qibo import models
+import numpy as np
 
 def circuit_folding(circ, scaling_factor):
     ''' folds a quantum circuit
@@ -15,3 +16,18 @@ def circuit_folding(circ, scaling_factor):
         c += (circ + circ_inv)
     c += circ
     return c
+
+
+def depolarizing(rho,f):
+    '''
+    Args:
+        rho: a function in L(C^D), set of linear map, represented as a matrix.
+        f: f is a parameter, smaller f means less noise, it is between 0 and 1
+    Returns:
+        D: rho maps to f*rho + (1-f)*trace(rho)*I/d
+    '''
+
+    d = rho.shape[0]
+    return f*rho + (1-f)*np.trace(rho)*np.eye(d)/d
+
+
