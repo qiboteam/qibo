f1a94a65070a517b196fb90484c30000c569dd8d
Andrea Pasquale <andreapasquale97@gmail.com>
2022-04-13 10:42:33 +0200

    Add random state and random density matrix to tests

--- src/qibo/tests/test_noise.py
+++ src/qibo/tests/test_noise.py
@@ -3,11 +3,13 @@ import pytest
 from qibo import K, gates
 from qibo.models import Circuit
 from qibo.noise import *
-from qibo.tests.utils import random_density_matrix
+from qibo.tests.utils import random_density_matrix, random_state
 
+@pytest.mark.parametrize("density_matrix", [False, True])
+def test_pauli_error(backend, density_matrix):
 
-def test_pauli_error(backend):
-
+    np.random.seed(123)
+    K.set_seed(123)
     pauli = PauliError(0, 0.2, 0.3)
 
     noise = NoiseModel()
@@ -15,14 +17,14 @@ def test_pauli_error(backend):
     noise.add(pauli, "cx")
     noise.add(pauli, "z", (0,1))
 
-    circuit = Circuit(3)
+    circuit = Circuit(3, density_matrix=density_matrix)
     circuit.add(gates.CNOT(0,1))
     circuit.add(gates.Z(1))
     circuit.add(gates.X(1))
     circuit.add(gates.X(2))
     circuit.add(gates.Z(2))
 
-    target_circuit = Circuit(3)
+    target_circuit = Circuit(3, density_matrix=density_matrix)
     target_circuit.add(gates.PauliNoiseChannel(0, 0, 0.2, 0.3))
     target_circuit.add(gates.PauliNoiseChannel(1, 0, 0.2, 0.3))
     target_circuit.add(gates.CNOT(0,1))
@@ -33,15 +35,19 @@ def test_pauli_error(backend):
     target_circuit.add(gates.X(2))
     target_circuit.add(gates.Z(2))
 
+
+    initial_psi = random_density_matrix(3) if density_matrix else random_state(3)
     np.random.seed(123)
     K.set_seed(123)
-
-    final_state = noise.apply(circuit)()
-    target_final_state = target_circuit()
+    final_state = noise.apply(circuit)(initial_state=initial_psi)
+    np.random.seed(123)
+    K.set_seed(123)
+    target_final_state = target_circuit(initial_state=initial_psi)
 
     K.assert_allclose(final_state, target_final_state)
 
-def test_thermal_error(backend):
+@pytest.mark.parametrize("density_matrix", [False, True])
+def test_thermal_error(backend, density_matrix):
 
     thermal = ThermalRelaxationError(1, 1, 0.3)
 
@@ -50,33 +56,36 @@ def test_thermal_error(backend):
     noise.add(thermal, "cx")
     noise.add(thermal, "z", (0,1))
 
-    circuit = Circuit(3)
+    circuit = Circuit(3, density_matrix=density_matrix)
     circuit.add(gates.CNOT(0,1))
     circuit.add(gates.Z(1))
     circuit.add(gates.X(1))
     circuit.add(gates.X(2))
     circuit.add(gates.Z(2))
 
-    target_circuit = Circuit(3)
-    target_circuit.add(gates.ThermalRelaxationChannel(0, 1, 0.1, 0.2, 0.3))
-    target_circuit.add(gates.ThermalRelaxationChannel(1, 1, 0.1, 0.2, 0.3))
+    target_circuit = Circuit(3, density_matrix=density_matrix)
+    target_circuit.add(gates.ThermalRelaxationChannel(0, 1, 1, 0.3))
+    target_circuit.add(gates.ThermalRelaxationChannel(1, 1, 1, 0.3))
     target_circuit.add(gates.CNOT(0,1))
-    target_circuit.add(gates.ThermalRelaxationChannel(1, 1, 0.1, 0.2, 0.3))
+    target_circuit.add(gates.ThermalRelaxationChannel(1, 1, 1, 0.3))
     target_circuit.add(gates.Z(1))
-    target_circuit.add(gates.ThermalRelaxationChannel(1, 1, 0.1, 0.2, 0.3))
+    target_circuit.add(gates.ThermalRelaxationChannel(1, 1, 1, 0.3))
     target_circuit.add(gates.X(1))
     target_circuit.add(gates.X(2))
     target_circuit.add(gates.Z(2))
 
+    initial_psi = random_density_matrix(3) if density_matrix else random_state(3)
     np.random.seed(123)
     K.set_seed(123)
-
-    final_state = noise.apply(circuit)()
-    target_final_state = target_circuit()
+    final_state = noise.apply(circuit)(initial_state=initial_psi)
+    np.random.seed(123)
+    K.set_seed(123)
+    target_final_state = target_circuit(initial_state=initial_psi)
 
     K.assert_allclose(final_state, target_final_state)
 
-def test_reset_error(backend):
+@pytest.mark.parametrize("density_matrix", [False, True])
+def test_reset_error(backend, density_matrix):
 
     reset = ResetError(0.8, 0.2)
 
@@ -85,23 +94,25 @@ def test_reset_error(backend):
     noise.add(reset, "cx")
     noise.add(reset, "z", (0,1))
 
-    circuit = Circuit(3)
+    circuit = Circuit(3, density_matrix=density_matrix)
     circuit.add(gates.CNOT(0,1))
     circuit.add(gates.Z(1))
 
-    target_circuit = Circuit(3)
-    target_circuit.add(gates.ResetChannel(0, 0.5, 0.2))
-    target_circuit.add(gates.ResetChannel(1, 0.5, 0.2))
+    target_circuit = Circuit(3, density_matrix=density_matrix)
+    target_circuit.add(gates.ResetChannel(0, 0.8, 0.2))
+    target_circuit.add(gates.ResetChannel(1, 0.8, 0.2))
     target_circuit.add(gates.CNOT(0,1))
-    target_circuit.add(gates.ResetChannel(1, 0.5, 0.2))
+    target_circuit.add(gates.ResetChannel(1, 0.8, 0.2))
     target_circuit.add(gates.Z(1))
 
 
+    initial_psi = random_density_matrix(3) if density_matrix else random_state(3)
     np.random.seed(123)
     K.set_seed(123)
-
-    final_state = noise.apply(circuit)()
-    target_final_state = target_circuit()
+    final_state = noise.apply(circuit)(initial_state=initial_psi)
+    np.random.seed(123)
+    K.set_seed(123)
+    target_final_state = target_circuit(initial_state=initial_psi)
 
     K.assert_allclose(final_state, target_final_state)
 
