8818c39e03f4955d8a7ad2a6b9542b59be58aed6
Andrea Pasquale <andreapasquale97@gmail.com>
2022-04-13 08:42:05 +0200

    Add spaces

--- src/qibo/noise.py
+++ src/qibo/noise.py
@@ -5,15 +5,18 @@ class PauliError():
     self.options = px, py, pz
     self.channel = gates.PauliNoiseChannel
 
+
 class ThermalRelaxationError():
   def __init__(self, t1, t2, time, excited_population=0, seed=None):
     self.options = t1, t2, time, excited_population
     self.channel = gates.ThermalRelaxationChannel
 
+
 class ResetError():
   def __init__(self, p0, p1, seed=None):
     self.options = p0, p1
     self.channel = gates.ResetChannel
+
 
 class NoiseModel():
     """Generic noise model."""
