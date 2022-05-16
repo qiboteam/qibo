51e7022eecf52ec8fa801e62b8473dd32b57c123
shangtai <christo173@hotmail.com>
2022-05-16 22:52:07 +0800

    included an example to compute the histogram and the mode for various number of QAOA layers

--- src/qibo/models/tsp.py
+++ src/qibo/models/tsp.py
@@ -3,6 +3,7 @@ from qibo.models import Circuit, QAOA
 from qibo.core.hamiltonians import Hamiltonian, SymbolicHamiltonian
 from qibo import gates
 import numpy as np
+from collections import defaultdict
 
 
 def calculate_two_to_one(num_cities):
@@ -74,49 +75,64 @@ class tsp:
     """
     This is a TSP (traveling salesman problem) class that enables us to implement TSP according to
     `arxiv:1709.03489<https://arxiv.org/abs/1709.03489>` by Hadfield (2017).
-    ..testcode::
-    num_cities = 3
-    distance_matrix = np.array([[0, 0.9, 0.8],
-     [0.4, 0, 0.1],
-     [0,  0.7, 0]])
-    # there are two possible cycles, one with distance 1, one with distance 1.9
-    distance_matrix = distance_matrix.round(1)
-    small_tsp = tsp(distance_matrix)
-    obj_hamil, mixer = small_tsp.hamiltonians(dense=False)
-    initial_parameters = np.random.uniform(0, 1, 2)
-    initial_state = small_tsp.prepare_initial_state([i for i in range(num_cities)])
-    qaoa = QAOA(obj_hamil, mixer=mixer)
+    #..testcode::
+        num_cities = 3
+        distance_matrix = np.array([[0, 0.9, 0.8],
+         [0.4, 0, 0.1],
+         [0,  0.7, 0]])
+        # there are two possible cycles, one with distance 1, one with distance 1.9
+        distance_matrix = distance_matrix.round(1)
+        small_tsp = tsp(distance_matrix)
+        obj_hamil, mixer = small_tsp.hamiltonians(dense=False)
+        initial_parameters = np.random.uniform(0, 1, 2)
+        initial_state = small_tsp.prepare_initial_state([i for i in range(num_cities)])
+        qaoa = QAOA(obj_hamil, mixer=mixer)
 
 
-    def evaluate_dist(config):
-        m = int(np.sqrt(len(config)))
-        cauchy = [-1] * m  # Cauchy's notation for permutation
-        for i in range(m):
-            for j in range(m):
-                if config[m*i+j] == '1':
-                    cauchy[j] = i # citi i is in slot j
-        return sum(distance_matrix[cauchy[i]][cauchy[(i+1)%m]] for i in range(m))
+        def convert_to_standard_Cauchy(config):
+            m = int(np.sqrt(len(config)))
+            cauchy = [-1] * m  # Cauchy's notation for permutation, e.g. (1,2,0) or (2,0,1)
+            for i in range(m):
+                for j in range(m):
+                    if config[m * i + j] == '1':
+                        cauchy[j] = i  # citi i is in slot j
+            for i in range(m):
+                if cauchy[i] == 0:
+                    cauchy = cauchy[i:] + cauchy[:i]
+                    return tuple(cauchy)  # now, the cauchy notation for permutation begins with 0
 
 
-    def amplitude_config(long_config):
-        amplitude, config = long_config.split('|')
-        prob = np.linalg.norm(eval(amplitude))**2
-        dist = evaluate_dist(config)
-        return prob*dist
+
+        def evaluate_dist(cauchy):
+            #m = int(np.sqrt(len(config)))
+            m = len(cauchy)
+            return sum(distance_matrix[cauchy[i]][cauchy[(i+1)%m]] for i in range(m))
 
 
-    def qaoa_function_of_layer(layer):
-        best_energy, final_parameters, extra = qaoa.minimize(initial_p=[0.1] * 8,
-                                                             initial_state=initial_state)
-        qaoa.set_parameters(final_parameters)
-        quantum_state = qaoa.execute(initial_state)
-        quantum_state_symbolic = quantum_state.symbolic().split('>')[:-1]
-        return sum(amplitude_config(long_config) for long_config in quantum_state_symbolic)
+        def qaoa_function_of_layer(layer):
+            best_energy, final_parameters, extra = qaoa.minimize(initial_p=[0.1] * layer,
+                                                                 initial_state=initial_state)
+            qaoa.set_parameters(final_parameters)
+            quantum_state = qaoa.execute(initial_state)
+            # we have to measure the quantum_state here
+            meas = quantum_state.measure(gates.M(*range(9)), nshots = 1000)
+            freq_counter = meas.frequencies()
+            # let's combine freq_counter here, first convert each key and sum up the frequency
+            cauchy_dict = defaultdict(int)
+            for freq_key in freq_counter:
+                standard_cauchy_key = convert_to_standard_Cauchy(freq_key)
+                cauchy_dict[standard_cauchy_key] += freq_counter[freq_key]
+            print("for {} layers, this is the corresponding histogram:".format(layer))
+            print(cauchy_dict)
+            # find the mode and compute the corresponding distance
+            max_key = max(cauchy_dict, key=cauchy_dict.get)
+            return evaluate_dist(max_key)
 
 
-    print([qaoa_function_of_layer(i) for i in [2, 4, 6, 8]])
-    # example of output [1.12205683883, 1.7475528727300003, 1.26960151366, 1.4211151713499999]
-    """
+        print([qaoa_function_of_layer(i) for i in [2, 4, 6, 8]])
+        # we should obtain [1.9, 1.0, 1.0, 1.0]
+        # as the number of layers increases, it is more likely to obtain the optimal solution which is 1.0
+            """
 
     def __init__(self, distance_matrix):
         """
@@ -147,9 +163,3 @@ class tsp:
             c.add(gates.X(int(self.two_to_one[ordering[i], i])))
         result = c()
         return result.state(numpy=True)
-
-
-
-
-
-
