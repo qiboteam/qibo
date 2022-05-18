bad349b5fcf370113c094479b1b69ffa27a57903
Stavros <35475381+stavros11@users.noreply.github.com>
2022-05-18 16:02:01 +0400

    Implement matrices for all gates

--- src/qibo/engines/matrices.py
+++ src/qibo/engines/matrices.py
@@ -1,8 +1,9 @@
 import numpy as np
+from qibo.config import raise_error
 
 
 class Matrices:
-    # TODO: Implement matrices for all gates
+    """Matrix representation of every gate as a numpy array."""
 
     def __init__(self, dtype):
         self.dtype = dtype
@@ -51,6 +52,12 @@ class Matrices:
 
     def I(self):
         return np.eye(2, dtype=self.dtype)
+
+    def Align(self):
+        raise_error(NotImplementedError)
+
+    def M(self):
+        raise_error(NotImplementedError)
 
     def RX(self, theta):
         cos = np.cos(theta / 2.0) + 0j
@@ -116,6 +123,36 @@ class Matrices:
             [0, 0, 0, -1]
         ], dtype=self.dtype)
 
+    def CRX(self, theta):
+        m = np.eye(4, dtype=self.dtype)
+        m[2:, 2:] = self.RX(theta)
+        return m
+
+    def CRY(self, theta):
+        m = np.eye(4, dtype=self.dtype)
+        m[2:, 2:] = self.RY(theta)
+        return m
+
+    def CRZ(self, theta):
+        m = np.eye(4, dtype=self.dtype)
+        m[2:, 2:] = self.RZ(theta)
+        return m
+
+    def CU1(self, theta):
+        m = np.eye(4, dtype=self.dtype)
+        m[2:, 2:] = self.U1(theta)
+        return m
+
+    def CU2(self, phi, lam):
+        m = np.eye(4, dtype=self.dtype)
+        m[2:, 2:] = self.U2(phi, lam)
+        return m
+
+    def CU3(self, theta, phi, lam):
+        m = np.eye(4, dtype=self.dtype)
+        m[2:, 2:] = self.U3(theta, phi, lam)
+        return m
+
     def SWAP(self):
         return np.array([
             [1, 0, 0, 0], 
@@ -132,8 +169,58 @@ class Matrices:
             [0, 0, 0, -1]
         ], dtype=self.dtype)
 
+    def fSim(self, theta, phi):
+        cost = np.cos(theta) + 0j
+        isint = -1j * np.sin(theta)
+        phase = np.exp(-1j * phi)
+        return np.array([
+            [1, 0, 0, 0],
+            [0, cost, isint, 0],
+            [0, isint, cost, 0],
+            [0, 0, 0, phase],
+        ], dtype=self.dtype)
+
+    def GeneralizedfSim(self, u, phi):
+        phase = np.exp(-1j * phi)
+        return np.array([
+            [1, 0, 0, 0],
+            [0, u[0, 0], u[0, 1], 0],
+            [0, u[1, 0], u[1, 1], 0],
+            [0, 0, 0, phase],
+        ], dtype=self.dtype)
+
     def TOFFOLI(self):
         m = np.eye(8, dtype=self.dtype)
         m[-2, -2], m[-2, -1] = 0, 1
         m[-1, -2], m[-1, -1] = 1, 0
         return m
+
+    def Unitary(self, u):
+        return u.astype(self.dtype)
+
+    def VariationalLayer(self, *args):
+        raise_error(NotImplementedError)
+
+    def Flatten(self):
+        raise_error(NotImplementedError)
+
+    def CallbackGate(self):
+        raise_error(NotImplementedError)
+
+    def PartialTrace(self):
+        raise_error(NotImplementedError)
+
+    def UnitaryChannel(self):
+        raise_error(NotImplementedError)
+
+    def PauliNoiseChannel(self):
+        raise_error(NotImplementedError)
+
+    def ResetChannel(self):
+        raise_error(NotImplementedError)
+
+    def ThermalRelaxationChannel(self):
+        raise_error(NotImplementedError)
+
+    def FusedGate(self):
+        raise_error(NotImplementedError)
