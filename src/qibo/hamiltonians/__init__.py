3475d85e0e26b11e25dfab2d3d414a0bf8955997
Andrea Pasquale <andreapasquale97@gmail.com>
2022-06-06 10:33:01 +0200

    Fix typo

--- src/qibo/hamiltonians/__init__.py
+++ src/qibo/hamiltonians/__init__.py
@@ -1,3 +1,3 @@
 from qibo.hamiltonians.hamiltonians import *
 from qibo.hamiltonians.symbols import *
-from qibo.hamiltonians.models import X, Y, Z, XXZ, TFMI, MaxCut
+from qibo.hamiltonians.models import X, Y, Z, XXZ, TFIM, MaxCut
