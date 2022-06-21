5e866c6e6bed1a0680e0edf6cdb8f6918d722aae
shangtai <christo173@hotmail.com>
2022-06-22 00:15:55 +0800

    sphix documentation

--- src/qibo/models/tsp.py
+++ src/qibo/models/tsp.py
@@ -8,11 +8,9 @@ from qibo import gates
 def calculate_two_to_one(num_cities):
     """
     Args:
-
         num_cities: number of cities
 
     Returns:
-
         a matrix to faciliate conversion from 2 dimension to one dimension
 
     """
@@ -22,11 +20,9 @@ def calculate_two_to_one(num_cities):
 def tsp_phaser(distance_matrix):
     """
     Args:
-
         distance_matrix: a numpy matrix encoding the distances
 
     Returns:
-
         a hamiltonian representing the TSP(traveling salesman problem) phaser according to
     `arxiv:1709.03489<https://arxiv.org/abs/1709.03489>`_ by Hadfield (2017).
 
@@ -47,11 +43,9 @@ def tsp_phaser(distance_matrix):
 def tsp_mixer(num_cities):
     """
     Args:
-
         num_cities: number of cities in TSP
 
     Returns:
-
         a mixer hamiltonian representing the TSP(traveling salesman problem) phaser
     according to
     `arxiv:1709.03489<https://arxiv.org/abs/1709.03489>` by Hadfield (2017).
@@ -77,19 +71,21 @@ class TSP:
     """
     This is a TSP (traveling salesman problem) class that enables us to implement TSP according to
     `arxiv:1709.03489<https://arxiv.org/abs/1709.03489>`_ by Hadfield (2017).
+
+    Example:
     ..testcode::
+        from collections import defaultdict
         from qibo.models import QAOA
+        np.random.seed(42)
         num_cities = 3
         distance_matrix = np.array([[0, 0.9, 0.8],
-         [0.4, 0, 0.1],
-         [0,  0.7, 0]])
+                                    [0.4, 0, 0.1],
+                                    [0, 0.7, 0]])
         # there are two possible cycles, one with distance 1, one with distance 1.9
         distance_matrix = distance_matrix.round(1)
         small_tsp = TSP(distance_matrix)
-        obj_hamil, mixer = small_tsp.hamiltonians()
         initial_parameters = np.random.uniform(0, 1, 2)
         initial_state = small_tsp.prepare_initial_state([i for i in range(num_cities)])
-        qaoa = QAOA(obj_hamil, mixer=mixer)
 
 
         def convert_to_standard_Cauchy(config):
@@ -107,39 +103,39 @@ class TSP:
 
 
         def evaluate_dist(cauchy):
-           '''
-           Given a permutation of 0 to n-1, we compute the distance of the tour
-           '''
+            '''
+            Given a permutation of 0 to n-1, we compute the distance of the tour
+            '''
             m = len(cauchy)
             return sum(distance_matrix[cauchy[i]][cauchy[(i+1)%m]] for i in range(m))
 
 
-        def qaoa_function_of_layer(layer):
-           '''
-           this is a function to study the impact of the number of layers on QAOA, it takes
-           in the number of layers and compute the distance of the mode of the histogram obtained
-           from QAOA
-           '''
-            best_energy, final_parameters, extra = qaoa.minimize(initial_p=[0.1] * layer,
-                                                                 initial_state=initial_state)
+        def qaoa_function_of_layer(layer, distance_matrix):
+            '''
+            This is a function to study the impact of the number of layers on QAOA, it takes
+            in the number of layers and compute the distance of the mode of the histogram obtained
+            from QAOA
+            '''
+            small_tsp = TSP(distance_matrix)
+            obj_hamil, mixer = small_tsp.hamiltonians()
+            qaoa = QAOA(obj_hamil, mixer=mixer)
+            best_energy, final_parameters, extra = qaoa.minimize(initial_p=[0.1 for i in range(layer)] ,
+                                                                 initial_state=initial_state, method='BFGS')
             qaoa.set_parameters(final_parameters)
             quantum_state = qaoa.execute(initial_state)
-            meas = quantum_state.measure(gates.M(*range(9)), nshots = 1000)
+            meas = quantum_state.measure(gates.M(*range(9)), nshots=1000)
             freq_counter = meas.frequencies()
             # let's combine freq_counter here, first convert each key and sum up the frequency
             cauchy_dict = defaultdict(int)
             for freq_key in freq_counter:
                 standard_cauchy_key = convert_to_standard_Cauchy(freq_key)
                 cauchy_dict[standard_cauchy_key] += freq_counter[freq_key]
-            print("for {} layers, this is the corresponding histogram:".format(layer))
-            print(cauchy_dict)
-            # find the mode and compute the corresponding distance
             max_key = max(cauchy_dict, key=cauchy_dict.get)
             return evaluate_dist(max_key)
 
 
-        print([qaoa_function_of_layer(i) for i in [2, 4, 6, 8]])
-        # we should obtain an array such as [1.9, 1.0, 1.0, 1.9]
+        print([qaoa_function_of_layer(i, distance_matrix) for i in [2, 4, 6, 8]])
+        # we should obtain the array [1.0, 1.0, 1.0, 1.9]
     """
 
     def __init__(self, distance_matrix):
@@ -156,16 +152,16 @@ class TSP:
     def hamiltonians(self):
         """
         Returns:
-
             Return a pair of Hamiltonian for the objective as well as the mixer.
+
         """
         return tsp_phaser(self.distance_matrix), tsp_mixer(self.num_cities)
 
     def prepare_initial_state(self, ordering):
         """
         To run QAOA by Hadsfield, we need to start from a valid permutation function to ensure feasibility.
-        Args:
 
+        Args:
             ordering is a list which is a permutation from 0 to n-1
 
         Returns:
