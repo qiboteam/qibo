5db75c1506c3ace90c22292f422ca66af28b6009
shangtai <christo173@hotmail.com>
2022-06-23 03:07:43 +0800

    debugging

--- src/qibo/models/tsp.py
+++ src/qibo/models/tsp.py
@@ -60,9 +60,7 @@ class TSP:
             from qibo.models import QAOA
             np.random.seed(42)
             num_cities = 3
-            distance_matrix = np.array([[0, 0.9, 0.8],
-                                        [0.4, 0, 0.1],
-                                        [0, 0.7, 0]])
+            distance_matrix = np.array([[0, 0.9, 0.8], [0.4, 0, 0.1],[0, 0.7, 0]])
             distance_matrix = distance_matrix.round(1)
             small_tsp = TSP(distance_matrix)
             initial_parameters = np.random.uniform(0, 1, 2)
