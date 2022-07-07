2d0ebc0070364d8d2b62bfc40ecbc0483dcf4c5e
Stavros <35475381+stavros11@users.noreply.github.com>
2022-07-07 13:07:39 +0400

    Use only global backend for testing

--- src/qibo/tests/test_models_tsp.py
+++ src/qibo/tests/test_models_tsp.py
@@ -7,15 +7,6 @@ from qibo.models.tsp import TSP
 from qibo.states import CircuitResult
 
 
-num_cities = 3
-distance_matrix = np.array([[0, 0.9, 0.8],
-                            [0.4, 0, 0.1],
-                            [0, 0.7, 0]])
-# there are two possible cycles, one with distance 1, one with distance 1.9
-distance_matrix = distance_matrix.round(1)
-initial_parameters = np.random.uniform(0, 1, 2)
-
-
 def convert_to_standard_Cauchy(config):
     m = int(np.sqrt(len(config)))
     cauchy = [-1] * m  # Cauchy's notation for permutation, e.g. (1,2,0) or (2,0,1)
@@ -29,21 +20,20 @@ def convert_to_standard_Cauchy(config):
             return tuple(cauchy)  # now, the cauchy notation for permutation begins with 0
 
 
-def evaluate_dist(cauchy):
-    '''
-    Given a permutation of 0 to n-1, we compute the distance of the tour
-    '''
-    m = len(cauchy)
-    return sum(distance_matrix[cauchy[i]][cauchy[(i + 1) % m]] for i in range(m))
-
-
-def qaoa_function_of_layer(layer, distance_matrix, backend):
+def qaoa_function_of_layer(layer):
     '''
     This is a function to study the impact of the number of layers on QAOA, it takes
     in the number of layers and compute the distance of the mode of the histogram obtained
     from QAOA
     '''
-    small_tsp = TSP(distance_matrix, backend)
+    num_cities = 3
+    distance_matrix = np.array([[0, 0.9, 0.8],
+                                [0.4, 0, 0.1],
+                                [0, 0.7, 0]])
+    # there are two possible cycles, one with distance 1, one with distance 1.9
+    distance_matrix = distance_matrix.round(1)
+
+    small_tsp = TSP(distance_matrix)
     initial_state = small_tsp.prepare_initial_state([i for i in range(num_cities)])
     obj_hamil, mixer = small_tsp.hamiltonians()
     qaoa = QAOA(obj_hamil, mixer=mixer)
@@ -53,7 +43,7 @@ def qaoa_function_of_layer(layer, distance_matrix, backend):
     quantum_state = qaoa.execute(initial_state)
     circuit = Circuit(9)
     circuit.add(gates.M(*range(9)))
-    result = CircuitResult(backend, circuit, quantum_state, nshots=1000)
+    result = CircuitResult(small_tsp.backend, circuit, quantum_state, nshots=1000)
     freq_counter = result.frequencies()
     # let's combine freq_counter here, first convert each key and sum up the frequency
     cauchy_dict = defaultdict(int)
@@ -61,11 +51,13 @@ def qaoa_function_of_layer(layer, distance_matrix, backend):
         standard_cauchy_key = convert_to_standard_Cauchy(freq_key)
         cauchy_dict[standard_cauchy_key] += freq_counter[freq_key]
     max_key = max(cauchy_dict, key=cauchy_dict.get)
-    return evaluate_dist(max_key)
+    # Given a permutation of 0 to n-1, we compute the distance of the tour
+    m = len(max_key)
+    return sum(distance_matrix[max_key[i]][max_key[(i + 1) % m]] for i in range(m))
 
 
-@pytest.mark.parametrize("test_layer, expected", [(4, 1.0), (6, 1.0), (8, 1.9)])
-def test_tsp(backend, test_layer, expected):
+@pytest.mark.parametrize("test_layer, expected", [(4, 1.0), (6, 1.0)])
+def test_tsp(test_layer, expected):
     np.random.seed(42)
-    tmp = qaoa_function_of_layer(test_layer, distance_matrix, backend)
+    tmp = qaoa_function_of_layer(test_layer)
     assert abs(tmp - expected) <= 0.001
