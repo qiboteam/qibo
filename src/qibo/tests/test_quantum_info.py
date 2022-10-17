94daf296109fffbebb05e9e1b7557de7410bd0bc
Renato Mello <renato.msf@gmail.com>
2022-10-17 20:58:26 +0400

    add shannon_entropy() test

---
+++ src/qibo/tests/test_quantum_info.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pytest
+
+from qibo.quantum_info import *
+
+@pytest.mark.parametrize("base", [2, 10, np.e, 5])
+def test_shannon_entropy(backend, base):
+    prob_array = np.asarray([1., 0.])
+    result = shannon_entropy(prob_array, base)
+    backend.assert_allclose(result, 0.0)
+
+    if base == 2:
+        prob_array = np.asarray([0.5, 0.5])
+        result = shannon_entropy(prob_array, base)
+        backend.assert_allclose(result, 2.)
+
+def test_shannon_entropy_errors():
+    with pytest.raises(ValueError):
+        x = np.asarray([1., 0.])
+        shannon_entropy(x, -2)
+    with pytest.raises(TypeError):
+        x = np.asarray([[1., 0.]])
+        shannon_entropy(x)
+    with pytest.raises(TypeError):
+        x = np.asarray([])
+        shannon_entropy(x)
+    with pytest.raises(ValueError):
+        x = np.asarray([0.5, 0.4999999])
+        shannon_entropy(x)
