23e066f2c2dda76b568dffedffca976dc39ddae4
Renato Mello <renato.msf@gmail.com>
2022-10-17 09:23:37 +0400

    identation

--- src/qibo/metrics/metrics.py
+++ src/qibo/metrics/metrics.py
@@ -126,7 +126,9 @@ def process_fidelity(channel, target=None):
         # With no target, return process fidelity with Identity channel
         return np.trace(channel) / d**2
     else:
-        return np.trace(np.dot(np.conj(np.transpose(channel)), target)) / d**2
+        return np.trace(
+            np.dot(np.conj(np.transpose(channel)), target)
+        ) / d**2
 
 
 def average_gate_fidelity(channel, target=None):
