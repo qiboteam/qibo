fd141ab1c42244fea717786242c796da903cf992
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-11-11 09:12:07 +0100

    from Standard to Furo doc's theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -25,7 +26,7 @@ copyright = "2020-2022 by the Qibo team"
 author = "The Qibo team"
 
 # The full version, including alpha/beta/rc tags
-release = qibo.__version__
+#release = qibo.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -71,7 +72,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#6400FF",
+        "color-brand-secondary": "#6400FF",
+        "color-brand-content": "#6400FF",
+    }
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -102,6 +111,6 @@ def setup(app):
     app.add_css_file("css/style.css")
 
 
-html_logo = "logo.png"
+#html_logo = "logo.png"
 
 html_show_sourcelink = False
