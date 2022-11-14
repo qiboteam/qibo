39aa3e466daae198c00ad074f9f8174ce0874d8f
Renato Mello <renato.msf@gmail.com>
2022-11-14 15:55:29 +0400

    add several typeerror checks and import MAX_ITERATIONS

--- src/qibo/quantum_info/Random.py
+++ src/qibo/quantum_info/Random.py
@@ -1,6 +1,7 @@
+#%%
 import numpy as np
 
-from qibo.config import PRECISION_TOL, raise_error
+from qibo.config import MAX_ITERATIONS, PRECISION_TOL, raise_error
 
 
 def random_ginibre_unitary_matrix(dims: int, rank: int = None):
@@ -34,6 +35,8 @@ def random_hermitian_operator(
 
     if dims <= 0:
         raise_error(ValueError, f"dims ({dims}) must be type int and positive.")
+    if not isinstance(semidefinite, bool) or not isinstance(normalize, bool):
+        raise_error(TypeError, f"semidefinite and normalize must be type bool.")
 
     operator = random_ginibre_unitary_matrix(dims, dims)
     if semidefinite:
@@ -42,7 +45,7 @@ def random_hermitian_operator(
         operator = (operator + np.transpose(np.conj(operator))) / 2
 
     if normalize:
-        operator = operator / np.trace(operator)
+        operator = operator / np.linalg.norm(operator)  # / np.trace(operator)
 
     return operator
 
@@ -53,8 +56,13 @@ def random_unitary(dims: int, measure: str = "haar"):
     if dims <= 0:
         raise_error(ValueError, f"dims must be type int and positive.")
 
-    if measure is not None and measure != "haar":
-        raise_error(ValueError, f"measure {measure} not implemented.")
+    if measure is not None:
+        if not isinstance(measure, str):
+            raise_error(
+                TypeError, f"measure must be type str but it is type {type(measure)}."
+            )
+        if measure != "haar":
+            raise_error(ValueError, f"measure {measure} not implemented.")
 
     if measure == "haar":
         gaussian_matrix = random_ginibre_unitary_matrix(dims, dims)
@@ -85,15 +93,15 @@ def random_unitary(dims: int, measure: str = "haar"):
     return unitary
 
 
-def random_statevector(dim: int):
+def random_statevector(dims: int):
     """."""
 
-    if dim <= 0:
+    if dims <= 0:
         raise_error(ValueError, "dim must be of type int and >= 1")
 
-    probabilities = np.random.rand(dim)
+    probabilities = np.random.rand(dims)
     probabilities = probabilities / np.sum(probabilities)
-    phases = 2 * np.pi * np.random.rand(dim)
+    phases = 2 * np.pi * np.random.rand(dims)
 
     return np.sqrt(probabilities) * np.exp(1.0j * phases)
 
@@ -105,10 +113,16 @@ def random_density_matrix(
 
     if dims <= 0:
         raise_error(ValueError, f"dims must be type int and positive.")
-    if rank > dims:
+    if rank is not None and rank > dims:
         raise_error(ValueError, f"rank ({rank}) cannot be greater than dims ({dims}).")
-    if rank <= 0:
+    if rank is not None and rank <= 0:
         raise_error(ValueError, f"rank ({rank}) must be an int between 1 and dims.")
+    if not isinstance(pure, bool):
+        raise_error(TypeError, f"pure must be type bool, but it is type {type(pure)}.")
+    if not isinstance(method, str):
+        raise_error(
+            TypeError, f"method must be type str, but it is type {type(method)}."
+        )
 
     if pure:
         state = random_statevector(dims)
@@ -132,20 +146,38 @@ def random_density_matrix(
 def stochastic_matrix(
     dims: int,
     bistochastic: bool = False,
-    max_iterations: int = None,
     precision_tol: float = None,
+    max_iterations: int = None,
 ):
     """."""
-
-    if precision_tol is not None and precision_tol < 0.0:
-        raise_error(ValueError, f"precision_tol must be non-negative.")
-    if max_iterations is not None and max_iterations <= 0.0:
-        raise_error(ValueError, f"precision_tol must be non-negative.")
+    if dims <= 0:
+        raise_error(ValueError, f"dims must be type int and positive.")
+    if not isinstance(bistochastic, bool):
+        raise_error(
+            TypeError,
+            f"bistochastic must be type bool, but it is type {type(bistochastic)}.",
+        )
+    if precision_tol is not None:
+        if not isinstance(precision_tol, float):
+            raise_error(
+                TypeError,
+                f"precision_tol must be type float, but it is type {type(precision_tol)}.",
+            )
+        if precision_tol < 0.0:
+            raise_error(ValueError, f"precision_tol must be non-negative.")
+    if max_iterations is not None:
+        if not isinstance(max_iterations, int):
+            raise_error(
+                TypeError,
+                f"max_iterations must be type int, but it is type {type(precision_tol)}.",
+            )
+        if max_iterations <= 0.0:
+            raise_error(ValueError, f"max_iterations must be a positive int.")
 
     if precision_tol is None:
         precision_tol = PRECISION_TOL
     if max_iterations is None:
-        max_iterations = 20
+        max_iterations = MAX_ITERATIONS
 
     matrix = np.random.rand(dims, dims)
     row_sum = matrix.sum(axis=1)
@@ -153,9 +185,8 @@ def stochastic_matrix(
     if bistochastic:
         column_sum = matrix.sum(axis=0)
         count = 0
-        while (
-            count <= max_iterations - 1
-            and (
+        while count <= max_iterations - 1 and (
+            (
                 np.any(row_sum >= 1 + precision_tol)
                 or np.any(row_sum <= 1 - precision_tol)
             )
@@ -169,10 +200,10 @@ def stochastic_matrix(
             row_sum = matrix.sum(axis=1)
             column_sum = matrix.sum(axis=0)
             count += 1
-        if count == max_iterations - 1:
+        if count == max_iterations:
             import warnings
 
-            warnings.warn("Reached max iterations.", RuntimeError)
+            warnings.warn("Reached max iterations.", RuntimeWarning)
     else:
         matrix = matrix / np.outer(row_sum, [1] * dims)
 
