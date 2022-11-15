6cfe52fb1094f2f54af3a590b0b676c7bccd5c26
Alejandro Sopena <alexsopena15@gmail.com>
2022-11-15 12:15:07 +0100

    Updated tests

--- src/qibo/tests/test_error_mitigation_cdr.py
+++ src/qibo/tests/test_error_mitigation_cdr.py
@@ -15,26 +15,26 @@ def get_noise_model(error):
     return noise
 
 
-@pytest.mark.parametrize("nqubits", [2, 3, 4])
+@pytest.mark.parametrize("nqubits", [4])
 @pytest.mark.parametrize(
     "noise",
-    [get_noise_model(DepolarizingError(0.1)), get_noise_model(PauliError(pz=0.1))],
+    [get_noise_model(DepolarizingError(0.1))],
 )
 def test_cdr(nqubits, noise):
     """Test that CDR reduces the noise."""
     # Define the circuit
-    nlayers = 3
-    c = Circuit(nqubits, density_matrix=True)
+    nlayers=3
+    c = Circuit(nqubits, density_matrix = True)
     for l in range(nlayers):
-        c.add(gates.RY(q, np.pi / 2) for q in range(nqubits))
-        c.add(gates.RZ(q, theta=np.random.rand() * 2 * np.pi) for q in range(nqubits))
-        c.add(gates.CNOT(q, q + 1) for q in range(0, nqubits - 1, 2))
-        c.add(gates.RZ(q, theta=np.random.rand() * 2 * np.pi) for q in range(nqubits))
-        c.add(gates.CNOT(q, q + 1) for q in range(1, nqubits - 2, 2))
-        c.add(gates.CNOT(0, nqubits - 1))
-        c.add(gates.RZ(q, theta=np.random.rand() * 2 * np.pi) for q in range(nqubits))
-    c.add(gates.M(*range(nqubits)))
-
+        c.add((gates.RY(q,theta=np.random.rand()*2*np.pi)for q in range(nqubits)))
+        c.add((gates.RZ(q, theta=np.random.rand()*2*np.pi) for q in range(nqubits)))
+        c.add((gates.CNOT(q, q+1) for q in range(0, nqubits-1, 2)))
+        c.add((gates.RZ(q, theta=np.random.rand()*2*np.pi) for q in range(nqubits)))
+        c.add((gates.CNOT(q, q+1) for q in range(1, nqubits-2, 2)))
+        c.add((gates.RY(q,theta=np.random.rand()*2*np.pi)for q in range(nqubits)))
+        c.add(gates.CNOT(0, nqubits-1))
+        c.add((gates.RZ(q, theta=np.random.rand()*2*np.pi) for q in range(nqubits)))
+    c.add(gates.M(q) for q in range(nqubits))
     # Define the observable
     obs = np.prod([Z(i) for i in range(nqubits)])
     obs = SymbolicHamiltonian(obs)
@@ -42,5 +42,5 @@ def test_cdr(nqubits, noise):
     # Noise-free expected value
     exact = obs.expectation(c().state())
     noisy = obs.expectation(noise.apply(c)().state())
-    estimate = CDR(circuit=c, observable=obs, noise_model=noise, nshots=10000)
+    estimate = CDR(circuit=c, observable=obs, backend=None, noise_model=noise, nshots=10000)
     assert np.abs(exact - estimate) <= np.abs(exact - noisy)
--- src/qibo/tests/test_error_mitigation_zne.py
+++ src/qibo/tests/test_error_mitigation_zne.py
@@ -14,25 +14,26 @@ def get_noise_model(error):
     return noise
 
 
-@pytest.mark.parametrize("nqubits", [2, 3, 4])
+@pytest.mark.parametrize("nqubits", [4])
 @pytest.mark.parametrize(
     "noise",
-    [get_noise_model(DepolarizingError(0.1)), get_noise_model(PauliError(pz=0.1))],
+    [get_noise_model(DepolarizingError(0.1))],
 )
 def test_zne(nqubits, noise):
     """Test that ZNE reduces the noise."""
     # Define the circuit
-    nlayers = 3
-    c = Circuit(nqubits, density_matrix=True)
+    nlayers=3
+    c = Circuit(nqubits, density_matrix = True)
     for l in range(nlayers):
-        c.add(gates.RX(q, np.pi / 2) for q in range(nqubits))
-        c.add(gates.RZ(q, theta=np.random.rand() * 2 * np.pi) for q in range(nqubits))
-        c.add(gates.CNOT(q, q + 1) for q in range(0, nqubits - 1, 2))
-        c.add(gates.RZ(q, theta=np.random.rand() * 2 * np.pi) for q in range(nqubits))
-        c.add(gates.CNOT(q, q + 1) for q in range(1, nqubits - 2, 2))
-        c.add(gates.CNOT(0, nqubits - 1))
-        c.add(gates.RZ(q, theta=np.random.rand() * 2 * np.pi) for q in range(nqubits))
-    c.add(gates.M(*range(nqubits)))
+        c.add((gates.RY(q,theta=np.random.rand()*2*np.pi)for q in range(nqubits)))
+        c.add((gates.RZ(q, theta=np.random.rand()*2*np.pi) for q in range(nqubits)))
+        c.add((gates.CNOT(q, q+1) for q in range(0, nqubits-1, 2)))
+        c.add((gates.RZ(q, theta=np.random.rand()*2*np.pi) for q in range(nqubits)))
+        c.add((gates.CNOT(q, q+1) for q in range(1, nqubits-2, 2)))
+        c.add((gates.RY(q,theta=np.random.rand()*2*np.pi)for q in range(nqubits)))
+        c.add(gates.CNOT(0, nqubits-1))
+        c.add((gates.RZ(q, theta=np.random.rand()*2*np.pi) for q in range(nqubits)))
+    c.add(gates.M(q) for q in range(nqubits))
     # Define the observable
     obs = np.prod([Z(i) for i in range(nqubits)])
     obs = hamiltonians.SymbolicHamiltonian(obs)
@@ -40,6 +41,5 @@ def test_zne(nqubits, noise):
     # Noise-free expected value
     exact = obs.expectation(c().state())
     noisy = obs.expectation(noise.apply(c)().state())
-    for j in range(2, 7):
-        estimate = ZNE(circuit=c, observable=obs, c=range(j), noise_model=noise)
-        assert np.abs(exact - estimate) <= np.abs(exact - noisy)
+    estimate = ZNE(circuit=c, observable=obs, backend=None, noise_levels=range(5), noise_model=noise, nshots=10000)
+    assert np.abs(exact - estimate) <= np.abs(exact - noisy)
