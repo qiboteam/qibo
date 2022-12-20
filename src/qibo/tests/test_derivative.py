14ab23823afa1056cd0267983d787df291337894
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-12-20 10:09:43 +0100

    add assertions and backend deps in test_derivative

--- src/qibo/tests/test_derivative.py
+++ src/qibo/tests/test_derivative.py
@@ -8,10 +8,8 @@ from qibo.models import Circuit
 
 
 # defining an observable
-def hamiltonian(nqubits=1):
-    m0 = (1 / nqubits) * hamiltonians.Z(nqubits).matrix
-    ham = hamiltonians.Hamiltonian(nqubits, m0)
-    return ham
+def hamiltonian(nqubits, backend):
+    return (1 / nqubits) * hamiltonians.Z(nqubits, backend=backend)
 
 
 # defining a dummy circuit
@@ -25,16 +23,17 @@ def circuit(nqubits=1):
     return c
 
 
-def test_derivative():
+def test_derivative(backend):
 
     # initializing the circuit
     c = circuit(nqubits=1)
 
     # some parameters
-    test_params = np.random.randn(3)
+    # we know the derivative's values with these params
+    test_params = np.linspace(0.1, 1, 3)
     c.set_parameters(test_params)
 
-    test_hamiltonian = hamiltonian()
+    test_hamiltonian = hamiltonian(nqubits=1, backend=backend)
 
     # testing parameter out of bounds
     with pytest.raises(ValueError):
@@ -46,7 +45,14 @@ def test_derivative():
     with pytest.raises(TypeError):
         grad_0 = parameter_shift(circuit=c, hamiltonian=c, parameter_shift=0)
 
+    # executing all the procedure
     grad_0 = parameter_shift(circuit=c, hamiltonian=test_hamiltonian, parameter_index=0)
     grad_1 = parameter_shift(circuit=c, hamiltonian=test_hamiltonian, parameter_index=1)
+    grad_2 = parameter_shift(circuit=c, hamiltonian=test_hamiltonian, parameter_index=2)
 
     assert isinstance(test_hamiltonian, hamiltonians.AbstractHamiltonian)
+    # check of known values
+    # calculated using tf.GradientTape
+    assert round(grad_0, 10) == 8.51104358e-02
+    assert round(grad_1, 8) == 5.20075970e-01
+    assert round(grad_2, 10) == 0.0000000000
