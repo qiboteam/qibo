3abebc206d69bbc932b59349a7e8738810a09858
DaniloKolesnyk <danylo.kolesnyk@tii.ae>
2022-12-21 12:40:22 +0400

    Added a function for plotting density as a 3d histogram

---
+++ src/qibo/tests/test_histogram_density.py
@@ -0,0 +1,11 @@
+from histogram_density import plot_density_hist
+from qibo import models, gates
+
+def test_plot_density_hist():
+    c = models.Circuit(2)
+    c.add(gates.H(0))
+    c.add(gates.CNOT(0, 1))
+
+    plot_density_hist(c)
+
+test_plot_density_hist()
