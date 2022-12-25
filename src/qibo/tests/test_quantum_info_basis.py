5b69ae602e2541feba9fdc326fd56d45b2d55bff
Renato Mello <renato.msf@gmail.com>
2022-12-25 12:53:07 +0400

    add tests for generalized vectorization and unvectorization

--- src/qibo/tests/test_quantum_info_basis.py
+++ src/qibo/tests/test_quantum_info_basis.py
@@ -6,93 +6,140 @@ from qibo.config import PRECISION_TOL
 from qibo.quantum_info import *
 
 
+@pytest.mark.parametrize("order", ["row", "column", "system"])
 @pytest.mark.parametrize("nqubits", [1, 2, 3])
-def test_vectorization(nqubits):
+def test_vectorization(nqubits, order):
     with pytest.raises(TypeError):
         vectorization(np.array([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]]))
     with pytest.raises(TypeError):
-        vectorization(np.array([[[0.0, 0.0], [0.0, 0.0]], [[0.0, 0.0]]]))
+        vectorization(
+            np.array([[[0.0, 0.0], [0.0, 0.0]], [[0.0, 0.0]]], dtype="object")
+        )
     with pytest.raises(TypeError):
         vectorization(np.array([]))
+    with pytest.raises(TypeError):
+        vectorization(random_statevector(4), order=1)
+    with pytest.raises(ValueError):
+        vectorization(random_statevector(4), order="1")
+
+    d = 2**nqubits
 
     if nqubits == 1:
-        matrix_test = [0, 2, 1, 3]
+        if order == "system" or order == "column":
+            matrix_test = [0, 2, 1, 3]
+        else:
+            matrix_test = [0, 1, 2, 3]
     elif nqubits == 2:
-        matrix_test = [0, 4, 1, 5, 8, 12, 9, 13, 2, 6, 3, 7, 10, 14, 11, 15]
+        if order == "row":
+            matrix_test = np.arange(d**2)
+        elif order == "column":
+            matrix_test = np.arange(d**2)
+            matrix_test = np.reshape(matrix_test, (d, d))
+            matrix_test = np.reshape(matrix_test, (1, -1), order="F")[0]
+        else:
+            matrix_test = [0, 4, 1, 5, 8, 12, 9, 13, 2, 6, 3, 7, 10, 14, 11, 15]
     else:
-        matrix_test = [
-            0,
-            8,
-            1,
-            9,
-            16,
-            24,
-            17,
-            25,
-            2,
-            10,
-            3,
-            11,
-            18,
-            26,
-            19,
-            27,
-            32,
-            40,
-            33,
-            41,
-            48,
-            56,
-            49,
-            57,
-            34,
-            42,
-            35,
-            43,
-            50,
-            58,
-            51,
-            59,
-            4,
-            12,
-            5,
-            13,
-            20,
-            28,
-            21,
-            29,
-            6,
-            14,
-            7,
-            15,
-            22,
-            30,
-            23,
-            31,
-            36,
-            44,
-            37,
-            45,
-            52,
-            60,
-            53,
-            61,
-            38,
-            46,
-            39,
-            47,
-            54,
-            62,
-            55,
-            63,
-        ]
+        if order == "row":
+            matrix_test = np.arange(d**2)
+        elif order == "column":
+            matrix_test = np.arange(d**2)
+            matrix_test = np.reshape(matrix_test, (d, d))
+            matrix_test = np.reshape(matrix_test, (1, -1), order="F")[0]
+        else:
+            matrix_test = [
+                0,
+                8,
+                1,
+                9,
+                16,
+                24,
+                17,
+                25,
+                2,
+                10,
+                3,
+                11,
+                18,
+                26,
+                19,
+                27,
+                32,
+                40,
+                33,
+                41,
+                48,
+                56,
+                49,
+                57,
+                34,
+                42,
+                35,
+                43,
+                50,
+                58,
+                51,
+                59,
+                4,
+                12,
+                5,
+                13,
+                20,
+                28,
+                21,
+                29,
+                6,
+                14,
+                7,
+                15,
+                22,
+                30,
+                23,
+                31,
+                36,
+                44,
+                37,
+                45,
+                52,
+                60,
+                53,
+                61,
+                38,
+                46,
+                39,
+                47,
+                54,
+                62,
+                55,
+                63,
+            ]
     matrix_test = np.array(matrix_test)
 
     d = 2**nqubits
     matrix = np.arange(d**2).reshape((d, d))
-    matrix = vectorization(matrix)
+    matrix = vectorization(matrix, order)
 
     assert np.linalg.norm(matrix - matrix_test) < PRECISION_TOL
+
+
+@pytest.mark.parametrize("order", ["row", "column"])
+@pytest.mark.parametrize("nqubits", [2, 3])
+def test_unvectorization(nqubits, order):
+    with pytest.raises(TypeError):
+        unvectorization(random_density_matrix(2**nqubits))
+    with pytest.raises(TypeError):
+        unvectorization(random_statevector(4**nqubits), order=1)
+    with pytest.raises(ValueError):
+        unvectorization(random_statevector(4**2), order="1")
+    with pytest.raises(NotImplementedError):
+        unvectorization(random_statevector(4**2), order="system")
+
+    d = 2**nqubits
+    matrix_test = random_density_matrix(d)
+
+    matrix = vectorization(matrix_test, order)
+    matrix = unvectorization(matrix, order)
+
+    assert np.linalg.norm(matrix_test - matrix) < PRECISION_TOL
 
 
 @pytest.mark.parametrize("vectorize", [False, True])
