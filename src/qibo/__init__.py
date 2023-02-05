b7c9babff45fbe7981059d0346ebaba919824a1b
Stefano Carrazza <stefano.carrazza@cern.ch>
2023-02-05 10:37:08 +0100

    increase version

--- src/qibo/__init__.py
+++ src/qibo/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.11"
+__version__ = "0.1.12.dev0"
 from qibo import callbacks, gates, hamiltonians, models, optimizers, parallel, solvers
 from qibo.backends import (
     get_backend,
