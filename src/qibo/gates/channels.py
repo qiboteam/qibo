9b21c9b830abe0f0e2e9eda9d7393a26542571f9
vodovozovaliza <vodovozovalizza@gmail.com>
2023-04-24 14:16:11 +0400

    Remove comments

--- src/qibo/gates/channels.py
+++ src/qibo/gates/channels.py
@@ -543,8 +543,6 @@ class ThermalRelaxationChannel(KrausChannel):
         preset0 = p_reset * (1 - excited_population)
         preset1 = p_reset * excited_population
 
-        # import numpy as np
-
         if t_1 < t_2:
             exp_t2 = exp(-time / t_2)
 
@@ -644,8 +642,6 @@ class ReadoutErrorChannel(KrausChannel):
         if isinstance(q, int) is True:
             q = (q,)
 
-        # import numpy as np
-
         dim = len(probabilities)
         operators = []
         for j in range(dim):
@@ -677,8 +673,6 @@ class ResetChannel(KrausChannel):
     """
 
     def __init__(self, q, p0=0.0, p1=0.0):
-        # import numpy as np
-
         if p0 < 0:
             raise_error(ValueError, "Invalid p0 ({p0} < 0).")
         if p1 < 0:
