624f1ff6687fc1730afe93b90b7ebc1a01cd2305
Renato Mello <renato.msf@gmail.com>
2023-05-01 09:58:06 +0400

    remove old Pauli channel from tests

--- tests/test_noise.py
+++ tests/test_noise.py
@@ -126,60 +126,11 @@ def test_unitary_error(backend, density_matrix, nshots):
 @pytest.mark.parametrize("density_matrix", [False, True])
 @pytest.mark.parametrize("nshots", [None, 10, 100])
 def test_pauli_error(backend, density_matrix, nshots):
-    pauli = PauliError(0, 0.2, 0.3)
-    noise = NoiseModel()
-    noise.add(pauli, gates.X, 1)
-    noise.add(pauli, gates.CNOT)
-    noise.add(pauli, gates.Z, (0, 1))
-
-    circuit = Circuit(3, density_matrix=density_matrix)
-    circuit.add(gates.CNOT(0, 1))
-    circuit.add(gates.Z(1))
-    circuit.add(gates.X(1))
-    circuit.add(gates.X(2))
-    circuit.add(gates.Z(2))
-    circuit.add(gates.M(0, 1, 2))
-
-    target_circuit = Circuit(3, density_matrix=density_matrix)
-    target_circuit.add(gates.CNOT(0, 1))
-    target_circuit.add(gates.PauliNoiseChannel(0, 0, 0.2, 0.3))
-    target_circuit.add(gates.PauliNoiseChannel(1, 0, 0.2, 0.3))
-    target_circuit.add(gates.Z(1))
-    target_circuit.add(gates.PauliNoiseChannel(1, 0, 0.2, 0.3))
-    target_circuit.add(gates.X(1))
-    target_circuit.add(gates.PauliNoiseChannel(1, 0, 0.2, 0.3))
-    target_circuit.add(gates.X(2))
-    target_circuit.add(gates.Z(2))
-    target_circuit.add(gates.M(0, 1, 2))
-
-    initial_psi = (
-        random_density_matrix(2**3) if density_matrix else random_statevector(2**3)
-    )
-    backend.set_seed(123)
-    final_state = backend.execute_circuit(
-        noise.apply(circuit), initial_state=np.copy(initial_psi), nshots=nshots
-    )
-    final_state_samples = final_state.samples() if nshots else None
-    backend.set_seed(123)
-    target_final_state = backend.execute_circuit(
-        target_circuit, initial_state=np.copy(initial_psi), nshots=nshots
-    )
-    target_final_state_samples = target_final_state.samples() if nshots else None
-
-    if nshots is None:
-        backend.assert_allclose(final_state, target_final_state)
-    else:
-        backend.assert_allclose(final_state_samples, target_final_state_samples)
-
-
-@pytest.mark.parametrize("density_matrix", [False, True])
-@pytest.mark.parametrize("nshots", [None, 10, 100])
-def test_generalized_pauli_error(backend, density_matrix, nshots):
     list_paulis = ["X", "Y", "Z"]
     probabilities = np.array([0, 0.2, 0.3])
     zipped = list(zip(list_paulis, probabilities))
 
-    pauli = GeneralizedPauliError(zipped)
+    pauli = PauliError(zipped)
     noise = NoiseModel()
     noise.add(pauli, gates.X, 1)
     noise.add(pauli, gates.CNOT)
@@ -195,12 +146,12 @@ def test_generalized_pauli_error(backend, density_matrix, nshots):
 
     target_circuit = Circuit(3, density_matrix=density_matrix)
     target_circuit.add(gates.CNOT(0, 1))
-    target_circuit.add(gates.GeneralizedPauliNoiseChannel(0, zipped))
-    target_circuit.add(gates.GeneralizedPauliNoiseChannel(1, zipped))
+    target_circuit.add(gates.PauliNoiseChannel(0, zipped))
+    target_circuit.add(gates.PauliNoiseChannel(1, zipped))
     target_circuit.add(gates.Z(1))
-    target_circuit.add(gates.GeneralizedPauliNoiseChannel(1, zipped))
+    target_circuit.add(gates.PauliNoiseChannel(1, zipped))
     target_circuit.add(gates.X(1))
-    target_circuit.add(gates.GeneralizedPauliNoiseChannel(1, zipped))
+    target_circuit.add(gates.PauliNoiseChannel(1, zipped))
     target_circuit.add(gates.X(2))
     target_circuit.add(gates.Z(2))
     target_circuit.add(gates.M(0, 1, 2))
