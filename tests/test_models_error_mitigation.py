cbfd406e86623dcbb957e7535d5106d5f7270ece
Alejandro Sopena <alexsopena15@gmail.com>
2023-05-03 20:37:28 +0200

    Updating tests

--- tests/test_models_error_mitigation.py
+++ tests/test_models_error_mitigation.py
@@ -8,17 +8,21 @@ from qibo.models.error_mitigation import (
     CDR,
     ZNE,
     apply_readout_mitigation,
+    apply_temme_readout_mitigation,
     get_calibration_matrix,
     sample_training_circuit,
     vnCDR,
 )
-from qibo.noise import DepolarizingError, NoiseModel
+from qibo.noise import DepolarizingError, NoiseModel, ReadoutError
+from qibo.quantum_info import random_stochastic_matrix
 from qibo.symbols import Z
 
 
-def get_noise_model(error, gate):
+def get_noise_model(error, gate, cal_matrix=[False, None]):
     noise = NoiseModel()
     noise.add(error, gate)
+    if cal_matrix[0]:
+        noise.add(ReadoutError(probabilities=cal_matrix[1]), gate=gates.M)
     return noise
 
 
@@ -39,29 +43,58 @@ def get_circuit(nqubits, p=None):
     c.add(gates.CNOT(q, q + 1) for q in range(1, nqubits, 2))
     c.add(gates.RZ(q + 1, theta=-2 * dt) for q in range(1, nqubits, 2))
     c.add(gates.CNOT(q, q + 1) for q in range(1, nqubits, 2))
-    c.add(gates.M(q, p0=p) for q in range(nqubits))
+    c.add(gates.M(*range(nqubits), p0=p))
     return c
 
 
-# Precompute the calibration matrices
-p0 = 0.2
-cal_matrix_1q = get_calibration_matrix(nqubits=1, nshots=10000, p0=p0)
-cal_matrix_3q = get_calibration_matrix(nqubits=3, nshots=10000, p0=p0)
+from qibo.backends import construct_backend
+
+backend = construct_backend("numpy")
+# # Precompute the calibration matrices
+cal_matrix_1q = random_stochastic_matrix(
+    2, diagonally_dominant=True, seed=2, backend=backend
+)
+cal_matrix_3q = random_stochastic_matrix(
+    8, diagonally_dominant=True, seed=2, backend=backend
+)
 
 
 @pytest.mark.parametrize(
-    "nqubits,noise,insertion_gate,calibration_matrix",
+    "nqubits,noise,insertion_gate,readout",
     [
-        (3, get_noise_model(DepolarizingError(0.1), gates.CNOT), "CNOT", None),
-        (3, get_noise_model(DepolarizingError(0.1), gates.CNOT), "CNOT", cal_matrix_3q),
-        (1, get_noise_model(DepolarizingError(0.1), gates.RX), "RX", None),
-        (1, get_noise_model(DepolarizingError(0.1), gates.RX), "RX", cal_matrix_1q),
+        (3, get_noise_model(DepolarizingError(0.1), gates.CNOT), "CNOT", {}),
+        (
+            3,
+            get_noise_model(DepolarizingError(0.1), gates.CNOT),
+            "CNOT",
+            {"calibration_matrix": np.eye(8)},
+        ),
+        (
+            3,
+            get_noise_model(DepolarizingError(0.1), gates.CNOT),
+            "CNOT",
+            {"ncircuits": 2},
+        ),
+        (1, get_noise_model(DepolarizingError(0.1), gates.RX), "RX", {}),
+        (
+            1,
+            get_noise_model(DepolarizingError(0.1), gates.RX),
+            "RX",
+            {"calibration_matrix": np.eye(2)},
+        ),
+        (1, get_noise_model(DepolarizingError(0.1), gates.RX), "RX", {"ncircuits": 2}),
     ],
 )
 @pytest.mark.parametrize("solve", [False, True])
-def test_zne(backend, nqubits, noise, solve, insertion_gate, calibration_matrix):
+def test_zne(backend, nqubits, noise, solve, insertion_gate, readout):
     """Test that ZNE reduces the noise."""
-    backend.set_threads(1)
+    if backend.name == "tensorflow":
+        import tensorflow as tf
+
+        tf.config.threading.set_inter_op_parallelism_threads = 1
+        tf.config.threading.set_intra_op_parallelism_threads = 1
+    else:
+        backend.set_threads(1)
     # Define the circuit
     c = get_circuit(nqubits)
     # Define the observable
@@ -70,8 +103,6 @@ def test_zne(backend, nqubits, noise, solve, insertion_gate, calibration_matrix)
     # Noise-free expected value
     exact = obs.expectation(backend.execute_circuit(c).state())
     # Noisy expected value without mitigation
-    if calibration_matrix is not None:
-        c = get_circuit(nqubits, p=p0)
     state = backend.execute_circuit(noise.apply(c), nshots=10000)
     noisy = state.expectation_from_samples(obs)
     # Mitigated expected value
@@ -79,25 +110,40 @@ def test_zne(backend, nqubits, noise, solve, insertion_gate, calibration_matrix)
         circuit=c,
         observable=obs,
         backend=backend,
-        noise_levels=np.array(range(5)),
+        noise_levels=np.array(range(4)),
         noise_model=noise,
         nshots=10000,
         solve_for_gammas=solve,
         insertion_gate=insertion_gate,
-        calibration_matrix=calibration_matrix,
+        readout=readout,
     )
     assert np.abs(exact - estimate) <= np.abs(exact - noisy)
 
 
 @pytest.mark.parametrize("nqubits", [3])
-@pytest.mark.parametrize(
-    "noise",
-    [get_noise_model(DepolarizingError(0.1), gates.CNOT)],
-)
 @pytest.mark.parametrize("full_output", [False, True])
-@pytest.mark.parametrize("calibration_matrix", [None, cal_matrix_3q])
-def test_cdr(backend, nqubits, noise, full_output, calibration_matrix):
-    backend.set_threads(1)
+@pytest.mark.parametrize(
+    "noise,readout",
+    [
+        (get_noise_model(DepolarizingError(0.1), gates.CNOT), {}),
+        (
+            get_noise_model(DepolarizingError(0.1), gates.CNOT, [True, cal_matrix_3q]),
+            {"calibration_matrix": cal_matrix_3q},
+        ),
+        (
+            get_noise_model(DepolarizingError(0.1), gates.CNOT, [True, cal_matrix_3q]),
+            {"ncircuits": 2},
+        ),
+    ],
+)
+def test_cdr(backend, nqubits, noise, full_output, readout):
+    if backend.name == "tensorflow":
+        import tensorflow as tf
+
+        tf.config.threading.set_inter_op_parallelism_threads = 1
+        tf.config.threading.set_intra_op_parallelism_threads = 1
+    else:
+        backend.set_threads(1)
     """Test that CDR reduces the noise."""
     # Define the circuit
     c = get_circuit(nqubits)
@@ -107,8 +153,6 @@ def test_cdr(backend, nqubits, noise, full_output, calibration_matrix):
     # Noise-free expected value
     exact = obs.expectation(backend.execute_circuit(c).state())
     # Noisy expected value without mitigation
-    if calibration_matrix is not None:
-        c = get_circuit(nqubits, p=p0)
     state = backend.execute_circuit(noise.apply(c), nshots=10000)
     noisy = state.expectation_from_samples(obs)
     # Mitigated expected value
@@ -118,7 +162,8 @@ def test_cdr(backend, nqubits, noise, full_output, calibration_matrix):
         backend=backend,
         noise_model=noise,
         nshots=10000,
-        calibration_matrix=calibration_matrix,
+        n_training_samples=20,
+        readout=readout,
         full_output=full_output,
     )
     if full_output:
@@ -150,20 +195,46 @@ def test_sample_training_circuit(nqubits):
 
 
 @pytest.mark.parametrize(
-    "nqubits,noise,insertion_gate,calibration_matrix",
+    "nqubits,noise,insertion_gate,readout",
     [
-        (3, get_noise_model(DepolarizingError(0.1), gates.CNOT), "CNOT", None),
-        (3, get_noise_model(DepolarizingError(0.1), gates.CNOT), "CNOT", cal_matrix_3q),
-        (1, get_noise_model(DepolarizingError(0.1), gates.RX), "RX", None),
-        (1, get_noise_model(DepolarizingError(0.1), gates.RX), "RX", cal_matrix_1q),
+        (3, get_noise_model(DepolarizingError(0.1), gates.CNOT), "CNOT", {}),
+        (
+            3,
+            get_noise_model(DepolarizingError(0.1), gates.CNOT, [True, cal_matrix_3q]),
+            "CNOT",
+            {"calibration_matrix": cal_matrix_3q},
+        ),
+        (
+            3,
+            get_noise_model(DepolarizingError(0.1), gates.CNOT, [True, cal_matrix_3q]),
+            "CNOT",
+            {"ncircuits": 2},
+        ),
+        (1, get_noise_model(DepolarizingError(0.1), gates.RX), "RX", {}),
+        (
+            1,
+            get_noise_model(DepolarizingError(0.1), gates.RX, [True, cal_matrix_1q]),
+            "RX",
+            {"calibration_matrix": cal_matrix_1q},
+        ),
+        (
+            1,
+            get_noise_model(DepolarizingError(0.1), gates.RX, [True, cal_matrix_1q]),
+            "RX",
+            {"ncircuits": 2},
+        ),
     ],
 )
 @pytest.mark.parametrize("full_output", [False, True])
-def test_vncdr(
-    backend, nqubits, noise, full_output, insertion_gate, calibration_matrix
-):
+def test_vncdr(backend, nqubits, noise, full_output, insertion_gate, readout):
     """Test that vnCDR reduces the noise."""
-    backend.set_threads(1)
+    if backend.name == "tensorflow":
+        import tensorflow as tf
+
+        tf.config.threading.set_inter_op_parallelism_threads = 1
+        tf.config.threading.set_intra_op_parallelism_threads = 1
+    else:
+        backend.set_threads(1)
     # Define the circuit
     c = get_circuit(nqubits)
     # Define the observable
@@ -172,8 +243,12 @@ def test_vncdr(
     # Noise-free expected value
     exact = obs.expectation(backend.execute_circuit(c).state())
     # Noisy expected value without mitigation
-    if calibration_matrix is not None:
-        c = get_circuit(nqubits, p=p0)
+    if "calibration_matrix" in readout.keys() or "ncircuits" in readout.keys():
+        if nqubits == 1:
+            p = cal_matrix_1q
+        elif nqubits == 3:
+            p = cal_matrix_3q
+        # noise.add(ReadoutError(probabilities=p),gate=gates.M)
     state = backend.execute_circuit(noise.apply(c), nshots=10000)
     noisy = state.expectation_from_samples(obs)
     # Mitigated expected value
@@ -184,9 +259,10 @@ def test_vncdr(
         noise_levels=range(3),
         noise_model=noise,
         nshots=10000,
+        n_training_samples=20,
         insertion_gate=insertion_gate,
         full_output=full_output,
-        calibration_matrix=calibration_matrix,
+        readout=readout,
     )
     if full_output:
         estimate = estimate[0]
@@ -194,27 +270,42 @@ def test_vncdr(
 
 
 @pytest.mark.parametrize("nqubits", [3])
-def test_readout_mitigation(backend, nqubits):
-    backend.set_threads(1)
+@pytest.mark.parametrize("method", ["cal_matrix", "temme"])
+def test_readout_mitigation(backend, nqubits, method):
+    if backend.name == "tensorflow":
+        import tensorflow as tf
+
+        tf.config.threading.set_inter_op_parallelism_threads = 1
+        tf.config.threading.set_intra_op_parallelism_threads = 1
+    else:
+        backend.set_threads(1)
     nshots = 10000
-    p0 = [0.1, 0.2, 0.3]
-    p1 = [0.3, 0.1, 0.2]
-    calibration_matrix = get_calibration_matrix(nqubits, nshots=nshots, p0=p0, p1=p1)
+    p = random_stochastic_matrix(2**nqubits, diagonally_dominant=True, seed=5)
+    noise = NoiseModel()
+    noise.add(ReadoutError(probabilities=p), gate=gates.M)
+    if method == "cal_matrix":
+        calibration_matrix = get_calibration_matrix(
+            nqubits, noise, backend, nshots=nshots
+        )
     # Define the observable
     obs = np.prod([Z(i) for i in range(nqubits)])
     obs = SymbolicHamiltonian(obs, backend=backend)
     # get noise free expected val
-    c = Circuit(nqubits)
-    c.add(gates.X(0))
-    c.add(gates.M(*range(nqubits)))
+    c = get_circuit(nqubits)
     true_state = backend.execute_circuit(c, nshots=nshots)
     true_val = true_state.expectation_from_samples(obs)
     # get noisy expected val
-    c = Circuit(nqubits)
-    c.add(gates.X(0))
-    c.add(gates.M(*range(nqubits), p0=p0, p1=p1))
-    state = backend.execute_circuit(c, nshots=nshots)
+    state = backend.execute_circuit(noise.apply(c), nshots=nshots)
     noisy_val = state.expectation_from_samples(obs)
-    mit_state = apply_readout_mitigation(state, calibration_matrix)
-    mit_val = mit_state.expectation_from_samples(obs)
+    if method == "cal_matrix":
+        mit_state = apply_readout_mitigation(state, calibration_matrix)
+        mit_val = mit_state.expectation_from_samples(obs)
+    elif method == "temme":
+        ncircuits = 10
+        result, result_cal = apply_temme_readout_mitigation(
+            c, backend, noise, nshots, ncircuits
+        )
+        mit_val = result.expectation_from_samples(
+            obs
+        ) / result_cal.expectation_from_samples(obs)
     assert np.abs(true_val - mit_val) <= np.abs(true_val - noisy_val)
