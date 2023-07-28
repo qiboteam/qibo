9fe282a413c933dda66c16b3b9303f6f492696d3
acse-b99192e1 <michael.tsesmelis22@ic.ac.uk>
2023-07-28 15:28:38 +0200

    new

--- src/qibo/optimizers.py
+++ src/qibo/optimizers.py
@@ -240,19 +240,18 @@ class SGD(Optimizer):
             self.file.write(f"Feature {feat}, duration {ftime-self.ftime}\n")
             self.ftime = ftime
             results[i] = self.predict(feat)
-            exit(0)
 
             obs_gradients = np.empty((self.nlabels, self.nparams))
-
+            print(obs_gradients)
             for h, ham in enumerate(self.hamiltonian):
                 obs_gradients[h] = calculate_gradients(
                     self, self.cdr_params, ham, self.options["nshots"]
                 )  # d<B> N params, N label gradients
 
             loss_func_grad = self.calculate_loss_func_grad(results, labels, i)
-
+            print(loss_func_grad)
             circ_grads += np.dot(loss_func_grad.T, obs_gradients)
-
+            print(circ_grads)
             if self.options["natgrad"]:
                 fubini += generate_fubini(
                     self._circuit,
@@ -263,7 +262,8 @@ class SGD(Optimizer):
                     mitigation=self.options["mitigation"],
                     noise_model=self.options["noise_model"],
                 )  # separate pull request
-
+            print(fubini)
+            exit(0)
         # gradient average
         loss = self.loss_function(results, labels, self.args)
         loss_gradients = circ_grads / (self.nsample)
