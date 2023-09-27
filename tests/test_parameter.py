4b5b255d6a30e7b18000916fece3f7174ff77e90
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-09-27 11:51:29 +0200

    add tests

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -56,6 +56,10 @@ def test_parameter():
     gate_value = param()
     assert gate_value == 585
 
+    # testing call with new values
+    executed = param(features=[0.5, 2.0], trainable=[2.0, 0.1, 4.0])
+    assert executed == 1.3
+
 
 def test_parameter_errors():
     param = Parameter(
@@ -92,3 +96,15 @@ def test_parameter_errors():
     # test type error due to wrong initialization
     with pytest.raises(TypeError):
         param = Parameter(func=lambda x, y: x + y**2)
+
+    # test call function with wrong features and trainable dimensionality
+    param = Parameter(
+        func=lambda x, th1, th2: th1 * x + th2, features=[1.2], trainable=[0.2, 9.1]
+    )
+
+    # wrong features length
+    with pytest.raises(TypeError):
+        param(features=[2.3, 9.2], trainable=[0.4, 9.3])
+    # wrong trainable length
+    with pytest.raises(TypeError):
+        param(features=[0.4], trainable=[3.4, 0.1, 5.6])
