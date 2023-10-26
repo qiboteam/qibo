dc4c1ed33ad9660f6d371a24214b5e7d2243dfae
Renato Mello <renato.msf@gmail.com>
2023-10-26 16:39:02 +0400

    `unary_encoder_random_gaussian`

--- src/qibo/models/encodings.py
+++ src/qibo/models/encodings.py
@@ -3,6 +3,7 @@
 import math
 
 import numpy as np
+from scipy.stats import rv_continuous
 
 from qibo import gates
 from qibo.backends import GlobalBackend
@@ -18,16 +19,20 @@ def unary_encoder(data):
     .. math::
         \\mathbf{x} = (x_{1}, x_{2}, \\dots, x_{d}) \\, ,
 
-    this function generate the circuit that prepares the quantum state
+    this function generate the circuit that prepares the following quantum state
+    :math:`\\ket{\\psi} \\in \\mathcal{H}`:
 
     .. math::
-        \\frac{1}{\\|\\mathbf{x}\\|} \\, \\sum_{k=1}^{d} \\, x_{k} \\, \\ket{k} \\, .
+        \\ket{\\psi} = \\frac{1}{\\|\\mathbf{x}\\|_{\\textup{HS}}} \\,
+            \\sum_{k=1}^{d} \\, x_{k} \\, \\ket{k} \\, ,
 
+    with :math:`\\mathcal{H} \\cong \\mathbb{C}^{d}` being a :math:`d`-qubit Hilbert space,
+    and :math:`\\|\\cdot\\|_{\\textup{HS}}` being the Hilbert-Schmidt norm.
     Here, :math:`\\ket{k}` is a unary representation of the number :math:`1` through
     :math:`d`.
 
     Args:
-        data (ndarray): array of data to be loaded.
+        data (ndarray, optional): :math:`1`-dimensional array of data to be loaded.
 
     Returns:
         :class:`qibo.models.circuit.Circuit`: circuit that loads ``data`` in unary representation.
@@ -41,26 +46,15 @@ def unary_encoder(data):
             TypeError,
             f"``data`` must be a 1-dimensional array, but it has dimensions {data.shape}.",
         )
+    elif math.log2(data.shape[0]) % 2 != 0.0:
+        raise_error(
+            ValueError, f"len(data) must be a power of 2, but it is {len(data)}."
+        )
 
     nqubits = len(data)
     j_max = int(nqubits / 2)
 
-    # generating list of indexes representing the RBS connections
-    pairs_rbs = [[(0, int(nqubits / 2))]]
-    indexes = list(np.array(pairs_rbs).flatten())
-    for depth in range(2, int(math.log2(nqubits)) + 1):
-        pairs_rbs_per_depth = [
-            [(index, index + int(nqubits / 2**depth)) for index in indexes]
-        ]
-        pairs_rbs += pairs_rbs_per_depth
-        indexes = list(np.array(pairs_rbs_per_depth).flatten())
-
-    # creating circuit with all RBS initialised with 0.0 phase
-    circuit = Circuit(nqubits)
-    circuit.add(gates.X(0))
-    for row in pairs_rbs:
-        for pair in row:
-            circuit.add(gates.RBS(*pair, 0.0, trainable=True))
+    circuit, _ = _generate_rbs_pairs(nqubits)
 
     # calculating phases and setting circuit parameters
     r_array = np.zeros(nqubits - 1, dtype=float)
@@ -79,3 +73,115 @@ def unary_encoder(data):
     circuit.set_parameters(phases)
 
     return circuit
+
+
+def unary_encoder_random_gaussian(nqubits: int, seed=None):
+    """Creates a circuit that performs the unary encoding of a random Gaussian state.
+
+    Given :math:`d` qubits, encodes the quantum state
+    :math:`\\ket{\\psi} \\in \\mathcal{H}` such that
+
+
+    .. math::
+        \\ket{\\psi} = \\frac{1}{\\|\\mathbf{x}\\|_{\\textup{HS}}} \\,
+            \\sum_{k=1}^{d} \\, x_{k} \\, \\ket{k}
+
+    where :math:`x_{k}` are independent Gaussian random variables,
+    :math:`\\mathcal{H} \\cong \\mathbb{C}^{d}` is a :math:`d`-qubit Hilbert space,
+    and :math:`\\|\\cdot\\|_{\\textup{HS}}` being the Hilbert-Schmidt norm.
+    Here, :math:`\\ket{k}` is a unary representation of the number :math:`1` through
+    :math:`d`.
+
+    At depth :math:`h`, the angles :math:`\\theta_{k}` of the the gates :math:`RBS(\\theta_{k})`
+    are sampled from the following probability density function:
+
+    .. math::
+        p_{h}(\\theta) = \\frac{2}{\\sqrt{\\pi}} \\,
+             \\frac{\\Gamma(\\frac{1+2^{h-1}}{2})}{\\Gamma(2^{h-2})} \\,
+             \\left(\\sin(2\\theta)\\right)^{2^{h-1}-1} \\, ,
+
+    where :math:`\\Gamma(\\cdot)` is the Gamma function.
+
+    Args:
+        nqubits (int): number of qubits.
+        seed (int or :class:`numpy.random.Generator`, optional): Either a generator of
+            random numbers or a fixed seed to initialize a generator. If ``None``,
+            initializes a generator with a random seed. Defaults to ``None``.
+
+    Returns:
+        :class:`qibo.models.circuit.Circuit`: circuit that loads a random Gaussian array in unary representation.
+
+    References:
+        1. A. Bouland, A. Dandapani, and A. Prakash, *A quantum spectral method for simulating
+        stochastic processes, with applications to Monte Carlo*.
+        `arXiv:2303.06719v1 [quant-ph] <https://arxiv.org/abs/2303.06719>`_
+    """
+    circuit, pairs_rbs = _generate_rbs_pairs(nqubits)
+
+    if not isinstance(nqubits, int):
+        raise_error(
+            TypeError, f"nqubits must be type int, but it is type {type(nqubits)}."
+        )
+    elif nqubits <= 0.0:
+        raise_error(
+            ValueError, f"nqubits must be a positive integer, but it is {nqubits}."
+        )
+    elif math.log2(nqubits) % 2 != 0.0:
+        raise_error(ValueError, f"nqubits must be a power of 2, but it is {nqubits}.")
+
+    if (
+        seed is not None
+        and not isinstance(seed, int)
+        and not isinstance(seed, np.random.Generator)
+    ):
+        raise_error(
+            TypeError, "seed must be either type int or numpy.random.Generator."
+        )
+
+    local_state = (
+        np.random.default_rng(seed) if seed is None or isinstance(seed, int) else seed
+    )
+
+    sampler = _probability_distribution_gaussian_loader(
+        a=0, b=math.pi / 2, seed=local_state
+    )
+
+    phases = []
+    for depth, row in enumerate(pairs_rbs, 1):
+        phases += list(sampler.rvs(depth=depth, size=len(row)))
+
+    circuit.set_parameters(phases)
+
+    return circuit
+
+
+def _generate_rbs_pairs(nqubits):
+    # generating list of indexes representing the RBS connections
+    pairs_rbs = [[(0, int(nqubits / 2))]]
+    indexes = list(np.array(pairs_rbs).flatten())
+    for depth in range(2, int(math.log2(nqubits)) + 1):
+        pairs_rbs_per_depth = [
+            [(index, index + int(nqubits / 2**depth)) for index in indexes]
+        ]
+        pairs_rbs += pairs_rbs_per_depth
+        indexes = list(np.array(pairs_rbs_per_depth).flatten())
+
+    # creating circuit with all RBS initialised with 0.0 phase
+    circuit = Circuit(nqubits)
+    circuit.add(gates.X(0))
+    for row in pairs_rbs:
+        for pair in row:
+            circuit.add(gates.RBS(*pair, 0.0, trainable=True))
+
+    return circuit, pairs_rbs
+
+
+class _probability_distribution_gaussian_loader(rv_continuous):
+    def _pdf(self, theta: float, depth: int):
+        amplitude = (
+            2
+            * math.gamma((2 ** (depth - 1) + 1) / 2)
+            / (math.sqrt(math.pi) * math.gamma(2 ** (depth - 2)))
+        )
+
+        return amplitude * (math.sin(2 * theta)) ** (2 ** (depth - 1) - 1)
