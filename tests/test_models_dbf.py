57141c46616e82b4971d64dd51f889233c129bec
Andrea <andreapasquale97@gmail.com>
2023-11-03 18:57:28 +0400

    Ease tests

--- tests/test_models_dbf.py
+++ tests/test_models_dbf.py
@@ -6,22 +6,22 @@ from qibo.hamiltonians import Hamiltonian
 from qibo.models.double_bracket import DoubleBracketFlow, DoubleBracketFlowMode
 from qibo.quantum_info import random_hermitian
 
-NSTEPS = 100
+NSTEPS = 50
 """Number of steps for evolution."""
 
 
-@pytest.mark.parametrize("nqubits", [4, 5, 6])
+@pytest.mark.parametrize("nqubits", [3, 4, 5])
 def test_double_bracket_flow_canonical(backend, nqubits):
     h0 = random_hermitian(2**nqubits, backend=backend)
     dbf = DoubleBracketFlow(Hamiltonian(nqubits, h0, backend=backend))
     initial_off_diagonal_norm = dbf.off_diagonal_norm
     for _ in range(NSTEPS):
-        dbf(mode=DoubleBracketFlowMode.canonical, step=np.sqrt(0.01))
+        dbf(mode=DoubleBracketFlowMode.canonical, step=np.sqrt(0.001))
 
     assert initial_off_diagonal_norm > dbf.off_diagonal_norm
 
 
-@pytest.mark.parametrize("nqubits", [4, 5, 6])
+@pytest.mark.parametrize("nqubits", [3, 4, 5])
 def test_double_bracket_flow_group_commutator(backend, nqubits):
     h0 = random_hermitian(2**nqubits, backend=backend)
     d = backend.cast(np.diag(np.diag(backend.to_numpy(h0))))
@@ -29,9 +29,9 @@ def test_double_bracket_flow_group_commutator(backend, nqubits):
     initial_off_diagonal_norm = dbf.off_diagonal_norm
 
     with pytest.raises(ValueError):
-        dbf(mode=DoubleBracketFlowMode.group_commutator, step=0.1)
+        dbf(mode=DoubleBracketFlowMode.group_commutator, step=0.01)
 
     for _ in range(NSTEPS):
-        dbf(mode=DoubleBracketFlowMode.group_commutator, step=0.1, d=d)
+        dbf(mode=DoubleBracketFlowMode.group_commutator, step=0.01, d=d)
 
     assert initial_off_diagonal_norm > dbf.off_diagonal_norm
