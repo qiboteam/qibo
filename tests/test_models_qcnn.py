8cb02f7218ac68b3649c4b31eda92357cd1c5a45
jykhoo1987 <90653892+jykhoo1987@users.noreply.github.com>
2023-11-07 16:00:52 +0800

    Update test_models_qcnn.py
    fix pytest

--- tests/test_models_qcnn.py
+++ tests/test_models_qcnn.py
@@ -3,8 +3,9 @@ import math
 import numpy as np
 import pytest
 
-from qibo.models import Circuit, qcnn
 from qibo import gates
+from qibo.models import Circuit
+from qibo.models.qcnn import QuantumCNN
 
 num_angles = 21
 angles0 = [i * math.pi / num_angles for i in range(num_angles)]
