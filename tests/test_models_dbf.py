0b7b7dc6e9f65a812b178b8eeb6315a80109a674
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-11-15 09:13:46 +0100

    test constructors with flow mode

--- tests/test_models_dbf.py
+++ tests/test_models_dbf.py
@@ -15,10 +15,12 @@ NSTEPS = 50
 @pytest.mark.parametrize("nqubits", [3, 4, 5])
 def test_double_bracket_flow_canonical(backend, nqubits):
     h0 = random_hermitian(2**nqubits, backend=backend)
-    dbf = DoubleBracketFlow(Hamiltonian(nqubits, h0, backend=backend))
+    dbf = DoubleBracketFlow(
+        Hamiltonian(nqubits, h0, backend=backend), mode=FlowGeneratorType.canonical
+    )
     initial_off_diagonal_norm = dbf.off_diagonal_norm
     for _ in range(NSTEPS):
-        dbf(mode=FlowGeneratorType.canonical, step=np.sqrt(0.001))
+        dbf(step=np.sqrt(0.001))
 
     assert initial_off_diagonal_norm > dbf.off_diagonal_norm
 
@@ -27,14 +29,17 @@ def test_double_bracket_flow_canonical(backend, nqubits):
 def test_double_bracket_flow_group_commutator(backend, nqubits):
     h0 = random_hermitian(2**nqubits, backend=backend)
     d = backend.cast(np.diag(np.diag(backend.to_numpy(h0))))
-    dbf = DoubleBracketFlow(Hamiltonian(nqubits, h0, backend=backend))
+    dbf = DoubleBracketFlow(
+        Hamiltonian(nqubits, h0, backend=backend),
+        mode=FlowGeneratorType.group_commutator,
+    )
     initial_off_diagonal_norm = dbf.off_diagonal_norm
 
     with pytest.raises(ValueError):
         dbf(mode=FlowGeneratorType.group_commutator, step=0.01)
 
     for _ in range(NSTEPS):
-        dbf(mode=FlowGeneratorType.group_commutator, step=0.01, d=d)
+        dbf(step=0.01, d=d)
 
     assert initial_off_diagonal_norm > dbf.off_diagonal_norm
 
@@ -43,14 +48,17 @@ def test_double_bracket_flow_group_commutator(backend, nqubits):
 def test_double_bracket_flow_single_commutator(backend, nqubits):
     h0 = random_hermitian(2**nqubits, backend=backend)
     d = backend.cast(np.diag(np.diag(backend.to_numpy(h0))))
-    dbf = DoubleBracketFlow(Hamiltonian(nqubits, h0, backend=backend))
+    dbf = DoubleBracketFlow(
+        Hamiltonian(nqubits, h0, backend=backend),
+        mode=FlowGeneratorType.single_commutator,
+    )
     initial_off_diagonal_norm = dbf.off_diagonal_norm
 
     with pytest.raises(ValueError):
         dbf(mode=FlowGeneratorType.single_commutator, step=0.01)
 
     for _ in range(NSTEPS):
-        dbf(mode=FlowGeneratorType.single_commutator, step=0.01, d=d)
+        dbf(step=0.01, d=d)
 
     assert initial_off_diagonal_norm > dbf.off_diagonal_norm
 
