fixed prob calculation

src/qibo/backends/clifford.py
@@ -45,7 +45,7 @@ class CliffordOperations:
         return new_tab
 
     def CZ(self, tableau, control_q, target_q, nqubits):
-        """Decomposition --> HCNOTH"""
+        """Decomposition --> H-CNOT-H"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -79,7 +79,7 @@ class CliffordOperations:
         return new_tab
 
     def Z(self, tableau, q, nqubits):
-        """Decomposition --> SS"""
+        """Decomposition --> S-S"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -92,7 +92,7 @@ class CliffordOperations:
         return new_tab
 
     def X(self, tableau, q, nqubits):
-        """Decomposition --> HSSH"""
+        """Decomposition --> H-S-S-H"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -108,7 +108,7 @@ class CliffordOperations:
         return new_tab
 
     def Y(self, tableau, q, nqubits):
-        """Decomposition --> SSHSSH"""  # double check this, cause it should be
+        """Decomposition --> S-S-H-S-S-H"""  # double check this, cause it should be
         new_tab = tableau.copy()  # Y = i * HZHZ --> HSSHSS
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -124,7 +124,7 @@ class CliffordOperations:
         return new_tab
 
     def SX(self, tableau, q, nqubits):
-        """Decomposition --> HSH"""
+        """Decomposition --> H-S-H"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -139,7 +139,7 @@ class CliffordOperations:
         return new_tab
 
     def SDG(self, tableau, q, nqubits):
-        """Decomposition --> SSS"""
+        """Decomposition --> S-S-S"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -154,7 +154,7 @@ class CliffordOperations:
         return new_tab
 
     def SXDG(self, tableau, q, nqubits):
-        """Decomposition --> HSSSH"""
+        """Decomposition --> H-S-S-S-H"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -195,7 +195,7 @@ class CliffordOperations:
             return self.Y(tableau, q, nqubits)
         # not working
         elif (theta / (self.np.pi / 2) - 1) % 4 == 0:
-            """Decomposition --> HSS"""
+            """Decomposition --> H-S-S"""
             new_tab = tableau.copy()
             r, x, z = (
                 self.get_r(new_tab, nqubits),
@@ -209,7 +209,7 @@ class CliffordOperations:
             new_tab[:-1, [nqubits + q, q]] = new_tab[:-1, [q, nqubits + q]]
             return new_tab
         else:  # theta == 3*pi/2 + 2*n*pi
-            """Decomposition --> HSSHSSHSS"""
+            """Decomposition --> H-S-S-H-S-S-H-S-S"""
             new_tab = tableau.copy()
             r, x, z = (
                 self.get_r(new_tab, nqubits),
@@ -224,7 +224,7 @@ class CliffordOperations:
             return new_tab
 
     def SWAP(self, tableau, control_q, target_q, nqubits):
-        """Decomposition --> CNOTCNOTCNOT"""
+        """Decomposition --> CNOT-CNOT-CNOT"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -262,7 +262,7 @@ class CliffordOperations:
         return new_tab
 
     def iSWAP(self, tableau, control_q, target_q, nqubits):
-        """Decomposition --> HCNOTCNOTHSS"""
+        """Decomposition --> H-CNOT-CNOT-H-S-S"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -315,7 +315,7 @@ class CliffordOperations:
         return self.X(new_tab, control_q, nqubits)
 
     def CY(self, tableau, control_q, target_q, nqubits):
-        """Decomposition --> HCNOTCNOTHSS"""
+        """Decomposition --> H-CNOT-CNOT-H-S-S"""
         new_tab = tableau.copy()
         r, x, z = (
             self.get_r(new_tab, nqubits),
@@ -403,7 +403,7 @@ class CliffordOperations:
             # determined outcome, state unchanged
             else:
                 CliffordOperations.set_scratch(state_copy, 0)
-                for i in (x[:, q] == 1).nonzero()[0]:
+                for i in x[:nqubits, q].nonzero()[0]:
                     state_copy = self.rowsum(
                         state_copy,
                         2 * nqubits,

src/qibo/quantum_info/clifford.py
@@ -118,7 +118,7 @@ class Clifford:
             return self._backend.samples_to_decimal(self._samples, len(measured_qubits))
 
     def frequencies(self, binary=True):
-        measured_qubits = [q for m in self.measurements for q in m.target_qubits]
+        measured_qubits = {q for m in self.measurements for q in m.target_qubits}
         freq = self._backend.calculate_frequencies(self.samples(False))
         if binary:
             return frequencies_to_binary(freq, len(measured_qubits))
@@ -126,12 +126,14 @@ class Clifford:
             return freq
 
     def probabilities(self, qubits=None):
-        measured_qubits = [q for m in self.measurements for q in m.target_qubits]
+        measured_qubits = sorted(
+            {q for m in self.measurements for q in m.target_qubits}
+        )
         if qubits is not None:
             if not set(qubits).issubset(set(measured_qubits)):
                 raise_error(
                     RuntimeError,
-                    f"Asking probabilities for qubits {qubits}, but only {measured_qubits} were measured.",
+                    f"Asking probabilities for qubits {qubits}, but only qubits {measured_qubits} were measured.",
                 )
         else:
             qubits = measured_qubits
@@ -141,7 +143,7 @@ class Clifford:
         for s in samples:
             probs[s] += 1
         probs = self._backend.cast(probs) / len(samples)
-        qubits, _ = zip(*sorted(zip(range(len(qubits)), qubits), key=lambda x: x[1]))
+        qubits = [measured_qubits.index(q) for q in qubits]
         return self._backend.calculate_probabilities(
             np.sqrt(probs), qubits, len(measured_qubits)
         )

tests/test_backends_clifford.py
@@ -62,22 +62,50 @@ def test_two_qubits_gates(gate):
     numpy_bkd.assert_allclose(clifford_state, numpy_state, atol=1e-8)
 
 
-def test_random_clifford_circuit():
-    for _ in range(4):
-        measured_qubits = np.random.choice(range(5), size=3, replace=False)
-        print("----------------- Numpy -------------------")
-        c = random_clifford(5)
-        c.add(gates.M(*measured_qubits))
-        # for q in np.random.choice(range(5), size=3, replace=False):
-        #    c.add(gates.M(q))
-        result = numpy_bkd.execute_circuit(c)
-        print(result.frequencies())
-        print(result.probabilities(measured_qubits[:2]))
-        print("----------------- Clifford -------------------")
-        c = random_clifford(5)
-        c.add(gates.M(*measured_qubits))
-        result = clifford_bkd.execute_circuit(c)
-        # print(result.samples())
-        print(result.frequencies())
-        print(result.probabilities(measured_qubits[:2]))
-        assert False
+MEASURED_QUBITS = np.random.choice(range(5), size=3, replace=False)
+
+
+@pytest.mark.parametrize(
+    "prob_qubits",
+    [
+        range(5),
+        np.random.choice(MEASURED_QUBITS, size=2, replace=False),
+        [0],
+        [1],
+        [2],
+        [3],
+        [4],
+    ],
+)
+def test_random_clifford_circuit(prob_qubits):
+    c = random_clifford(5)
+    # c.add(gates.M(*MEASURED_QUBITS))
+    c.density_matrix = True
+    c_copy = c.copy()
+    c.add(gates.M(*MEASURED_QUBITS))
+    c_copy.add(gates.M(*MEASURED_QUBITS))
+    numpy_result = numpy_bkd.execute_circuit(c, nshots=1000)
+    clifford_result = clifford_bkd.execute_circuit(c_copy)
+    numpy_bkd.assert_allclose(numpy_result.state(), clifford_result.state())
+    if not set(prob_qubits).issubset(set(MEASURED_QUBITS)):
+        with pytest.raises(RuntimeError) as excinfo:
+            numpy_bkd.assert_allclose(
+                numpy_result.probabilities(prob_qubits),
+                clifford_result.probabilities(prob_qubits),
+            )
+            assert (
+                str(excinfo.value)
+                == f"Asking probabilities for qubits {prob_qubits}, but only qubits {MEASURED_QUBITS} were measured."
+            )
+    else:
+        print(f"Measured qubits: {MEASURED_QUBITS}")
+        print(f"Probs qubits: {prob_qubits}")
+        print(f"Numpy Frequencies: {numpy_result.frequencies()}")
+        print(f"Clifford Frequencies: {clifford_result.frequencies()}")
+        print(f"Numpy probs: {numpy_result.probabilities(MEASURED_QUBITS)}")
+        print(f"Clifford probs: {clifford_result.probabilities()}")
+        numpy_bkd.assert_allclose(
+            numpy_result.probabilities(prob_qubits),
+            clifford_result.probabilities(prob_qubits),
+            atol=1e-1,
+        )