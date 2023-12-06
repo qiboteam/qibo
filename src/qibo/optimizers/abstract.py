bd3ca79234b19b0b5994330a37efa6e9fbfaef26
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-12-06 15:15:29 +0100

    mv loss, args and params to fit method

--- src/qibo/optimizers/abstract.py
+++ src/qibo/optimizers/abstract.py
@@ -19,27 +19,37 @@ def check_options(function, options):
 
 
 class Optimizer:
-    """
-    Agnostic optimizer class, on top of which specific optimizers are built.
+    """Qibo optimizer class."""
 
-    Args:
-        initial_parameters (np.ndarray or list): array with initial values
-            for gate parameters.
-        loss (callable): loss function to train on.
-        args (tuple): tuple containing loss function arguments.
-    """
+    def __init__(self, options={}):
+        """
+        Args:
+            options (dict): optimizer's parameters. This dictionary has to be
+                filled according to each specific optimizer interfate.
+        """
+        self.options = options
 
-    def __init__(self, initial_parameters, args=(), loss=None):
-        # saving to class objects
-        self.loss = loss
+    def set_options(self, updates):
+        """Update self.options dictionary"""
+        self.options.update(updates)
+
+    def fit(self, loss, initial_parameters, args=(), fit_options={}):
+        """
+        Compute the optimization strategy given loss function generalities.
+
+        Args:
+            loss (callable): loss function to train on.
+            initial_parameters (np.ndarray or list): array with initial values
+                for gate parameters.
+            args (tuple): tuple containing loss function arguments.
+            fit_options (dict): extra options to customize the fit.
+        """
         if not isinstance(args, tuple):
             self.args = (args,)
         else:
             self.args = args
 
         self.params = initial_parameters
-
-        self.options = {}
 
         if not isinstance(initial_parameters, np.ndarray) and not isinstance(
             initial_parameters, list
@@ -48,14 +58,3 @@ class Optimizer:
                 TypeError,
                 "Parameters must be a list of Parameter objects or a numpy array.",
             )
-
-    def set_options(self, updates):
-        """Update self.options dictionary"""
-        self.options.update(updates)
-
-    def fit(self):
-        """Compute the optimization strategy."""
-        raise_error(
-            NotImplementedError,
-            "fit method is not implemented in the parent Optimizer class.",
-        )
