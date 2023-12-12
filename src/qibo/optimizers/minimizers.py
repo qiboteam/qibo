ce078f93f4edae32121955b850d951448ecec788
WanderingMike <michael.tsesmelis99@gmail.com>
2023-12-12 23:43:42 +0100

    new

--- src/qibo/optimizers/minimizers.py
+++ src/qibo/optimizers/minimizers.py
@@ -46,7 +46,9 @@ class ScipyMinimizer(Optimizer):
         # update options with minimizer extra options
         self.set_options({"options": fit_options})
         print("####", self.options)
-        print(initial_parameters, loss(initial_parameters, *args), self.options)
+        print(
+            initial_parameters, loss(np.array(initial_parameters), *args), self.options
+        )
 
         r = minimize(
             loss,
