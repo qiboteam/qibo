27c65633e0a4e6474ff2513e74bc8f1e475fd927
Renato Mello <renato.msf@gmail.com>
2023-12-13 14:36:47 +0400

    add `quantum_info.quantum_networks` to quantum_info.__init__`

--- src/qibo/quantum_info/__init__.py
+++ src/qibo/quantum_info/__init__.py
@@ -1,5 +1,6 @@
 from qibo.quantum_info.basis import *
 from qibo.quantum_info.metrics import *
+from qibo.quantum_info.quantum_networks import *
 from qibo.quantum_info.random_ensembles import *
 from qibo.quantum_info.superoperator_transformations import *
 from qibo.quantum_info.utils import *
