83d7ba8eb14fd8a77ee8c3a09efb951b5febf0b8
Renato Mello <renato.msf@gmail.com>
2023-12-19 14:02:30 +0400

    diagonal and semi-diagonal

--- src/qibo/models/encodings.py
+++ src/qibo/models/encodings.py
@@ -10,7 +10,7 @@ from qibo.config import raise_error
 from qibo.models.circuit import Circuit
 
 
-def unary_encoder(data, architecture: str = "diagonal"):
+def unary_encoder(data, architecture: str = "tree"):
     """Creates circuit that performs the unary encoding of ``data``.
 
     Given a classical ``data`` array :math:`\\mathbf{x} \\in \\mathbb{R}^{d}` such that
@@ -45,10 +45,6 @@ def unary_encoder(data, architecture: str = "diagonal"):
             TypeError,
             f"``data`` must be a 1-dimensional array, but it has dimensions {data.shape}.",
         )
-    elif not math.log2(data.shape[0]).is_integer():
-        raise_error(
-            ValueError, f"len(data) must be a power of 2, but it is {len(data)}."
-        )
 
     if not isinstance(architecture, str):
         raise_error(
@@ -58,33 +54,44 @@ def unary_encoder(data, architecture: str = "diagonal"):
     if architecture not in ["diagonal", "semi-diagonal", "tree"]:
         raise_error(ValueError, f"``architecture`` {architecture} not found.")
 
-    nqubits = len(data)
-    j_max = int(nqubits / 2)
+    if architecture == "tree" and not math.log2(data.shape[0]).is_integer():
+        raise_error(
+            ValueError,
+            "When ``architecture = 'tree'``, len(data) must be a power of 2. "
+            + f"However, it is {len(data)}.",
+        )
 
-    circuit, _ = _generate_rbs_pairs(nqubits, architecture=architecture)
+    nqubits = len(data)
+
+    circuit = Circuit(nqubits)
+    if architecture == "semi-diagonal":
+        circuit.add(gates.X(int(nqubits / 2)))
+    else:
+        circuit.add(gates.X(nqubits - 1))
+    circuit_rbs, _ = _generate_rbs_pairs(nqubits, architecture=architecture)
+    circuit += circuit_rbs
 
     # calculating phases and setting circuit parameters
-    r_array = np.zeros(nqubits - 1, dtype=float)
-    phases = np.zeros(nqubits - 1, dtype=float)
-    for j in range(1, j_max + 1):
-        r_array[j_max + j - 2] = math.sqrt(data[2 * j - 1] ** 2 + data[2 * j - 2] ** 2)
-        theta = math.acos(data[2 * j - 2] / r_array[j_max + j - 2])
-        if data[2 * j - 1] < 0.0:
-            theta = 2 * math.pi - theta
-        phases[j_max + j - 2] = theta
+    phases = _rbs_angles_deterministic(data, nqubits, architecture)
+    # r_array = np.zeros(nqubits - 1, dtype=float)
+    # phases = np.zeros(nqubits - 1, dtype=float)
+    # for j in range(1, j_max + 1):
+    #     r_array[j_max + j - 2] = math.sqrt(data[2 * j - 1] ** 2 + data[2 * j - 2] ** 2)
+    #     theta = math.acos(data[2 * j - 2] / r_array[j_max + j - 2])
+    #     if data[2 * j - 1] < 0.0:
+    #         theta = 2 * math.pi - theta
+    #     phases[j_max + j - 2] = theta
 
-    for j in range(j_max - 1, 0, -1):
-        r_array[j - 1] = math.sqrt(r_array[2 * j] ** 2 + r_array[2 * j - 1] ** 2)
-        phases[j - 1] = math.acos(r_array[2 * j - 1] / r_array[j - 1])
+    # for j in range(j_max - 1, 0, -1):
+    #     r_array[j - 1] = math.sqrt(r_array[2 * j] ** 2 + r_array[2 * j - 1] ** 2)
+    #     phases[j - 1] = math.acos(r_array[2 * j - 1] / r_array[j - 1])
 
     circuit.set_parameters(phases)
 
     return circuit
 
 
-def unary_encoder_random_gaussian(
-    nqubits: int, architecture: str = "parallel", seed=None
-):
+def unary_encoder_random_gaussian(nqubits: int, architecture: str = "tree", seed=None):
     """Creates a circuit that performs the unary encoding of a random Gaussian state.
 
     Given :math:`d` qubits, encodes the quantum state
@@ -153,7 +160,10 @@ def unary_encoder_random_gaussian(
         a=0, b=2 * math.pi, seed=local_state
     )
 
-    circuit, pairs_rbs = _generate_rbs_pairs(nqubits, architecture)
+    circuit = Circuit(nqubits)
+    circuit.add(gates.X(nqubits - 1))
+    circuit_rbs, pairs_rbs = _generate_rbs_pairs(nqubits, architecture)
+    circuit += circuit_rbs
 
     phases = []
     for depth, row in enumerate(pairs_rbs, 1):
@@ -168,6 +178,17 @@ def _generate_rbs_pairs(nqubits: int, architecture: str):
     """Generating list of indexes representing the RBS connections
     and creating circuit with all RBS initialised with 0.0 phase."""
 
+    if architecture == "diagonal":
+        pairs_rbs = np.arange(nqubits)
+        pairs_rbs = [[pair] for pair in zip(pairs_rbs[:-1], pairs_rbs[1:])]
+
+    if architecture == "semi-diagonal":
+        index = int(nqubits / 2) - 1
+        pairs_rbs = [[(index, index + 1)]]
+        for k in range(index):
+            target, control = pairs_rbs[k][0][0], pairs_rbs[k][-1][1]
+            pairs_rbs.append([(target - 1, target), (control, control + 1)])
+
     if architecture == "tree":
         pairs_rbs = [[(0, int(nqubits / 2))]]
         indexes = list(np.array(pairs_rbs).flatten())
@@ -178,17 +199,64 @@ def _generate_rbs_pairs(nqubits: int, architecture: str):
             pairs_rbs += pairs_rbs_per_depth
             indexes = list(np.array(pairs_rbs_per_depth).flatten())
 
-        pairs_rbs = [
-            [(nqubits - 1 - a, nqubits - 1 - b) for a, b in row] for row in pairs_rbs
-        ]
+    pairs_rbs = [
+        [(nqubits - 1 - a, nqubits - 1 - b) for a, b in row] for row in pairs_rbs
+    ]
 
-        circuit = Circuit(nqubits)
-        circuit.add(gates.X(nqubits - 1))
-        for row in pairs_rbs:
-            for pair in row:
-                circuit.add(gates.RBS(*pair, 0.0, trainable=True))
+    circuit = Circuit(nqubits)
+    for row in pairs_rbs:
+        for pair in row:
+            circuit.add(gates.RBS(*pair, 0.0, trainable=True))
 
     return circuit, pairs_rbs
+
+
+def _rbs_angles_deterministic(data, nqubits: int, architecture: str):
+    if architecture == "diagonal":
+        norm = np.linalg.norm(data)
+        denominator = norm
+        phases = []
+        for k, elem in enumerate(data[:-1], 1):
+            sin = 1 if k == 1 else math.sin(phases[-1])
+            denominator *= sin
+            phases.append(math.acos(elem / denominator))
+
+    if architecture == "semi-diagonal":
+        phi_1 = math.atan(data[0] / data[1])
+        phi_last = math.atan(data[-1] / data[-2])
+
+        phases, phases_reverse = [phi_1], [phi_last]
+        k = 1
+        while len(phases) + len(phases_reverse) < len(data) - 1:
+            phases.append(math.atan(data[k] / (data[k + 1] * math.cos(phases[k - 1]))))
+            phases_reverse.append(
+                math.atan(
+                    data[nqubits - 1 - k]
+                    / (data[nqubits - 1 - k] * math.cos(phases_reverse[-k]))
+                )
+            )
+            k += 1
+        phases.extend(phases_reverse[::-1][1:])
+
+    if architecture == "tree":
+        j_max = int(nqubits / 2)
+
+        r_array = np.zeros(nqubits - 1, dtype=float)
+        phases = np.zeros(nqubits - 1, dtype=float)
+        for j in range(1, j_max + 1):
+            r_array[j_max + j - 2] = math.sqrt(
+                data[2 * j - 1] ** 2 + data[2 * j - 2] ** 2
+            )
+            theta = math.acos(data[2 * j - 2] / r_array[j_max + j - 2])
+            if data[2 * j - 1] < 0.0:
+                theta = 2 * math.pi - theta
+            phases[j_max + j - 2] = theta
+
+        for j in range(j_max - 1, 0, -1):
+            r_array[j - 1] = math.sqrt(r_array[2 * j] ** 2 + r_array[2 * j - 1] ** 2)
+            phases[j - 1] = math.acos(r_array[2 * j - 1] / r_array[j - 1])
+
+    return phases
 
 
 class _ProbabilityDistributionGaussianLoader(rv_continuous):
