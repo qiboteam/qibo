eed752cc1ca7ccd9ab540ad10653c5f8df6c5a0a
Matthew <46650770+mho291@users.noreply.github.com>
2024-01-24 11:22:06 +0800

    Update gate_set_tomography.py
    - Added a comment on L93 to discuss.
    - Restructured the conditional statements in GST_execute_circuit().

--- src/qibo/validation/gate_set_tomography.py
+++ src/qibo/validation/gate_set_tomography.py
@@ -90,7 +90,7 @@ def reset_register(circuit, invert_register):
                 if theta is None:
                     new_circ.add(getattr(gates, data["_class"])(0))
                 else:
-                    new_circ.add(getattr(gates, data["_class"])(0, theta))
+                    new_circ.add(getattr(gates, data["_class"])(0, theta)) # <-- Not covered in tests. Thinking about future proofing when we need to invert a random gate that has theta parameter.
 
     elif invert_register == "sp_t":
         new_circ = circuit.copy()
@@ -125,9 +125,7 @@ def GST_execute_circuit(circuit, k, j, nshots=int(1e4), backend=None):
     Returns:
         numpy.float: Expectation value given by either Tr(Q_j rho_k) or Tr(Q_j O_l rho_k).
     """
-
-    if j == 0:
-        return 1.0
+    
 
     nqubits = circuit.nqubits
     if nqubits != 1 and nqubits != 2:
@@ -135,20 +133,24 @@ def GST_execute_circuit(circuit, k, j, nshots=int(1e4), backend=None):
             f"nqubits given as {nqubits}. nqubits needs to be either 1 or 2."
         )
 
-    if backend is None:  # pragma: no cover
-        backend = GlobalBackend()
-
-    result = backend.execute_circuit(circuit, nshots=nshots)
-    observables = [symbols.I, symbols.Z, symbols.Z, symbols.Z]
-    observables_list = list(product(observables, repeat=nqubits))[j]
-    observable = 1
-    for q, obs in enumerate(observables_list):
-        if obs is not symbols.I:
-            observable *= obs(q)
-    observable = SymbolicHamiltonian(observable, nqubits=nqubits)
-    expectation_val = result.expectation_from_samples(observable)
-
-    return expectation_val
+    else:
+        if j == 0:
+            return 1.0
+        else:
+            if backend is None:  # pragma: no cover
+                backend = GlobalBackend()
+        
+            result = backend.execute_circuit(circuit, nshots=nshots)
+            observables = [symbols.I, symbols.Z, symbols.Z, symbols.Z]
+            observables_list = list(product(observables, repeat=nqubits))[j]
+            observable = 1
+            for q, obs in enumerate(observables_list):
+                if obs is not symbols.I:
+                    observable *= obs(q)
+            observable = SymbolicHamiltonian(observable, nqubits=nqubits)
+            expectation_val = result.expectation_from_samples(observable)
+        
+            return expectation_val
 
 
 def GST(
