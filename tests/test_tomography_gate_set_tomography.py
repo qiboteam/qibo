6020e08ea36346ae54e7f4cf5eb2994007259706
Matthew <46650770+mho291@users.noreply.github.com>
2024-01-24 17:23:45 +0800

    Update test_tomography_gate_set_tomography.py

--- tests/test_tomography_gate_set_tomography.py
+++ tests/test_tomography_gate_set_tomography.py
@@ -278,4 +278,14 @@ def test_GST_two_qubit_with_gate_with_valid_reset_register_string():
     assert np.shape(result) == (16, 16)
 
 
+def test_GST_empty_circuit_with_invalid_qb():
+    nqubits = 3
+    
+    # Check if ValueError is raised
+    with pytest.raises(ValueError, match="nqubits needs to be either 1 or 2"):
+        result = GST(
+        nqubits, gate=None, invert_register=None, noise_model=None, backend=None
+    )
+
+
 ##################################################################################
