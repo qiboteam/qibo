8994079ddf28f6d40e52c7f84f90044f22e1e4ce
Renato Mello <renato.msf@gmail.com>
2024-01-28 09:15:44 +0400

    update `__init__.py`

--- src/qibo/quantum_info/__init__.py
+++ src/qibo/quantum_info/__init__.py
@@ -1,5 +1,7 @@
 from qibo.quantum_info.basis import *
 from qibo.quantum_info.clifford import *
+from qibo.quantum_info.entanglement import *
+from qibo.quantum_info.entropies import *
 from qibo.quantum_info.metrics import *
 from qibo.quantum_info.random_ensembles import *
 from qibo.quantum_info.superoperator_transformations import *
