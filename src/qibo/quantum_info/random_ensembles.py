287528256b3cdbb15489a63e1873e1fe6a0050d6
Renato Mello <renato.msf@gmail.com>
2024-02-04 04:08:28 +0000

    Update src/qibo/quantum_info/random_ensembles.py
    Co-authored-by: Alejandro Sopena <44305203+AlejandroSopena@users.noreply.github.com>

--- src/qibo/quantum_info/random_ensembles.py
+++ src/qibo/quantum_info/random_ensembles.py
@@ -424,6 +424,7 @@ def random_statevector(dims: int, seed=None, backend=None):
     state = local_state.standard_normal(dims).astype(complex)
     state += 1.0j * local_state.standard_normal(dims)
     state /= np.linalg.norm(state)
+    state =  backend.cast(state, dtype=state.dtype)
 
     return backend.cast(state, dtype=state.dtype)
 
