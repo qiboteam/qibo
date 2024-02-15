4722f501cc87229be80ad412d5a67f04cb767bef
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-02-15 17:36:58 +0100

    fix math rendering

--- src/qibo/quantum_info/random_ensembles.py
+++ src/qibo/quantum_info/random_ensembles.py
@@ -32,7 +32,8 @@ class _probability_distribution_sin(rv_continuous):  # pragma: no cover
 
 
 def uniform_sampling_U3(ngates: int, seed=None, backend=None):
-    """Samples parameters for Haar-random :math:`U_{3}`s (:class:`qibo.gates.U3`).
+    """
+    Samples parameters for Haar-random :math:`U_{3}` (``qibo.gates.U3``).
 
     Args:
         ngates (int): Total number of :math:`U_{3}`s to be sampled.
