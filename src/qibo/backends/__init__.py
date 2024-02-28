dab48f490453b9a99e1eec52353cfa68a1acef01
tankya2 <tankya2@ihpc.a-star.edu.sg>
2024-02-28 17:55:07 +0800

    Add qibotn option

--- src/qibo/backends/__init__.py
+++ src/qibo/backends/__init__.py
@@ -33,6 +33,14 @@ def construct_backend(backend, **kwargs):
 
     elif backend == "qibolab":  # pragma: no cover
         from qibolab.backends import QibolabBackend  # pylint: disable=E0401
+        
+    elif backend == "qibotn":  # pragma: no cover
+        from qibotn.backends import CuTensorNet, QuimbBackend  # pylint: disable=E0401
+ 
+        if platform == "cutensornet":  # pragma: no cover
+            return CuTensorNet(runcard)
+        elif platform == "qutensornet":  # pragma: no cover
+            return QuimbBackend(runcard)
 
         return QibolabBackend(**kwargs)
     elif backend == "clifford":
