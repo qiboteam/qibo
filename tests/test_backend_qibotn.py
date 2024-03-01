da49e0b577719ce1a1b2b5ccbdf2d773e7588aa6
Stefano Carrazza <stefano.carrazza@cern.ch>
2024-03-01 19:37:51 +0100

    removing cuquantum

--- tests/test_backend_qibotn.py
+++ tests/test_backend_qibotn.py
@@ -1,18 +1,9 @@
-import pytest
-from qibotn.backends import CuTensorNet, QuimbBackend
+from qibotn.backends import QuimbBackend
 
 import qibo
 from qibo.backends import GlobalBackend
 
 
-@pytest.mark.parametrize("platform", ["cutensornet", "qutensornet"])
-def test_backend_qibotn(platform):
-
-    qibo.set_backend(backend="qibotn", platform=platform, runcard=None)
-
-    if platform == "qutensornet":
-        qibotn_backend = QuimbBackend
-    else:
-        qibotn_backend = CuTensorNet
-
-    assert isinstance(GlobalBackend(), qibotn_backend)
+def test_backend_qibotn():
+    qibo.set_backend(backend="qibotn", platform="qutensornet", runcard=None)
+    assert isinstance(GlobalBackend(), QuimbBackend)
