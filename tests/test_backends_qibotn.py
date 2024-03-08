ac9a0b24c37f7dc168faab99522e804af31b684d
Renato Mello <renato.msf@gmail.com>
2024-03-08 13:16:29 +0400

    fix dill test

--- tests/test_backends_qibotn.py
+++ tests/test_backends_qibotn.py
@@ -10,5 +10,7 @@ from qibo.backends import GlobalBackend
 
 
 def test_backend_qibotn():
-    qibo.set_backend(backend="qibotn", platform="qutensornet")
+    qibo.set_backend(backend="qibotn", platform="qutensornet", runcard=None)
     assert isinstance(GlobalBackend(), QuimbBackend)
+
+    qibo.set_backend("numpy")
