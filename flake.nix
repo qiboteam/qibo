3c45b3225ef187ae36699c2044b323f3a2082cf4
Alessandro Candido <candido.ale@gmail.com>
2024-03-14 11:17:52 +0100

    build: Upgrade Nix shell to use Poetry 1.8

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
     nixpkgs-python = {
