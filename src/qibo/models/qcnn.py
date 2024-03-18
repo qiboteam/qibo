fd296e3dd8cd7fe40f88da4a6ca2429380e9fee3
J F Kong <jfkong34@gmail.com>
2024-03-19 00:59:18 +0800

    corrected docstrings

--- src/qibo/models/qcnn.py
+++ src/qibo/models/qcnn.py
@@ -67,7 +67,7 @@ class QuantumCNN:
             nlayers (int): The number of layers in the QCNN.
             nclasses (int, optional): The number of classes for the classification task. Defaults to 2.
             params (np.ndarray, optional): The initial parameters for the QCNN. If None, random parameters are generated. Defaults to None.
-            twoqubitansatz (qibo.core.circuit.Circuit, optional): A two-qubit ansatz for the convolutional layers. If None, a default ansatz is used. Defaults to None.
+            twoqubitansatz (qibo.models.circuit.Circuit, optional): A two-qubit ansatz for the convolutional layers. If None, a default ansatz is used. Defaults to None.
             copy_init_state (bool, optional): Whether to copy the initial state for each shot in the simulation. If None, the behavior depends on the backend. Defaults to None.
 
         Raises:
