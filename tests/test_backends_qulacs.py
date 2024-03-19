4f0aacbd1741a934b9a07dff34e6d9551416d8e1
Andrea Papaluca <andreapapaluca@gmail.com>
2024-03-19 14:33:42 +0400

    fix: improved coverage

--- tests/test_backends_qulacs.py
+++ tests/test_backends_qulacs.py
@@ -4,26 +4,34 @@ import pytest
 
 from qibo import gates
 from qibo.backends import GlobalBackend, NumpyBackend, QulacsBackend, set_backend
-from qibo.quantum_info import random_clifford
+from qibo.quantum_info import random_clifford, random_density_matrix, random_statevector
 
 numpy_bkd = NumpyBackend()
 
 
 @pytest.mark.parametrize("density_matrix", [True, False])
-def test_qulacs(density_matrix):
+@pytest.mark.parametrize("with_measurements", [True, False])
+def test_qulacs(density_matrix, with_measurements):
     c = random_clifford(3, backend=numpy_bkd, density_matrix=density_matrix)
-    measured_qubits = random.sample([0, 1, 2], 2)
-    c.add(gates.M(*measured_qubits))
+    if with_measurements:
+        measured_qubits = random.sample([0, 1, 2], 2)
+        c.add(gates.M(*measured_qubits))
     qulacs_bkd = QulacsBackend()
     nshots = 1000
+    initial_state = (
+        random_density_matrix(3, backend=numpy_bkd)
+        if density_matrix
+        else random_statevector(3, backend=numpy_bkd)
+    )
     qulacs_res = qulacs_bkd.execute_circuit(c, nshots=nshots)
     numpy_res = numpy_bkd.execute_circuit(c, nshots=nshots)
     numpy_bkd.assert_allclose(numpy_res.probabilities(), qulacs_res.probabilities())
-    numpy_freq = numpy_res.frequencies(binary=True)
-    qulacs_freq = qulacs_res.frequencies(binary=True)
-    numpy_freq = [numpy_freq.get(state, 0) / nshots for state in range(8)]
-    qulacs_freq = [qulacs_freq.get(state, 0) / nshots for state in range(8)]
-    numpy_bkd.assert_allclose(numpy_freq, qulacs_freq, atol=1e-1)
+    if with_measurements:
+        numpy_freq = numpy_res.frequencies(binary=True)
+        qulacs_freq = qulacs_res.frequencies(binary=True)
+        numpy_freq = [numpy_freq.get(state, 0) / nshots for state in range(8)]
+        qulacs_freq = [qulacs_freq.get(state, 0) / nshots for state in range(8)]
+        numpy_bkd.assert_allclose(numpy_freq, qulacs_freq, atol=1e-1)
 
 
 def test_set_backend():
