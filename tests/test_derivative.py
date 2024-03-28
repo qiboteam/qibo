f1b8ac262ac67aec8c05ae9ea32d79564184d9d0
simone bordoni <simone.bordoni@uniroma1.it>
2024-03-28 17:38:21 +0400

    fic psr for pytorch backend

--- tests/test_derivative.py
+++ tests/test_derivative.py
@@ -2,7 +2,6 @@ import numpy as np
 import pytest
 
 from qibo import Circuit, gates, hamiltonians
-from qibo.backends.pytorch import PyTorchBackend
 from qibo.derivative import finite_differences, parameter_shift
 from qibo.symbols import Z
 
