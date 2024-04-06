fixed sign issue, now double_bracket.py has a scaling that works; next double check that the gci class matches that behavior & finish writing thests

src/qibo/models/dbi/double_bracket.py
@@ -134,9 +134,9 @@ class DoubleBracketIteration:
             sqrt_step = np.sqrt(step)
             operator = (
                 self.h.exp(-np.sqrt(step))
-                @ self.backend.calculate_matrix_exp(-np.sqrt(step), d)
-                @ self.h.exp(np.sqrt(step))
                 @ self.backend.calculate_matrix_exp(np.sqrt(step), d)
+                @ self.h.exp(np.sqrt(step))
+                @ self.backend.calculate_matrix_exp(-np.sqrt(step), d)
             )
         else:
             raise_error(ValueError, f"Mode { mode } not recognized.")