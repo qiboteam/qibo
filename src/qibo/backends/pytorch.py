fixed gradients problem in variational model

src/qibo/backends/pytorch.py
@@ -8,20 +8,26 @@ from qibo.backends.numpy import NumpyBackend
 
 
 class TorchMatrices(NumpyMatrices):
-    """Matrix representation of every gate as a torch Tensor."""
+    """Matrix representation of every gate as a torch Tensor.
 
-    def __init__(self, dtype):
+    Args:
+        dtype (torch.dtype): Data type of the matrices.
+        requires_grad (bool): If ``True`` the matrices require gradient.
+    """
+
+    def __init__(self, dtype, requires_grad):
         import torch  # pylint: disable=import-outside-toplevel
 
         super().__init__(dtype)
         self.np = torch
         self.dtype = dtype
+        self.requires_grad = requires_grad
 
     def _cast(self, x, dtype):
         return self.np.as_tensor(x, dtype=dtype)
 
     def _cast_parameter(self, x):
-        return self.np.tensor(x)
+        return self.np.tensor(x, requires_grad=self.requires_grad)
 
     def Unitary(self, u):
         return self._cast(u, dtype=self.dtype)
@@ -31,6 +37,9 @@ class PyTorchBackend(NumpyBackend):
     def __init__(self):
         super().__init__()
         import torch  # pylint: disable=import-outside-toplevel
+
+        # Global variable to enable or disable gradient calculation
+        self.gradients = True
 
         self.np = torch
 
@@ -42,7 +51,7 @@ class PyTorchBackend(NumpyBackend):
         }
 
         self.dtype = self._torch_dtype(self.dtype)
-        self.matrices = TorchMatrices(self.dtype)
+        self.matrices = TorchMatrices(self.dtype, requires_grad=self.gradients)
         self.device = self.np.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.nthreads = 0
         self.tensor_types = (self.np.Tensor, np.ndarray)
@@ -52,6 +61,11 @@ class PyTorchBackend(NumpyBackend):
         self.np.expand_dims = self.np.unsqueeze
         self.np.mod = self.np.remainder
         self.np.right_shift = self.np.bitwise_right_shift
+
+    def requires_grad(self, requires_grad):
+        """Enable or disable gradient calculation."""
+        self.gradients = requires_grad
+        self.matrices.requires_grad = requires_grad
 
     def _torch_dtype(self, dtype):
         if dtype == "float":
@@ -66,6 +80,7 @@ class PyTorchBackend(NumpyBackend):
         x,
         dtype=None,
         copy: bool = False,
+        requires_grad: bool = None,
     ):
         """Casts input as a Torch tensor of the specified dtype.
 
@@ -80,7 +95,13 @@ class PyTorchBackend(NumpyBackend):
                 Defaults to ``None``.
             copy (bool, optional): If ``True``, the input tensor is copied before casting.
                 Defaults to ``False``.
+            requires_grad (bool, optional): If ``True``, the input tensor requires gradient.
+                If ``False``, the input tensor does not require gradient.
+                If ``None``, the default gradient setting of the backend is used.
         """
+        if requires_grad is None:
+            requires_grad = self.gradients
+
         if dtype is None:
             dtype = self.dtype
         elif isinstance(dtype, type):
@@ -93,7 +114,7 @@ class PyTorchBackend(NumpyBackend):
         elif isinstance(x, list) and all(isinstance(row, self.np.Tensor) for row in x):
             x = self.np.stack(x)
         else:
-            x = self.np.tensor(x, dtype=dtype)
+            x = self.np.tensor(x, dtype=dtype, requires_grad=requires_grad)
 
         if copy:
             return x.clone()

src/qibo/hamiltonians/hamiltonians.py
@@ -181,7 +181,7 @@ class Hamiltonian(AbstractHamiltonian):
         h = self.matrix
         h2 = Hamiltonian(nqubits=self.nqubits, matrix=h @ h, backend=self.backend)
         average_h2 = self.backend.calculate_expectation_state(h2, state, normalize=True)
-        return np.sqrt(np.abs(average_h2 - energy**2))
+        return self.backend.np.sqrt(self.backend.np.abs(average_h2 - energy**2))
 
     def __add__(self, o):
         if isinstance(o, self.__class__):

src/qibo/optimizers.py
@@ -262,7 +262,7 @@ def sgd(loss, initial_parameters, args=(), options=None, compile=False, backend=
 
 def _sgd_torch(loss, initial_parameters, args, sgd_options, backend):
 
-    vparams = backend.np.tensor(initial_parameters, requires_grad=True)
+    vparams = initial_parameters
     optimizer = getattr(backend.np.optim, sgd_options["optimizer"])(
         params=[vparams], lr=sgd_options["learning_rate"]
     )
@@ -270,7 +270,6 @@ def _sgd_torch(loss, initial_parameters, args, sgd_options, backend):
     for e in range(sgd_options["nepochs"]):
         optimizer.zero_grad()
         l = loss(vparams, *args)
-        print("Loss_gradient: ", l.requires_grad)
         l.backward()
         optimizer.step()
 

tests/test_models_variational.py
@@ -125,6 +125,8 @@ def test_vqe(backend, method, options, compile, filename):
     hamiltonian = hamiltonians.XXZ(nqubits=nqubits, backend=backend)
     np.random.seed(0)
     initial_parameters = np.random.uniform(0, 2 * np.pi, 2 * nqubits * layers + nqubits)
+    if backend.name == "pytorch":
+        initial_parameters = backend.np.tensor(initial_parameters, requires_grad=True)
     v = models.VQE(circuit, hamiltonian)
     best, params, _ = v.minimize(
         initial_parameters, method=method, options=options, compile=compile
@@ -169,7 +171,7 @@ def test_qaoa_execution(backend, solver, dense, accel=None):
     else:
         atol = 0
 
-    target_state = np.copy(state)
+    target_state = backend.cast(state, copy=True)
     h_matrix = backend.to_numpy(h.matrix)
     m_matrix = backend.to_numpy(m.matrix)
     for i, p in enumerate(params):