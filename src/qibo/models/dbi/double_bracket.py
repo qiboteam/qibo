8a66baaefa95e6f510cbe8fd4b716d56e7dbf4d1
Edoardo-Pedicillo <edoardo.pedicillo@gmail.com>
2024-06-06 17:05:39 +0400

    test: skip NotImplementedError

--- src/qibo/models/dbi/double_bracket.py
+++ src/qibo/models/dbi/double_bracket.py
@@ -158,7 +158,7 @@ class DoubleBracketIteration:
                 @ self.backend.calculate_matrix_exp(sqrt_step, d)
             )
         else:
-            raise NotImplementedError(f"The mode {mode} is not supported")
+            raise NotImplementedError(f"The mode {mode} is not supported") # pragma: no cover
 
         return operator
 
