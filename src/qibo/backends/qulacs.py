5fe371a098ea3d70ab8d52a2bf08aeaf181b7106
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2024-06-11 12:21:00 +0200

    fix: restored macos-latest and skipping qulacs tests when not installed

--- src/qibo/backends/qulacs.py
+++ src/qibo/backends/qulacs.py
@@ -1,12 +1,6 @@
 import re
 
 import numpy as np
-import qulacs
-from qulacs import (  # pylint: disable=no-name-in-module
-    QuantumCircuitSimulator,
-    converter,
-)
-from qulacs.circuit import QuantumCircuitOptimizer  # pylint: disable=no-name-in-module
 
 from qibo import __version__
 from qibo.backends import NumpyBackend
@@ -17,13 +11,16 @@ class QulacsBackend(NumpyBackend):
 
     def __init__(self):
         super().__init__()
+        import qulacs
+
+        self.qulacs = qulacs
         self.name = "qulacs"
         self.versions = {"qibo": __version__, "qulacs": qulacs.__version__}
         self.device = "CPU"
 
     def circuit_to_qulacs(
         self, circuit: "qibo.Circuit"
-    ) -> qulacs.QuantumCircuit:  # pylint: disable=no-member
+    ) -> "qulacs.QuantumCircuit":  # pylint: disable=no-member
         """
         Converts a qibo circuit in a qulacs circuit.
 
@@ -36,7 +33,9 @@ class QulacsBackend(NumpyBackend):
         qasm_str = re.sub("^//.+\n", "", circuit.to_qasm())
         qasm_str = re.sub(r"creg\s.+;", "", qasm_str)
         qasm_str = re.sub(r"measure\s.+;", "", qasm_str)
-        circ = converter.convert_QASM_to_qulacs_circuit(qasm_str.splitlines())
+        circ = self.qulacs.converter.convert_QASM_to_qulacs_circuit(
+            qasm_str.splitlines()
+        )
         return circ
 
     def execute_circuit(
@@ -56,11 +55,11 @@ class QulacsBackend(NumpyBackend):
         """
         circ = self.circuit_to_qulacs(circuit)
         state = (
-            qulacs.DensityMatrix(circuit.nqubits)  # pylint: disable=no-member
+            self.qulacs.DensityMatrix(circuit.nqubits)  # pylint: disable=no-member
             if circuit.density_matrix
-            else qulacs.QuantumState(circuit.nqubits)  # pylint: disable=no-member
+            else self.qulacs.QuantumState(circuit.nqubits)  # pylint: disable=no-member
         )
-        sim = QuantumCircuitSimulator(circ, state)
+        sim = self.qulacs.QuantumCircuitSimulator(circ, state)
         sim.simulate()
         if circuit.density_matrix:
             dim = 2**circuit.nqubits
