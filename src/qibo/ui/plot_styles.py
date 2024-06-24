6b07d4894f6ade7d409bb1c4d349dde9ba3f6129
sergiomtzlosa <sergiomtz.losa@gmail.com>
2024-06-24 12:57:15 +0200

    rework styles dictionary and return default style

--- src/qibo/ui/plot_styles.py
+++ src/qibo/ui/plot_styles.py
@@ -12,34 +12,44 @@ def _get_style(style_name):
         }
 
     if "fardelejo" in style_name:
-        new_values = dict()
-        new_values["facecolor"] = "#e17a02"
-        new_values["edgecolor"] = "#fef1e2"
-        new_values["linecolor"] = "#fef1e2"
-        new_values["textcolor"] = "#FFFFFF"
-        new_values["fillcolor"] = "#fef1e2"
-        new_values["gatecolor"] = "#8b4513"
-        new_values["controlcolor"] = "#fef1e2"
-        return new_values
+        return {
+            "facecolor": "#e17a02",
+            "edgecolor": "#fef1e2",
+            "linecolor": "#fef1e2",
+            "textcolor": "#FFFFFF",
+            "fillcolor": "#fef1e2",
+            "gatecolor": "#8b4513",
+            "controlcolor": "#fef1e2",
+        }
 
     if "quantumspain" in style_name:
-        new_values = dict()
-        new_values["facecolor"] = "#EDEDF4"
-        new_values["edgecolor"] = "#092D4E"
-        new_values["linecolor"] = "#092D4E"
-        new_values["textcolor"] = "#8561C3"
-        new_values["fillcolor"] = "#092D4E"
-        new_values["gatecolor"] = "#53E7CA"
-        new_values["controlcolor"] = "#092D4E"
-        return new_values
+        return {
+            "facecolor": "#EDEDF4",
+            "edgecolor": "#092D4E",
+            "linecolor": "#092D4E",
+            "textcolor": "#8561C3",
+            "fillcolor": "#092D4E",
+            "gatecolor": "#53E7CA",
+            "controlcolor": "#092D4E",
+        }
 
     if "color-blind" in style_name:
-        new_values = dict()
-        new_values["facecolor"] = "#d55e00"
-        new_values["edgecolor"] = "#f0e442"
-        new_values["linecolor"] = "#f0e442"
-        new_values["textcolor"] = "#f0e442"
-        new_values["fillcolor"] = "#cc79a7"
-        new_values["gatecolor"] = "#d55e00"
-        new_values["controlcolor"] = "#f0e442"
-        return new_values
+        return {
+            "facecolor": "#d55e00",
+            "edgecolor": "#f0e442",
+            "linecolor": "#f0e442",
+            "textcolor": "#f0e442",
+            "fillcolor": "#cc79a7",
+            "gatecolor": "#d55e00",
+            "controlcolor": "#f0e442",
+        }
+
+    return {
+        "facecolor": "w",
+        "edgecolor": "#000000",
+        "linecolor": "k",
+        "textcolor": "k",
+        "fillcolor": "#000000",
+        "gatecolor": "w",
+        "controlcolor": "#000000",
+    }
