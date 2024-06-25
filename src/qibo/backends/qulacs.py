9a7fdaad17a0227998dd04c860e3faf217dffdc0
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2024-06-25 16:01:13 +0200

    feat: raise error with initial_state + removed some attributes from the backend

--- src/qibo/backends/qulacs.py
+++ src/qibo/backends/qulacs.py
@@ -1,46 +1,45 @@
 import re
 
 import numpy as np
+import qulacs  # pylint: disable=import-error
+from qulacs import (  # pylint: disable=no-name-in-module, import-error
+    QuantumCircuitSimulator,
+    converter,
+)
 
 from qibo import __version__
 from qibo.backends import NumpyBackend
+from qibo.config import raise_error
 from qibo.result import CircuitResult, QuantumState
+
+
+def circuit_to_qulacs(
+    circuit: "qibo.Circuit",
+) -> "qulacs.QuantumCircuit":  # pylint: disable=no-member
+    """
+    Converts a qibo circuit in a qulacs circuit.
+
+    Args:
+        circuit (:class:`qibo.models.circuit.Circuit`): Input circuit to convert.
+
+    Returns:
+        qulacs.QuantumCircuit: The converted qulacs circuit.
+    """
+    qasm_str = re.sub("^//.+\n", "", circuit.to_qasm())
+    qasm_str = re.sub(r"creg\s.+;", "", qasm_str)
+    qasm_str = re.sub(r"measure\s.+;", "", qasm_str)
+    circ = converter.convert_QASM_to_qulacs_circuit(qasm_str.splitlines())
+    return circ
 
 
 class QulacsBackend(NumpyBackend):
 
     def __init__(self):
         super().__init__()
-        import qulacs  # pylint: disable=import-error
-        from qulacs import (  # pylint: disable=no-name-in-module, import-error
-            QuantumCircuitSimulator,
-            converter,
-        )
 
-        self.qulacs = qulacs
-        self.simulator = QuantumCircuitSimulator
-        self.converter = converter
         self.name = "qulacs"
         self.versions = {"qibo": __version__, "qulacs": qulacs.__version__}
         self.device = "CPU"
-
-    def circuit_to_qulacs(
-        self, circuit: "qibo.Circuit"
-    ) -> "qulacs.QuantumCircuit":  # pylint: disable=no-member
-        """
-        Converts a qibo circuit in a qulacs circuit.
-
-        Args:
-            circuit (:class:`qibo.models.circuit.Circuit`): Input circuit to convert.
-
-        Returns:
-            qulacs.QuantumCircuit: The converted qulacs circuit.
-        """
-        qasm_str = re.sub("^//.+\n", "", circuit.to_qasm())
-        qasm_str = re.sub(r"creg\s.+;", "", qasm_str)
-        qasm_str = re.sub(r"measure\s.+;", "", qasm_str)
-        circ = self.converter.convert_QASM_to_qulacs_circuit(qasm_str.splitlines())
-        return circ
 
     def execute_circuit(
         self,
@@ -58,13 +57,18 @@ class QulacsBackend(NumpyBackend):
         Returns:
             :class:`qibo.result.CircuitResult`: Object storing to the final results.
         """
-        circ = self.circuit_to_qulacs(circuit)
+        if initial_state is not None:
+            raise_error(
+                NotImplementedError,
+                "The use of an initial state is not supported yet by the `QulacsBackend`.",
+            )
+        circ = circuit_to_qulacs(circuit)
         state = (
-            self.qulacs.DensityMatrix(circuit.nqubits)  # pylint: disable=no-member
+            qulacs.DensityMatrix(circuit.nqubits)  # pylint: disable=no-member
             if circuit.density_matrix
-            else self.qulacs.QuantumState(circuit.nqubits)  # pylint: disable=no-member
+            else qulacs.QuantumState(circuit.nqubits)  # pylint: disable=no-member
         )
-        sim = self.simulator(circ, state)
+        sim = QuantumCircuitSimulator(circ, state)
         sim.simulate()
         if circuit.density_matrix:
             dim = 2**circuit.nqubits
--- tests/test_backends_qulacs.py
+++ tests/test_backends_qulacs.py
@@ -1,8 +1,9 @@
 import random
 
+import numpy as np
 import pytest
 
-from qibo import gates
+from qibo import Circuit, gates
 from qibo.backends import GlobalBackend, NumpyBackend, QulacsBackend, set_backend
 from qibo.quantum_info import random_clifford, random_density_matrix, random_statevector
 
@@ -27,6 +28,14 @@ def test_qulacs(density_matrix, with_measurements):
         numpy_freq = [numpy_freq.get(state, 0) / nshots for state in range(8)]
         qulacs_freq = [qulacs_freq.get(state, 0) / nshots for state in range(8)]
         numpy_bkd.assert_allclose(numpy_freq, qulacs_freq, atol=1e-1)
+
+
+def test_initial_state_error():
+    c = Circuit(1)
+    qulacs_bkd = QulacsBackend()
+    initial_state = np.array([0.0, 1.0])
+    with pytest.raises(NotImplementedError):
+        qulacs_bkd.execute_circuit(c, initial_state=initial_state)
 
 
 def test_set_backend():
