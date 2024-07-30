ed06f01973e207666b43111c06a238e3911ac336
Renato Mello <renato.msf@gmail.com>
2024-07-30 19:59:21 +0400

    change shape

--- tests/test_quantum_info_metrics.py
+++ tests/test_quantum_info_metrics.py
@@ -268,12 +268,12 @@ def test_process_fidelity_and_infidelity(backend):
     d = 2
     with pytest.raises(TypeError):
         channel = np.random.rand(d**2, d**2)
-        target = np.random.rand(d**2, d**2, 1)
+        target = np.random.rand(d**2, d**2, d**2)
         channel = backend.cast(channel, dtype=channel.dtype)
         target = backend.cast(target, dtype=target.dtype)
         test = process_fidelity(channel, target, backend=backend)
     with pytest.raises(TypeError):
-        channel = random_hermitian(d**2, seed=10, backend=backend)
+        channel = random_hermitian(d**2, backend=backend)
         test = process_fidelity(channel, check_unitary=True, backend=backend)
     with pytest.raises(TypeError):
         channel = 10 * np.random.rand(d**2, d**2)
