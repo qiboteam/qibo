2708076738953d4a60991628aad786719bbd6abe
sergiomtzlosa <sergiomtz.losa@gmail.com>
2024-09-09 11:31:38 +0200

    created test for gates barrier cluster

--- tests/test_ui_mpldrawer.py
+++ tests/test_ui_mpldrawer.py
@@ -5,7 +5,7 @@ import pytest
 
 from qibo import Circuit, gates
 from qibo.ui import plot_circuit
-
+from qibo.ui.drawer_utils import FusedEndGateBarrier, FusedStartGateBarrier
 
 # defining a dummy circuit
 def circuit(nqubits=2):
@@ -21,3 +21,13 @@ def test_plot_circuit():
     circ = circuit()
     ax, _ = plot_circuit(circ)
     assert ax.title == ax.title
+
+def test_fused_gates():
+    min_q = 0
+    max_q = 1
+    l_gates = 1
+    equal_qbits = True
+    start_barrier = FusedStartGateBarrier(min_q, max_q, l_gates, equal_qbits)
+    end_barrier = FusedEndGateBarrier(min_q, max_q)
+    assert start_barrier.unitary == start_barrier.unitary
+    assert end_barrier.unitary == end_barrier.unitary
