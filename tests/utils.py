f768db72203f9437129826812d7aeaf20da89fe6
Renato Mello <renato.msf@gmail.com>
2024-10-16 13:49:43 +0400

    pylint

--- tests/utils.py
+++ tests/utils.py
@@ -8,8 +8,9 @@ def random_sparse_matrix(backend, n, sparse_type=None):
         indices = np.random.randint(0, n, size=(nonzero, 2))
         data = np.random.random(nonzero) + 1j * np.random.random(nonzero)
         data = backend.cast(data)
+
         return backend.tf.sparse.SparseTensor(indices, data, (n, n))
-    else:
-        re = sparse.rand(n, n, format=sparse_type)
-        im = sparse.rand(n, n, format=sparse_type)
-        return re + 1j * im
+
+    re = sparse.rand(n, n, format=sparse_type)
+    im = sparse.rand(n, n, format=sparse_type)
+    return re + 1j * im
