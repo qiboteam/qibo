08107cd01472e70fa297fb5af029de4b7ceb9645
changsookim <>
2024-10-16 13:55:02 +0400

    fix: Flag OR operation in FlagMeta

--- src/qibo/transpiler/unroller.py
+++ src/qibo/transpiler/unroller.py
@@ -1,6 +1,4 @@
 from enum import EnumMeta, Flag, auto
-from functools import reduce
-from operator import or_
 
 from qibo import gates
 from qibo.backends import _check_backend
@@ -21,7 +19,11 @@ class FlagMeta(EnumMeta):
     def __getitem__(self, keys):
         if isinstance(keys, str):
             return super().__getitem__(keys)
-        return reduce(or_, [super().__getitem__(key) for key in keys])
+
+        result = super().__getitem__(keys[0])
+        for key in keys[1:]:
+            result |= super().__getitem__(key)
+        return result
 
 
 class NativeGates(Flag, metaclass=FlagMeta):
