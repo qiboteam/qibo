77a73adf6b6dad476d7a31401f961b1e70cf0cd0
Matthew <matthew.ho.291@gmail.com>
2024-10-23 18:48:05 +0800

    Edited docstring

--- src/qibo/models/encodings.py
+++ src/qibo/models/encodings.py
@@ -486,7 +486,7 @@ def ghz_state(nqubits: int, **kwargs):
             For details, see the documentation of :class:`qibo.models.circuit.Circuit`.
 
     Returns:
-        :class:`qibo.models.circuit.Circuit`: GHZ state
+        :class:`qibo.models.circuit.Circuit`: Circuit that prepares the GHZ state.
     """
     if nqubits < 2:
         raise_error(
