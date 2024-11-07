acdab7fe40c4091e767ee2a303e2e55161bd8f00
Renato Mello <renato.msf@gmail.com>
2024-11-07 13:17:50 +0400

    rename partition

--- tests/test_quantum_info_entropies.py
+++ tests/test_quantum_info_entropies.py
@@ -822,14 +822,14 @@ def test_relative_tsallis_entropy(
 
 @pytest.mark.parametrize("check_hermitian", [False, True])
 @pytest.mark.parametrize("base", [2, 10, np.e, 5])
-@pytest.mark.parametrize("bipartition", [[0], [1]])
-def test_entanglement_entropy(backend, bipartition, base, check_hermitian):
+@pytest.mark.parametrize("partition", [[0], [1]])
+def test_entanglement_entropy(backend, partition, base, check_hermitian):
     with pytest.raises(TypeError):
         state = np.random.rand(2, 3)
         state = backend.cast(state, dtype=state.dtype)
         test = entanglement_entropy(
             state,
-            bipartition=bipartition,
+            partition=partition,
             base=base,
             check_hermitian=check_hermitian,
             backend=backend,
@@ -839,7 +839,7 @@ def test_entanglement_entropy(backend, bipartition, base, check_hermitian):
         state = backend.cast(state, dtype=state.dtype)
         test = entanglement_entropy(
             state,
-            bipartition=bipartition,
+            partition=partition,
             base=0,
             check_hermitian=check_hermitian,
             backend=backend,
@@ -849,7 +849,7 @@ def test_entanglement_entropy(backend, bipartition, base, check_hermitian):
             state = random_unitary(4, backend=backend)
             test = entanglement_entropy(
                 state,
-                bipartition=bipartition,
+                partition=partition,
                 base=base,
                 check_hermitian=True,
                 backend=backend,
@@ -861,7 +861,7 @@ def test_entanglement_entropy(backend, bipartition, base, check_hermitian):
 
     entang_entrop = entanglement_entropy(
         state,
-        bipartition=bipartition,
+        partition=partition,
         base=base,
         check_hermitian=check_hermitian,
         backend=backend,
@@ -885,7 +885,7 @@ def test_entanglement_entropy(backend, bipartition, base, check_hermitian):
 
     entang_entrop = entanglement_entropy(
         state,
-        bipartition=bipartition,
+        partition=partition,
         base=base,
         check_hermitian=check_hermitian,
         backend=backend,
