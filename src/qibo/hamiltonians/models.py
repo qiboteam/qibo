3655b466b8e77eef30fe7782d9f3829f1f337e2b
Renato Mello <renato.msf@gmail.com>
2024-12-08 11:30:53 +0400

    add XXX

--- src/qibo/hamiltonians/models.py
+++ src/qibo/hamiltonians/models.py
@@ -136,8 +136,8 @@ def MaxCut(nqubits, dense: bool = True, backend=None):
 
 def Heisenberg(
     nqubits,
-    coupling_constants: Union[float, int, List[int], Tuple[int, ...]],
-    external_field_strength: Union[float, int],
+    coupling_constants: Union[float, int, list, tuple],
+    external_field_strengths: Union[float, int],
     dense: bool = True,
     backend=None,
 ):
@@ -146,22 +146,28 @@ def Heisenberg(
     The general :math:`n`-qubit Hamiltonian is given by
 
     .. math::
-        H = -\\sum_{k = 1}^{n} \\, \\left(
+        \\begin{align}
+        H &= -\\sum_{k = 1}^{n} \\, \\left(
             J_{x} \\, X_{k} \\, X_{k + 1}
             + J_{y} \\, Y_{k} \\, Y_{k + 1}
-            + J_{z} \\, Z_{k} \\, Z_{k + 1} \\right)
-            - h \\, \\sum_{k = 1}^{n} \\left(X_{k} + Y_{k} + Z_{k}\\right) \\, ,
+            + J_{z} \\, Z_{k} \\, Z_{k + 1} \\right) \\\\
+        &\\quad\\,\\, - \\sum_{k = 1}^{n} \\left(
+            h_{x} \\, X_{k} + h_{y} \\, Y_{k} + h_{z} \\, Z_{k}
+            \\right) \\, ,
+        \\end{align}
 
-    where :math:`\\{J_{x}, J_{y}, J_{z}\\}` are called the ``coupling constants``,
-    :math:`h` is called the ``external field strength``, and :math:`\\{X, Y, Z\\}`
-    are the usual Pauli operators.
+    where :math:`\\{J_{x}, \\, J_{y}, \\, J_{z}\\}` are called the ``coupling constants``,
+    :math:`\\{h_{x}, \\, h_{y}, \\, h_{z}\\}` are called the ``external field strengths``, 
+    and :math:`\\{X, \\, Y, \\, Z\\}` are the usual Pauli operators.
 
     Args:
         nqubits (int): number of qubits.
-        coupling_constants (float or int or list or tuple): list or tuple with the
+        coupling_constants (list or tuple or float or int): list or tuple with the
             three coupling constants :math:`\\{J_{x}, J_{y}, J{z}\\}`.
             If ``int`` or ``float``, then :math:`J_{x} = J_{y} = J_{z}`.
-        external_field_strength (float or int): external magnetic field strength :math:`h`.
+        external_field_strength (list or tuple or float or int): list or tuple with the 
+            external magnetic field strengths :math:`\\{h_{x}, \\, h_{y}, \\, h_{z}\\}`.
+            If ``int`` or ``float``, then :math:`h_{x} = h_{y} = h_{z}`.
         dense (bool, optional): If ``True``, creates the Hamiltonian as a
             :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
             a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
@@ -177,28 +183,39 @@ def Heisenberg(
     if isinstance(coupling_constants, (list, tuple)) and len(coupling_constants) != 3:
         raise_error(
             ValueError,
-            f"When `coupling_constants` is type `int` or `list`, it must have length 3.",
+            f"When `coupling_constants` is type `int` or `list`, it must have length == 3.",
         )
-    elif isinstance(coupling_constants, (float, int)):
+
+    if (
+        isinstance(external_field_strengths, (list, tuple))
+        and len(external_field_strengths) != 3
+    ):
+        raise_error(
+            ValueError,
+            f"When `external_field_strengths` is type `int` or `list`, it must have length == 3.",
+        )
+
+    if isinstance(coupling_constants, (float, int)):
         coupling_constants = [coupling_constants] * 3
+
+    if isinstance(external_field_strengths, (float, int)):
+        external_field_strengths = [external_field_strengths] * 3
 
     backend = _check_backend(backend)
 
+    paulis = [matrices.X, matrices.Y, matrices.Z]
+
     if dense:
         condition = lambda i, j: i in {j % nqubits, (j + 1) % nqubits}
-        hx = _build_spin_model(nqubits, matrices.X, condition)
-        hy = _build_spin_model(nqubits, matrices.Y, condition)
-        hz = _build_spin_model(nqubits, matrices.Z, condition)
-        matrix = (
-            -coupling_constants[0] * hx
-            - coupling_constants[1] * hy
-            - coupling_constants[2] * hz
-        )
+        matrix = np.zeros((2**nqubits, 2**nqubits), dtype=complex)
         matrix = backend.cast(matrix, dtype=matrix.dtype)
-
-        for pauli in [matrices.X, matrices.Y, matrices.Z]:
-            matrix = matrix + (
-                external_field_strength
+        for ind, pauli in enumerate(paulis):
+            double_term = _build_spin_model(nqubits, pauli, condition)
+            double_term = backend.cast(double_term, dtype=double_term.dtype)
+            matrix = matrix - coupling_constants[ind] * double_term
+            matrix = (
+                matrix
+                + external_field_strengths[ind]
                 * _OneBodyPauli(nqubits, pauli, dense, backend).matrix
             )
 
@@ -217,19 +234,77 @@ def Heisenberg(
     terms = [HamiltonianTerm(matrix, i, i + 1) for i in range(nqubits - 1)]
     terms.append(HamiltonianTerm(matrix, nqubits - 1, 0))
 
-    if external_field_strength != 0.0:
-        terms.extend(
-            [
-                -external_field_strength * HamiltonianTerm(pauli, qubit)
-                for qubit in range(nqubits)
-                for pauli in [matrices.X, matrices.Y, matrices.Z]
-            ]
-        )
+    terms.extend(
+        [
+            -field_strength * HamiltonianTerm(pauli, qubit)
+            for qubit in range(nqubits)
+            for field_strength, pauli in zip(external_field_strengths, paulis)
+            if field_strength != 0.0
+        ]
+    )
 
     ham = SymbolicHamiltonian(backend=backend)
     ham.terms = terms
 
     return ham
+
+
+def XXX(
+    nqubits,
+    coupling_constant: Union[float, int] = 1,
+    external_field_strengths: Union[float, int, list, tuple] = [0.5, 0, 0],
+    dense: bool = True,
+    backend=None,
+):
+    """Heisenberg :math:`\\mathrm{XXX}` model with periodic boundary conditions.
+
+    The :math:`n`-qubit :math:`\\mathrm{XXX}` Hamiltonian is given by
+
+    .. math::
+        H = -J \\, \\sum_{k = 1}^{n} \\, \\left(
+            X_{k} \\, X_{k + 1} + Y_{k} \\, Y_{k + 1} + Z_{k} \\, Z_{k + 1}
+            \\right)
+            - \\sum_{k = 1}^{n} \\left(h_{x} \\, X_{k} + h_{y} \\, Y_{k}
+            + h_{z} \\, Z_{k} \\right) \\, ,
+
+    where :math:`J` is the ``coupling_constant``, :math:`\\{h_{x}, \\, h_{y}, \\, h_{z}\\}`
+    are called the ``external field strengths``, and :math:`\\{X, \\, Y, \\, Z\\}` are the usual
+    Pauli operators.
+
+    Args:
+        nqubits (int): number of qubits.
+        coupling_constant (float or int, optional): coupling constant :math:`J`.
+            Defaults to :math:`1`.
+        external_field_strengths (float or int or list or tuple, optional): list or tuple with the
+            external magnetic field strengths :math:`\\{h_{x}, \\, h_{y}, \\, h_{z}\\}`.
+            If ``int`` or ``float``, then :math:`h_{x} = h_{y} = h_{z}`.
+            Defaults to :math:`[1/2, \\, 0, \\, 0]`.
+        dense (bool, optional): If ``True``, creates the Hamiltonian as a
+            :class:`qibo.core.hamiltonians.Hamiltonian`, otherwise it creates
+            a :class:`qibo.core.hamiltonians.SymbolicHamiltonian`.
+            Defaults to ``True``.
+        backend (:class:`qibo.backends.abstract.Backend`, optional): backend to be used
+            in the execution. If ``None``, it uses the current backend.
+            Defaults to ``None``.
+
+    Returns:
+        :class:`qibo.hamiltonians.Hamiltonian` or :class:`qibo.hamiltonians.SymbolicHamiltonian`:
+        Heisenberg :math:`\\mathrm{XXX}` Hamiltonian.
+    """
+    if not isinstance(coupling_constant, (float, int)):
+        raise_error(
+            TypeError,
+            "`coupling_constant` must be type float or int, "
+            + f"but it is type {type(coupling_constant)}.",
+        )
+
+    return Heisenberg(
+        nqubits,
+        coupling_constants=[coupling_constant] * 3,
+        external_field_strengths=external_field_strengths,
+        dense=dense,
+        backend=backend,
+    )
 
 
 def XXZ(nqubits, delta=0.5, dense: bool = True, backend=None):
@@ -238,6 +313,12 @@ def XXZ(nqubits, delta=0.5, dense: bool = True, backend=None):
     .. math::
         H = \\sum _{k=0}^N \\, \\left( X_{k} \\, X_{k + 1} + Y_{k} \\, Y_{k + 1}
             + \\delta Z_{k} \\, Z_{k + 1} \\right) \\, .
+
+    Example:
+        .. testcode::
+
+            from qibo.hamiltonians import XXZ
+            h = XXZ(3) # initialized XXZ model with 3 qubits
 
     Args:
         nqubits (int): number of qubits.
@@ -251,11 +332,9 @@ def XXZ(nqubits, delta=0.5, dense: bool = True, backend=None):
             in the execution. If ``None``, it uses the current backend.
             Defaults to ``None``.
 
-    Example:
-        .. testcode::
-
-            from qibo.hamiltonians import XXZ
-            h = XXZ(3) # initialized XXZ model with 3 qubits
+    Returns:
+        :class:`qibo.hamiltonians.Hamiltonian` or :class:`qibo.hamiltonians.SymbolicHamiltonian`:
+        Heisenberg :math:`\\mathrm{XXZ}` Hamiltonian.
     """
     if nqubits < 2:
         raise_error(ValueError, "Number of qubits must be larger than one.")
