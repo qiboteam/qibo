f5b93b430e740c7c2608f7ff38240549106021b8
Renato Mello <renato.msf@gmail.com>
2024-12-16 13:46:36 +0400

    fix

--- src/qibo/models/circuit.py
+++ src/qibo/models/circuit.py
@@ -129,7 +129,7 @@ class Circuit:
             circuit = Circuit(5)  # Default wire names are [0, 1, 2, 3, 4]
             circuit = Circuit(["A", "B", "C", "D", "E"])
             circuit = Circuit(5, wire_names=["A", "B", "C", "D", "E"])
-            c = Circuit(wire_names=["A", "B", "C", "D", "E"])
+            circuit = Circuit(wire_names=["A", "B", "C", "D", "E"])
 
     Args:
         nqubits (int | list, optional): Number of qubits in the circuit or a list of wire names.
