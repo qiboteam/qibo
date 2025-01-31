0b83a3ca79b6d1f32c2f50eda27df7a87d3a0efa
Renato Mello <renato.msf@gmail.com>
2025-01-31 13:20:12 +0400

    backup

--- src/qibo/backends/__init__.py
+++ src/qibo/backends/__init__.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from qibo.backends.abstract import Backend
 from qibo.backends.clifford import CliffordBackend
+from qibo.backends.hamming_weight import HammingWeightBackend
 from qibo.backends.npmatrices import NumpyMatrices
 from qibo.backends.numpy import NumpyBackend
 from qibo.config import log, raise_error
