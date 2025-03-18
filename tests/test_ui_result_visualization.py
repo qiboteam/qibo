97b9ae3266bd7c5ec44e8e4e1827579619f7a8ba
MatteoRobbiati <robbiati.matteo@gmail.com>
2025-03-18 14:07:14 +0100

    test: testing error raises in ui.result_visualization

--- tests/test_ui_result_visualization.py
+++ tests/test_ui_result_visualization.py
@@ -17,34 +17,48 @@ matplotlib.use("agg")
 BASEPATH = str(Path(__file__).parent / "test_ui_array_images")
 
 
-def build_circuit(nqubits):
+def build_circuit(nqubits, measurements=True):
     """Helper function building a circuit."""
     circuit = Circuit(nqubits)
     for q in range(nqubits):
         circuit.add(gates.RY(q, theta=np.random.randn()))
         circuit.add(gates.RZ(q, theta=np.random.randn()))
-    circuit.add(gates.M(*range(nqubits)))
+    if measurements:
+        circuit.add(gates.M(*range(nqubits)))
     return circuit
 
 
 @pytest.mark.parametrize("mode", ["frequencies", "amplitudes", "probabilities"])
-@pytest.mark.parametrize("nqubits", [3])
-def test_visualize_state(mode, nqubits):
-    # Fixing seed for reproducibility
+def test_visualize_state(mode):
+    """Testing the plotting function."""
+
     backend = construct_backend("numpy")
     backend.set_seed(42)
     np.random.seed(42)
 
-    circuit = build_circuit(nqubits)
+    circuit = build_circuit(nqubits=3)
     outcome = backend.execute_circuit(circuit, nshots=1000)
 
     _, fig = visualize_state(
-        outcome,
+        execution_outcome=outcome,
         mode=mode,
         n_most_relevant_components=10,
     )
 
     assert (
-        match_figure_image(fig, f"{BASEPATH}/state_visualization_{mode}_{nqubits}q.npy")
-        == True
+        match_figure_image(fig, f"{BASEPATH}/state_visualization_{mode}_3q.npy") == True
     )
+
+
+@pytest.mark.parametrize("mode", ["frequencies", "non_valid"])
+def test_error_raises(mode):
+    """Testing the non valid options."""
+
+    circuit = build_circuit(nqubits=3, measurements=False)
+    outcome = circuit()
+
+    with pytest.raises(ValueError):
+        _, _ = visualize_state(
+            execution_outcome=outcome,
+            mode=mode,
+        )
