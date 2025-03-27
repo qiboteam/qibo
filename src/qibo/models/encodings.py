db770ed402d48e2256f6eb6a6df44d9435b0205e
Renato Mello <renato.msf@gmail.com>
2025-03-27 10:49:17 +0400

    change return

--- src/qibo/models/encodings.py
+++ src/qibo/models/encodings.py
@@ -182,7 +182,7 @@ def sparse_encoder(data, nqubits: int = None, **kwargs):
         ones, new_ones = np.argsort(b_0)[-hw_0:], np.argsort(b_1)[-hw_1:]
         controls = (set(ones) & set(new_ones)) & set(touched_qubits)
 
-        gate = _get_gate_sparse(
+        gate, touched_qubits = _get_gate_sparse(
             distance,
             difference,
             touched_qubits,
@@ -1335,7 +1335,7 @@ def _get_gate_sparse(
             *controls
         )
 
-    return gate
+    return gate, touched_qubits
 
 
 def _get_phase_gate_correction_sparse(
