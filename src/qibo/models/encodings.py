7dbd191579a5a22c50081af10a6994e0e140fb3a
Peter_YSQH <72824407+AprilSweettooth@users.noreply.github.com>
2025-05-28 17:10:24 +0100

    Update src/qibo/models/encodings.py
    Co-authored-by: Renato M S Farias <renato.msf@gmail.com>

--- src/qibo/models/encodings.py
+++ src/qibo/models/encodings.py
@@ -1515,7 +1515,7 @@ def dicke_state(nqubits: int, weight: int, backend=None, **kwargs):
         _add_scs_gate(circuit, m, weight)
 
     # Recursively build the unitary U_n,k
-    for m in np.flip(range(2, k + 1)):
+    for m in range(k, 1, -1):
         # Add SCS_{m,m-1} acting on last m qubits
         _add_scs_gate(circuit, m, m - 1)
 
