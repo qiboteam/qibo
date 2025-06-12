772016a0db6c4efc1bc44044c79adfe7301a7ce2
Giacomo Franceschetto <65191292+GiacomoFrn@users.noreply.github.com>
2025-06-12 11:23:28 +0200

    Update src/qibo/gates/abstract.py
    Co-authored-by: BrunoLiegiBastonLiegi <45011234+BrunoLiegiBastonLiegi@users.noreply.github.com>

--- src/qibo/gates/abstract.py
+++ src/qibo/gates/abstract.py
@@ -386,8 +386,7 @@ class Gate:
         if g1.target_qubits != g2.target_qubits:
             return False
         if (
-            getattr(g1, "is_controlled_by", True)
-            and g1.control_qubits != g2.control_qubits
+            g1.control_qubits != g2.control_qubits
         ):
             return False
 
