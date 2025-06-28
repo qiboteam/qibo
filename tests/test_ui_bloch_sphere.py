87622d354b9be8112c0e032fd2aee7f6fb04c510
niccololaurora <niccolo.laurora@gmail.com>
2025-06-28 17:13:47 +0200

    Tests completed.

--- tests/test_ui_bloch_sphere.py
+++ tests/test_ui_bloch_sphere.py
@@ -5,39 +5,38 @@ import pytest
 from qibo import Circuit, gates
 from qibo.ui.bloch import Bloch
 
-# matplotlib.use("Agg")
+BACKEND = "qtagg"
 
 
 def test_empty_sphere():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
     bs.plot()
 
 
 def test_state():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
     state = np.array([1 / np.sqrt(2), 1 / np.sqrt(2) * 1j], dtype="complex")
     bs.add_state(state)
     bs.plot()
 
 
-@pytest.mark.parametrize("mode", ["vector", "point"])
-def test_vector_point(mode):
-    bs = Bloch()
+def test_vector_point():
+    bs = Bloch(backend=BACKEND)
     vector = np.array([0, 0, 1])
-    bs.add_vector(vector, mode=mode, color="green")
+    bs.add_vector(vector, mode="point", color="green")
     bs.plot()
 
 
 def test_multiple_vectors_array():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
     vectors = np.random.normal(size=(100, 3))
     vectors /= np.linalg.norm(vectors, axis=1)[:, np.newaxis]
-    bs.add_vector(vectors, color=["royalblue"] * 100)
-    bs.plot(save=True, filename="bloch_multiple_vectors.pdf")
+    bs.add_vector(vectors, color="royalblue")
+    bs.plot()
 
 
 def test_multiple_vectors_list():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
 
     vectors = []
     for i in range(100):
@@ -46,19 +45,19 @@ def test_multiple_vectors_list():
         vectors.append(vector)
 
     bs.add_vector(vectors, color=["royalblue"] * 100)
-    bs.plot(save=True, filename="bloch_1.pdf")
+    bs.plot()
 
 
 def test_multiple_states():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
     states = np.random.normal(size=(100, 2))
     states /= np.linalg.norm(states, axis=1)[:, np.newaxis]
     bs.add_state(states)
-    bs.plot(save=True, filename="bloch_2.pdf")
+    bs.plot()
 
 
-def test_clear():
-    bs = Bloch()
+def test_state_clear():
+    bs = Bloch(backend=BACKEND)
 
     states = np.zeros(shape=(100, 2), dtype="complex")
 
@@ -71,70 +70,60 @@ def test_clear():
 
     bs.add_state(states)
     bs.plot()
+
     bs.clear()
     bs.plot()
 
 
+def _circuit():
+    circ = Circuit(1)
+    circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
+    circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
+    circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
+    return circ
+
+
 def test_classification():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
     bs.add_state(np.array([1, 0]), color="black")
     bs.add_state(np.array([0, 1]), color="black")
 
     for i in range(20):
-        circ = Circuit(1)
-        circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
-        state = circ().state()
+        state = _circuit()(np.array([1, 0], dtype="complex")).state()
         bs.add_state(state, mode="point", color="red")
 
     for i in range(20):
-        circ = Circuit(1)
-        circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
-        state = circ(np.array([0, 1], dtype="complex")).state()
+        state = _circuit()(np.array([0, 1], dtype="complex")).state()
         bs.add_state(state, mode="point", color="blue")
 
-    bs.plot(save=True, filename="binary_classification.pdf")
+    bs.plot()
 
 
 def test_multi_classification():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
     bs.add_state(np.array([1, 0]), color="black")
     bs.add_state(np.array([0, 1]), color="black")
     bs.add_vector(np.array([0, 1, 0]), color="black")
 
     for i in range(20):
-        circ = Circuit(1)
-        circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
-        state = circ().state()
+        state = _circuit()(np.array([1, 0], dtype="complex")).state()
         bs.add_state(state, mode="point", color="red")
 
     for i in range(20):
-        circ = Circuit(1)
+        circ = _circuit()
         circ.add(gates.RX(q=0, theta=-np.pi / 2))
-        circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
-        state = circ().state()
-        bs.add_state(state, mode="point", color="orange")
+        bs.add_state(circ().state(), mode="point", color="orange")
 
     for i in range(20):
-        circ = Circuit(1)
-        circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
-        circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
+        circ = _circuit()
         state = circ(np.array([0, 1], dtype="complex")).state()
         bs.add_state(state, mode="point", color="blue")
 
-    bs.plot(save=True, filename="multi_class_classification.pdf")
+    bs.plot()
 
 
 def test_qibo_output():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
 
     # --Circuit--
     nqubits = 1
@@ -147,13 +136,12 @@ def test_qibo_output():
     # state
     state = circ().state()
     bs.add_state(state, color="orange")
-    bs.plot(save=True, filename="bloch_4.pdf")
+    bs.plot()
 
 
 def test_point_vector_state():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
 
-    # --Circuit--
     nqubits = 1
     layers = 2
     circ = Circuit(nqubits)
@@ -173,14 +161,12 @@ def test_point_vector_state():
     vector = np.array([1, 0, 0])
     bs.add_vector(vector, mode="vector", color="red")
 
-    # --Sphere--
-    bs.plot(save=True, filename="bloch_sphere_point_state_vector.pdf")
+    bs.plot()
 
 
 def test_save():
-    bs = Bloch()
+    bs = Bloch(backend=BACKEND)
 
-    # --Circuit--
     nqubits = 1
     layers = 2
     circ = Circuit(nqubits)
@@ -189,83 +175,14 @@ def test_save():
         circ.add(gates.RZ(q=0, theta=np.random.randn()))
     state = circ().state()
 
-    # --Sphere--
     bs.add_state(state, color=["orange"])
-    bs.plot(save=True, filename="bloch_sphere_1.pdf")
+    bs.save("bloch.pdf")
 
 
-def test_mismatch_colors_state_1():
-    bs = Bloch()
+def class_state():
 
-    # --Circuit--
-    nqubits = 1
-    layers = 2
-    circ = Circuit(nqubits)
-    for i in range(layers):
-        circ.add(gates.RY(q=0, theta=np.random.randn()))
-        circ.add(gates.RZ(q=0, theta=np.random.randn()))
+    circ = _circuit()
     state = circ().state()
-
-    # --Sphere--
-    bs.add_state(state, color=["orange", "blue"])
-    bs.plot()
-
-
-def test_mismatch_colors_state_2():
-    bs = Bloch()
-
-    # --Circuit--
-    nqubits = 1
-    layers = 2
-    states = []
-    for j in range(10):
-        circ = Circuit(nqubits)
-        for i in range(layers):
-            circ.add(gates.RY(q=0, theta=np.random.randn()))
-            circ.add(gates.RZ(q=0, theta=np.random.randn()))
-        state = circ().state()
-        states.append(state)
-
-    # --Sphere--
-    bs.add_state(states, color=["red"])
-    bs.plot()
-
-
-def test_mismatch_colors_state_4():
-    bs = Bloch()
-
-    # --Circuit--
-    nqubits = 1
-    layers = 2
-    states = np.zeros((10, 2), dtype=np.complex128)
-    for j in range(10):
-        circ = Circuit(nqubits)
-        for i in range(layers):
-            circ.add(gates.RY(q=0, theta=np.random.randn()))
-            circ.add(gates.RZ(q=0, theta=np.random.randn()))
-        state = circ().state()
-        states[j] = state
-
-    # --Sphere--
-    bs.add_state(states, color=np.array(["red"]))
-    bs.plot()
-
-
-def test_mismatch_colors_state_5():
-    bs = Bloch()
-
-    # --Circuit--
-    nqubits = 1
-    layers = 2
-    states = np.zeros((10, 2), dtype=np.complex128)
-    for j in range(10):
-        circ = Circuit(nqubits)
-        for i in range(layers):
-            circ.add(gates.RY(q=0, theta=np.random.randn()))
-            circ.add(gates.RZ(q=0, theta=np.random.randn()))
-        state = circ().state()
-        states[j] = state
-
-    # --Sphere--
-    bs.add_state(states, color="red")
+    vector = [[0, 1, 0], [0, 1, 1]]
+    bs = Bloch(backend=BACKEND)
     bs.plot()
