35dfd304e91584e29a5b2f702a2310b1802a36c1
niccololaurora <niccolo.laurora@gmail.com>
2025-06-28 16:06:11 +0200

    Private attributes.

--- src/qibo/ui/bloch.py
+++ src/qibo/ui/bloch.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib as mpl
 import tkinter as tk
 import importlib
-import time
 
 from matplotlib.figure import Figure
 from matplotlib.patches import FancyArrowPatch
@@ -14,11 +13,9 @@ from qibo import hamiltonians
 from qibo.config import raise_error
 from qibo.symbols import X, Y, Z
 
+from typing import Union
 from dataclasses import dataclass, field
-from typing import Union, Optional
-from types import ModuleType
 from numpy.typing import ArrayLike
-from contextlib import contextmanager
 
 
 class Arrow3D(FancyArrowPatch):
@@ -36,15 +33,22 @@ class Arrow3D(FancyArrowPatch):
 
 @dataclass
 class Bloch:
+    """This class creates a Bloch sphere.
+
+    It takes advantage of three Matplotlibs's backends: qtagg, tkagg, agg.
+    The first two are interactive backends required to display the sphere in a window, the last one is
+    non-interactive and it is used to save the figure."""
+
     backend: str = "tkagg"
 
     # Data
-    points: list = field(default_factory=list)
-    vectors: list = field(default_factory=list)
+    _points: list = field(default_factory=list)
+    _vectors: list = field(default_factory=list)
 
     # Color data
-    color_points: list = field(default_factory=list)
-    color_vectors: list = field(default_factory=list)
+    _color_states: list = field(default_factory=list)
+    _color_points: list = field(default_factory=list)
+    _color_vectors: list = field(default_factory=list)
 
     # Plot style sheets
     STYLE = {
@@ -55,9 +59,11 @@ class Bloch:
 
     # Figure and axis
     def __post_init__(self):
+        # Figure
         self.fig = Figure(figsize=self.STYLE["figure.figsize"])
         self.ax = self.fig.add_subplot(projection="3d", elev=30, azim=30)
 
+        # Backends
         if self.backend == "qtagg":
             mpl.use("qtagg")
             self._backend = importlib.import_module(
@@ -68,8 +74,14 @@ class Bloch:
         else:
             raise_error(ValueError, "Backend not supported. Try: `qtagg` or `tkagg`.")
 
+    def _new_window(self):
+        """It creates a new Figure object and it adds to it a new Axis."""
+        self.fig = Figure(figsize=self.STYLE["figure.figsize"])
+        self.ax = self.fig.add_subplot(projection="3d", elev=30, azim=30)
+
     # -----Sphere-----
     def _sphere_surface(self):
+        """Helper method to `_create_sphere` to construct the sphere's surface"""
         phi, theta = np.mgrid[0.0 : np.pi : 100j, 0.0 : 2.0 * np.pi : 100j]
         x = np.sin(phi) * np.cos(theta)
         y = np.sin(phi) * np.sin(theta)
@@ -77,6 +89,7 @@ class Bloch:
         return x, y, z
 
     def _axis(self):
+        """Helper method to `_create_sphere` to construct the sphere's axis"""
         theta = np.linspace(0, 2 * np.pi, 100)
         z = np.zeros(100)
         x = np.sin(theta)
@@ -84,6 +97,7 @@ class Bloch:
         return x, y, z
 
     def _latitude(self, z):
+        """Helper method to `_create_sphere` to construct the sphere's latitudes"""
         theta = np.linspace(0, 2 * np.pi, 100)
         z = np.full(100, z)
         r = np.sqrt(1 - z[0] ** 2)
@@ -92,6 +106,7 @@ class Bloch:
         return x, y, z
 
     def _meridian(self, phi):
+        """Helper method to `_create_sphere` to construct the sphere's meridians"""
         theta = np.linspace(0, 2 * np.pi, 100)
         x = np.sin(theta) * np.cos(phi)
         y = np.sin(theta) * np.sin(phi)
@@ -99,7 +114,7 @@ class Bloch:
         return x, y, z
 
     def _create_sphere(self):
-        "Function to create an empty sphere."
+        "This function constructs an empty Bloch sphere."
 
         # Empty sphere
         self.ax.plot_surface(*self._sphere_surface(), color="lavenderblush", alpha=0.2)
@@ -141,7 +156,7 @@ class Bloch:
 
     # -----States and Vectors-----
     def _coordinates(self, state):
-        "Function to determine the coordinates of a qubit in the sphere."
+        "This function determines the coordinates of a qubit in the sphere."
         x, y, z = 0, 0, 0
         if state[0] == 1 and state[0] == 0:
             z = 1
@@ -172,6 +187,7 @@ class Bloch:
             pass
 
         num_vector = len(vector)
+        print(num_vector)
         if isinstance(mode, str):
             mode = [mode] * num_vector
 
@@ -180,24 +196,22 @@ class Bloch:
 
         return vector, mode, color
 
-    def _new_window(self):
-        self.fig = Figure(figsize=self.STYLE["figure.figsize"])
-        self.ax = self.fig.add_subplot(projection="3d", elev=30, azim=30)
-
     def add_vector(
         self,
         vector: ArrayLike,
         mode: Union[str, list[str]] = "vector",
         color: Union[str, list[str]] = "black",
     ):
+        """This function adds a vector to the sphere."""
+
         vectors, modes, colors = self._broadcasting_semantics(vector, mode, color)
         for vector, color, mode in zip(vectors, colors, modes):
             if mode == "vector":
-                self.vectors.append(vector)
-                self.color_vectors.append(color)
+                self._vectors.append(vector)
+                self._color_vectors.append(color)
             elif mode == "point":
-                self.points.append(vector)
-                self.color_points.append(color)
+                self._points.append(vector)
+                self._color_points.append(color)
             else:
                 raise_error(ValueError, "Mode not supported. Try: `point` or `vector`.")
 
@@ -207,20 +221,23 @@ class Bloch:
         mode: Union[str, list[str]] = "vector",
         color: Union[str, list[str]] = "black",
     ):
-        "Function to add a state to the sphere."
+        "This function adds a state to the sphere."
+
         vectors, modes, colors = self._broadcasting_semantics(state, mode, color)
         for vector, color, mode in zip(vectors, colors, modes):
             x, y, z = self._coordinates(vector)
             if mode == "vector":
-                self.vectors.append(np.array([x, y, z]))
-                self.color_vectors.append(color)
+                self._vectors.append(np.array([x, y, z]))
+                self._color_vectors.append(color)
             elif mode == "point":
-                self.points.append(np.array([x, y, z]))
-                self.color_points.append(color)
+                self._points.append(np.array([x, y, z]))
+                self._color_points.append(color)
             else:
                 raise_error(ValueError, "Mode not supported. Try: `point` or `vector`.")
 
     def clear(self):
+        """This function clears the sphere."""
+
         self._new_window()
 
         # Clear data
@@ -233,9 +250,12 @@ class Bloch:
 
     # ----Plot and Save-----
     def _rendering(self):
+        """This function creates the empty sphere and plots the vectors and points on it."""
+
         self._create_sphere()
 
-        for color, vector in zip(self.color_vectors, self.vectors):
+        for color, vector in zip(self._color_vectors, self._vectors):
+            print(f"Componente vettore {vector[0]}, {vector[1]}, {vector[2]}")
             xs3d = vector[0] * np.array([0, 1])
             ys3d = vector[1] * np.array([0, 1])
             zs3d = vector[2] * np.array([0, 1])
@@ -250,7 +270,7 @@ class Bloch:
             )
             self.ax.add_artist(a)
 
-        for color, point in zip(self.color_points, self.points):
+        for color, point in zip(self._color_points, self._points):
             self.ax.scatter(point[0], point[1], point[2], color=color, s=10)
 
         self.ax.set_xlim([-0.7, 0.7])
@@ -261,6 +281,7 @@ class Bloch:
         self.fig.tight_layout()
 
     def _qt_window(self):
+        "Helper method to `plot()` for the `qtagg` backend."
         manager = self._backend.new_figure_manager_given_figure(1, self.fig)
         manager.show()
         manager.set_window_title("Bloch Sphere")
@@ -268,6 +289,7 @@ class Bloch:
         self.fig.show()
 
     def _tk_window(self):
+        "Helper method to `plot()` for the `tkagg` backend."
 
         root = tk.Tk()
         root.title("Bloch Sphere")
@@ -282,6 +304,8 @@ class Bloch:
         root.mainloop()
 
     def plot(self):
+        "This function plots the sphere."
+
         self._rendering()
 
         if self.backend == "tkagg":
@@ -290,6 +314,7 @@ class Bloch:
             self._qt_window()
 
     def save(self, filename="bloch_sphere.pdf"):
+        "This function saves the sphere."
         self._rendering()
 
         mpl.use("Agg")
