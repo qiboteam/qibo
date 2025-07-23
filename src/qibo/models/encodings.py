758f8ebe29ead2066132ac42d9ec1aa009f12746
qumaciel <maciel@tutanota.com>
2025-07-23 11:13:09 +0400

    use binary_encoder

--- src/qibo/models/encodings.py
+++ src/qibo/models/encodings.py
@@ -301,10 +301,15 @@ def _sparse_encoder_li(data, nqubits: int, backend=None, **kwargs):
 
     sigma = list(sigma)
 
+
+    rem  = int(2**backend.np.ceil(backend.np.log2(dim))) - dim
+    data_sorted = backend.np.pad(data_sorted,(0, rem),'constant', constant_values=(0.))
     # binary enconder on \sum_i = xi |sigma^{-1}(b_i)>
-    circuit_binary = sparse_encoder(
-        data_binary, method="farias", nqubits=nqubits, backend=backend, **kwargs
-    )
+    circuit = binary_encoder(data_sorted, backend=backend, **kwargs)
+    circuit.queue = circuit.queue[:(dim-1)]
+    circuit_binary = Circuit(nqubits)
+    circuit_binary.add(circuit.on_qubits(*range(nqubits-circuit.nqubits,nqubits)))
+
     circuit_permutation = permutation_synthesis(sigma)
 
     return circuit_binary + circuit_permutation
