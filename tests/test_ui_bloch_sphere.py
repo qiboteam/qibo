25865bfb5fa50bd9192afb79efa0e4be71657379
niccololaurora <niccolo.laurora@gmail.com>
2025-09-21 17:40:10 +0200

    fix: plt.close() to a test

--- tests/test_ui_bloch_sphere.py
+++ tests/test_ui_bloch_sphere.py
@@ -4,7 +4,7 @@ import numpy as np
 from qibo import Circuit, gates
 from qibo.ui.bloch import Bloch
 
-BACKEND = "tkagg"
+
 
 def _circuit():
     circ = Circuit(1)
@@ -13,32 +13,29 @@ def _circuit():
     circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
     return circ
 
-
-def test_empty_sphere(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_empty_sphere():
+    bs = Bloch()
     bs.render()
     plt.show()
 
-test_empty_sphere(BACKEND)
-
-def test_state(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_state():
+    bs = Bloch()
     state = np.array([1 / np.sqrt(2), 1 / np.sqrt(2) * 1j], dtype="complex")
     bs.add_state(state)
     bs.render()
     plt.show()
 
 
-def test_vector_point(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_vector_point():
+    bs = Bloch()
     vector = np.array([0, 0, 1])
     bs.add_vector(vector, mode="point", color="green")
     bs.render()
     plt.show()
 
 
-def test_multiple_vectors_array(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_multiple_vectors_array():
+    bs = Bloch()
     vectors = np.random.normal(size=(100, 3))
     vectors /= np.linalg.norm(vectors, axis=1)[:, np.newaxis]
     bs.add_vector(vectors, color="royalblue")
@@ -46,8 +43,8 @@ def test_multiple_vectors_array(BACKEND):
     plt.show()
 
 
-def test_multiple_vectors_list(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_multiple_vectors_list():
+    bs = Bloch()
 
     vectors = []
     for i in range(100):
@@ -56,10 +53,11 @@ def test_multiple_vectors_list(BACKEND):
         vectors.append(vector)
 
     bs.add_vector(vectors, color=["royalblue"] * 100)
-    bs.show()
+    bs.render()
+    plt.show()
 
-def test_multiple_states(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_multiple_states():
+    bs = Bloch()
     states = np.random.normal(size=(100, 2))
     states /= np.linalg.norm(states, axis=1)[:, np.newaxis]
     bs.add_state(states)
@@ -67,8 +65,8 @@ def test_multiple_states(BACKEND):
     plt.show()
 
 
-def test_state_clear(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_state_clear():
+    bs = Bloch()
     states = np.zeros(shape=(100, 2), dtype="complex")
 
     for i in range(100):
@@ -87,8 +85,8 @@ def test_state_clear(BACKEND):
     plt.show()
 
 
-def test_classification(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_classification():
+    bs = Bloch()
     bs.add_state(np.array([1, 0]), color="black")
     bs.add_state(np.array([0, 1]), color="black")
 
@@ -104,8 +102,8 @@ def test_classification(BACKEND):
     plt.show()
 
 
-def test_multi_classification(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_multi_classification():
+    bs = Bloch()
     bs.add_state(np.array([1, 0]), color="black")
     bs.add_state(np.array([0, 1]), color="black")
     bs.add_vector(np.array([0, 1, 0]), color="black")
@@ -122,14 +120,14 @@ def test_multi_classification(BACKEND):
     for _ in range(20):
         circ = _circuit()
         state = circ(np.array([0, 1], dtype="complex")).state()
-        bs.add_state(state, mode="point", color="blue")
+        bs.add_state(state, mode="point", color="magenta")
 
     bs.render()
     plt.show()
 
 
-def test_qibo_output(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_qibo_output():
+    bs = Bloch()
 
     # --Circuit--
     nqubits = 1
@@ -146,8 +144,8 @@ def test_qibo_output(BACKEND):
     plt.show()
 
 
-def test_point_vector_state(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_point_vector_state():
+    bs = Bloch()
 
     nqubits = 1
     layers = 2
@@ -172,8 +170,8 @@ def test_point_vector_state(BACKEND):
     plt.show()
 
 
-def test_save(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_save():
+    bs = Bloch()
 
     nqubits = 1
     layers = 2
@@ -187,10 +185,11 @@ def test_save(BACKEND):
     bs.add_state(state, color=["orange"])
     bs.render()
     plt.savefig("bloch.pdf")
+    plt.close()
 
 
-def test_many_spheres(BACKEND):
-    bs = Bloch(backend=BACKEND)
+def test_many_spheres():
+    bs = Bloch()
     bs.render()
 
     for _ in range(20):
