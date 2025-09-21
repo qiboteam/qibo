7cdabe0e323aa183d816b32119607d7f0982b343
niccololaurora <niccolo.laurora@gmail.com>
2025-09-21 17:30:06 +0200

    fix: tests compatible with new Bloch class

--- tests/test_ui_bloch_sphere.py
+++ tests/test_ui_bloch_sphere.py
@@ -1,57 +1,40 @@
-import tkinter
-from unittest.mock import patch
-
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
-import pytest
-from matplotlib.figure import Figure
 
 from qibo import Circuit, gates
 from qibo.ui.bloch import Bloch
 
 BACKEND = "tkagg"
 
-
-@pytest.fixture(params=["tkagg", "qtagg"])
-def BACKEND(request):
-    return request.param
-
-
-def run_test(bs, mock_draw, mock_mainloop):
-    bs.plot()
-    mock_draw.assert_called_once()
-    mock_mainloop.assert_called_once()
-
-
-def patch_qtagg_tkagg(bs):
-    if BACKEND == "tkagg":
-        with patch.object(bs._backend.FigureCanvas, "draw") as mock_draw, patch(
-            "tkinter.Tk.mainloop"
-        ) as mock_mainloop:
-            run_test(bs, mock_draw, mock_mainloop)
-    elif BACKEND == "qtagg":
-        with patch.object(bs._backend, "Show") as mock_draw:
-            instance = mock_draw.return_value
-            with patch.object(instance, "mainloop") as mock_mainloop:
-                run_test(bs, mock_draw, mock_mainloop)
+def _circuit():
+    circ = Circuit(1)
+    circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
+    circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
+    circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
+    return circ
 
 
 def test_empty_sphere(BACKEND):
     bs = Bloch(backend=BACKEND)
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
+test_empty_sphere(BACKEND)
 
 def test_state(BACKEND):
     bs = Bloch(backend=BACKEND)
     state = np.array([1 / np.sqrt(2), 1 / np.sqrt(2) * 1j], dtype="complex")
     bs.add_state(state)
+    bs.render()
+    plt.show()
 
 
 def test_vector_point(BACKEND):
     bs = Bloch(backend=BACKEND)
     vector = np.array([0, 0, 1])
     bs.add_vector(vector, mode="point", color="green")
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
 
 def test_multiple_vectors_array(BACKEND):
@@ -59,7 +42,8 @@ def test_multiple_vectors_array(BACKEND):
     vectors = np.random.normal(size=(100, 3))
     vectors /= np.linalg.norm(vectors, axis=1)[:, np.newaxis]
     bs.add_vector(vectors, color="royalblue")
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
 
 def test_multiple_vectors_list(BACKEND):
@@ -72,20 +56,19 @@ def test_multiple_vectors_list(BACKEND):
         vectors.append(vector)
 
     bs.add_vector(vectors, color=["royalblue"] * 100)
-    patch_qtagg_tkagg(bs)
-
+    bs.show()
 
 def test_multiple_states(BACKEND):
     bs = Bloch(backend=BACKEND)
     states = np.random.normal(size=(100, 2))
     states /= np.linalg.norm(states, axis=1)[:, np.newaxis]
     bs.add_state(states)
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
 
 def test_state_clear(BACKEND):
     bs = Bloch(backend=BACKEND)
-
     states = np.zeros(shape=(100, 2), dtype="complex")
 
     for i in range(100):
@@ -96,18 +79,12 @@ def test_state_clear(BACKEND):
         states[i] = state
 
     bs.add_state(states)
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
     bs.clear()
-    patch_qtagg_tkagg(bs)
-
-
-def _circuit():
-    circ = Circuit(1)
-    circ.add(gates.RY(q=0, theta=np.random.randn() * 0.1))
-    circ.add(gates.RX(q=0, theta=np.random.randn() * 0.1))
-    circ.add(gates.RZ(q=0, theta=np.random.randn() * 0.1))
-    return circ
+    bs.render()
+    plt.show()
 
 
 def test_classification(BACKEND):
@@ -115,15 +92,16 @@ def test_classification(BACKEND):
     bs.add_state(np.array([1, 0]), color="black")
     bs.add_state(np.array([0, 1]), color="black")
 
-    for i in range(20):
+    for _ in range(20):
         state = _circuit()(np.array([1, 0], dtype="complex")).state()
         bs.add_state(state, mode="point", color="red")
 
-    for i in range(20):
+    for _ in range(20):
         state = _circuit()(np.array([0, 1], dtype="complex")).state()
         bs.add_state(state, mode="point", color="blue")
 
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
 
 def test_multi_classification(BACKEND):
@@ -132,21 +110,22 @@ def test_multi_classification(BACKEND):
     bs.add_state(np.array([0, 1]), color="black")
     bs.add_vector(np.array([0, 1, 0]), color="black")
 
-    for i in range(20):
+    for _ in range(20):
         state = _circuit()(np.array([1, 0], dtype="complex")).state()
         bs.add_state(state, mode="point", color="red")
 
-    for i in range(20):
+    for _ in range(20):
         circ = _circuit()
         circ.add(gates.RX(q=0, theta=-np.pi / 2))
         bs.add_state(circ().state(), mode="point", color="orange")
 
-    for i in range(20):
+    for _ in range(20):
         circ = _circuit()
         state = circ(np.array([0, 1], dtype="complex")).state()
         bs.add_state(state, mode="point", color="blue")
 
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
 
 def test_qibo_output(BACKEND):
@@ -163,7 +142,8 @@ def test_qibo_output(BACKEND):
     # state
     state = circ().state()
     bs.add_state(state, color="orange")
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
 
 def test_point_vector_state(BACKEND):
@@ -188,19 +168,49 @@ def test_point_vector_state(BACKEND):
     vector = np.array([1, 0, 0])
     bs.add_vector(vector, mode="vector", color="red")
 
-    patch_qtagg_tkagg(bs)
+    bs.render()
+    plt.show()
 
 
-def test_save():
-    bs = Bloch(backend="agg")
+def test_save(BACKEND):
+    bs = Bloch(backend=BACKEND)
 
     nqubits = 1
     layers = 2
+
     circ = Circuit(nqubits)
-    for i in range(layers):
+    for _ in range(layers):
         circ.add(gates.RY(q=0, theta=np.random.randn()))
         circ.add(gates.RZ(q=0, theta=np.random.randn()))
     state = circ().state()
 
     bs.add_state(state, color=["orange"])
-    bs.save("bloch.pdf")
+    bs.render()
+    plt.savefig("bloch.pdf")
+
+
+def test_many_spheres(BACKEND):
+    bs = Bloch(backend=BACKEND)
+    bs.render()
+
+    for _ in range(20):
+        circ = _circuit()
+        state = circ(np.array([0, 1], dtype="complex")).state()
+        bs.add_state(state, mode="point", color="blue")
+    
+    bs.render()
+    plt.show()
+    bs.clear()
+
+    for _ in range(20):
+        circ = _circuit()
+        state = circ(np.array([0, 1], dtype="complex")).state()
+        bs.add_state(state, mode="point", color="red")
+    
+    bs.render()
+    plt.show()
+    bs.clear()
+
+    bs.render()
+    plt.show()
+
