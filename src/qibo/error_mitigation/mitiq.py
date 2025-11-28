97329f7cd9afbfbb30ea86ebda59fce890ec4111
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2025-11-28 09:00:15 +0100

    feat: mitiq observable conversion

--- src/qibo/error_mitigation/mitiq.py
+++ src/qibo/error_mitigation/mitiq.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Dict, Optional
+from types import NoneType
+from typing import Callable, Dict, Optional, Union
 
 import mitiq
 
@@ -8,7 +9,7 @@ from qibo.error_mitigation.abstract import (
     ErrorMitigationRoutine,
     ReadoutMitigationRoutine,
 )
-from qibo.hamiltonians.abstract import AbstractHamiltonian
+from qibo.hamiltonians import SymbolicHamiltonian
 from qibo.models.circuit import Circuit
 from qibo.noise import NoiseModel
 
@@ -29,15 +30,43 @@ class Mitiq(ErrorMitigationRoutine):
             self.method_kwargs = {}
 
     @property
-    def _run(self):
+    def _run(self) -> Callable:
         return getattr(self.method, self._run_method)
+
+    def _qibo_observable_to_mitiq(
+        self, observable: SymbolicHamiltonian
+    ) -> mitiq.Observable:
+        pauli_strings = []
+        for coefficient, pauli, qubits in zip(*observable.simple_terms):
+            pauli_string = ""
+            for q in range(max(qubits)):
+                if q in qubits:
+                    index = qubits.index(q)
+                    pauli_string += pauli[index]
+                else:
+                    pauli_string += "I"
+            pauli_strings.append(
+                mitiq.PauliString(spec=pauli_string, coeff=coefficient)
+            )
+        return mitiq.Observable(*pauli_strings)
+
+    def _observable(
+        self, observable: Union[SymbolicHamiltonian, NoneType]
+    ) -> Union[SymbolicHamiltonian, NoneType]:
+        observable = super()._observable(observable)
+        return self._qibo_observable_to_mitiq(observable)
 
     def __call__(
         self,
         circuit: Circuit,
-        observable: AbstractHamiltonian,
+        observable: SymbolicHamiltonian,
         nshots: Optional[int] = None,
         noise_model: Optional[NoiseModel] = None,
+        **mitiq_kwargs,
     ):
-        circuit = self._circuit_preprocessing([circuit], noise_model)
-        return self._run(circuit[0], **self.method_kwargs)
+        circuit = self._circuit_preprocessing([circuit], noise_model)[0]
+        observable = self._observable(observable)
+        kwargs = self.method_kwargs.update(mitiq_kwargs)
+        if observable is not None:
+            kwargs["observable"] = observable
+        return self._run(circuit, **kwargs)
