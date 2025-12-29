79ab7b95a90c8cec24c3d098dfab71c20f994de2
Renato Mello <renato.msf@gmail.com>
2025-12-29 09:18:34 +0400

    remove redundancy

--- src/qibo/backends/abstract.py
+++ src/qibo/backends/abstract.py
@@ -704,11 +704,6 @@ class Backend:  # pylint: disable=R0904
             if k < matrix.shape[0]:
                 return self.eigsh(matrix, k=k, which="SA")
 
-            matrix = self.to_numpy(matrix)  # pylint: disable=E1111  # pragma: no cover
-            matrix = self.cast(  # pylint: disable=E1111  # pragma: no cover
-                matrix, dtype=matrix.dtype
-            )
-
         if hermitian:
             return self.eigh(matrix)
 
